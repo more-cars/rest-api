9054f42d98f1d2f8c582b6d810a51359af04001c
Dennis Gerike <dennis.gerike+github@gmail.com>
2024-08-30 16:54:11 +0200

    adding a validation script to verify that the API documentation conforms to the OpenApi specification

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 * Clone repository: https://github.com/more-cars/rest-api.git
 * run `npm install`
 * run `npm start`
+
+## API specification
+
+Every REST API endpoint is documented in an _OpenAPI_ file.
+It can be found in the `specification` folder in the project's
+root directory.
+To verify that the specification is a valid OpenAPI document, the command `npm run validate-api-schema` can be executed. 
---
+++ tests/schema-validation.ts
@@ -0,0 +1,12 @@
+const OpenAPISchemaValidator = require('openapi-schema-validator').default
+
+const targetVersion = 3
+const validator = new OpenAPISchemaValidator({version: targetVersion})
+const targetSchema = require('../specification/OpenApi.json')
+
+if (validator.validate(targetSchema).errors.length > 0) {
+    console.log(`⨯ Error: The provided schema does not conform to OpenApi version ${targetVersion}.`)
+    process.exitCode = 1
+} else {
+    console.log(`✓ The provided schema conforms to the OpenApi version ${targetVersion}.`)
+}
