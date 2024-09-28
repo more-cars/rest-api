af63898c9e31422598e8b0060412060dce3a6c4a
Dennis Gerike <dennis.gerike+github@gmail.com>
2024-09-28 12:22:05 +0200

    adding a command to remove the whole app from the local kubernetes cluster

---
+++ deployment/undeploy-app.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# determining the path to this script,
+# to find the kubernetes scripts,
+# so this script can be called from any folder
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+kubectl delete namespace dev
