442fcd1481ca198ba2e0fe1d5ad2f1088da9bb7a
Dennis Gerike <dennis.gerike+github@gmail.com>
2024-10-13 10:44:29 +0200

    extending the API to serve the OpenAPI specification under the root path

--- server.ts
+++ server.ts
@@ -7,6 +7,13 @@ server.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`)
 })
 
+// API specification
+server.get('/', async (req, res) => {
+    const apiSpec = require('./specification/OpenApi.json')
+    res.status(200)
+    res.send(apiSpec)
+})
+
 // Health check
 server.get('/health', async (req, res) => {
     res.sendStatus(200)
