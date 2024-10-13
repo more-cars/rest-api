c3f6f7eaf07699316a48e44b5064e146fac5987e
Dennis Gerike <dennis.gerike+github@gmail.com>
2024-10-13 10:50:40 +0200

    for the Swagger UI to be able to fetch the API specification the application needs to allow the cross-origin access (CORS)

--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
 import express, {Express} from "express"
+import cors from "cors"
 
 const server: Express = express()
 const PORT = 3000
+
+// Allowing all CORS requests
+// TODO: CORS was enabled to allow the Swagger UI to load the API Spec. Does this have any side-effects or introduce security risks?
+server.use(cors())
 
 server.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`)
