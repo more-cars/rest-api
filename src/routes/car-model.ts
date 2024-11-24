3cae5158fbc154850cec01bde1fe35b61a6b917b
Dennis Gerike <dennis.gerike+github@gmail.com>
2024-11-24 11:04:45 +0100

    MCA-4 extending the API to return the requested node

--- src/routes/car-model.ts
+++ src/routes/car-model.ts
@@ -6,10 +6,15 @@ module.exports = (app: Express) => {
     // Get car model by id
     app.get('/car-models/:id', async (req, res) => {
         const carModel = CarModel.findById(parseInt(req.params.id))
+
         if (!carModel) {
             res.status(404)
             res.setHeader('Content-Type', 'text/plain')
-            res.send(`A "Car Model" with ID ${req.params.id} could not be found.`)
+            return res.send(`A "Car Model" with ID ${req.params.id} could not be found.`)
         }
+
+        res.status(200)
+        res.setHeader('Content-Type', 'application/json')
+        res.send(carModel)
     })
 }
