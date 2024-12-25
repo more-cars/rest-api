83b229795c20e376858ba93456c3505abf545b00
Dennis Gerike <info@dennis-gerike.de>
2024-12-25 09:39:21 +0100

    creating a subfolder for API tests

--- tests/api/schema-validation.ts
+++ tests/api/schema-validation.ts
@@ -2,7 +2,7 @@ const OpenAPISchemaValidator = require('openapi-schema-validator').default
 
 const targetVersion = 3
 const validator = new OpenAPISchemaValidator({version: targetVersion})
-const targetSchema = require('../specification/OpenAPI/more-cars.openapi.json')
+const targetSchema = require('../../specification/OpenAPI/more-cars.openapi.json')
 
 if (validator.validate(targetSchema).errors.length > 0) {
     console.log(`⨯ Error: The provided schema does not conform to OpenApi version ${targetVersion}.`)
