60b2108ff32e33489b1fd414dbeb5fb516e31319
Dennis Gerike <info@dennis-gerike.de>
2024-12-28 11:18:29 +0100

    removing default value from jest config

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,6 @@ import type {Config} from 'jest'
 
 const config: Config = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
     testMatch: ['<rootDir>/tests/**/*Test.ts'],
     collectCoverage: false,
     coverageProvider: "v8",
