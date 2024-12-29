04d11f919579d74a2864691312d7f7db86a00fde
Dennis Gerike <info@dennis-gerike.de>
2024-12-29 18:30:08 +0100

    MCA-8 adding functionality (on models level) to create new car models

--- src/models/CarModel.ts
+++ src/models/CarModel.ts
@@ -1,7 +1,13 @@
 import {getCarModelNodeById} from "../db/getCarModelNodeById"
+import {CarModelType} from "../types/CarModelType"
+import {createCarModelNode} from "../db/createCarModelNode"
 
 export class CarModel {
     static async findById(id: number) {
         return await getCarModelNodeById(id)
+    }
+
+    static async create(data: CarModelType) {
+        return await createCarModelNode(data)
     }
 }
