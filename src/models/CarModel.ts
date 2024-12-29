f9d02dc642094ab6c2e393b0994627257edc0094
Dennis Gerike <info@dennis-gerike.de>
2024-12-29 17:14:13 +0100

    simplifying the car model test on the "models level" (mapping between "nodes" and "models" is not necessary at the moment, because they are identical)

--- src/models/CarModel.ts
+++ src/models/CarModel.ts
@@ -1,19 +1,7 @@
 import {getCarModelNodeById} from "../db/getCarModelNodeById"
-import {CarModelType} from "../types/CarModelType"
 
 export class CarModel {
     static async findById(id: number) {
-        const dbNode = await getCarModelNodeById(id)
-
-        if (!dbNode) {
-            return false
-        }
-
-        const node: CarModelType = {
-            mc_id: dbNode.mc_id,
-            name: dbNode.name,
-        }
-
-        return node
+        return await getCarModelNodeById(id)
     }
 }
