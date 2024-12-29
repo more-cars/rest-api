a7ac8ed033898a974a1b2ac5cbd022cc10a2dcc5
Dennis Gerike <info@dennis-gerike.de>
2024-12-29 16:47:44 +0100

    MCA-4 adding integration test that checks that the returned car model node satisfies the expected schema (database level)

--- tests/integration/db/carModelTest.ts
+++ tests/integration/db/carModelTest.ts
@@ -2,6 +2,8 @@ import {getCarModelNodeById} from "../../../src/db/getCarModelNodeById"
 import {createCarModelNode} from "../../../src/db/createCarModelNode"
 import {faker} from "@faker-js/faker"
 import {Neo4jError} from "neo4j-driver"
+import Ajv from "ajv"
+import {CarModelSchema} from "../../_schemas/CarModelSchema"
 
 describe('Car Model', () => {
     test('Creating a car model with invalid data should not be possible', async () => {
@@ -35,5 +37,26 @@ describe('Car Model', () => {
 
         expect(actualCarModelNode)
             .toBe(expectedCarModelNode)
+    })
+
+    test('Querying an existing car model should return a db node with correct schema', async () => {
+        // ARRANGE
+        const nodeId = faker.number.int({max: 10000})
+        await createCarModelNode({
+            mc_id: nodeId,
+            name: faker.vehicle.model(),
+        })
+
+        // ACT
+        const carModelNode = await getCarModelNodeById(nodeId)
+
+        // ASSERT
+        const validate = new Ajv().compile(CarModelSchema)
+        const valid = validate(carModelNode)
+        if (!valid) {
+            console.log(validate.errors)
+        }
+
+        expect(valid).toBe(true)
     })
 })
