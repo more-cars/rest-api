1b6c1ce2d4c755ba9b15c92a96d88166b3ab32d4
Dennis Gerike <info@dennis-gerike.de>
2024-12-29 18:22:11 +0100

    the "cors" middleware has to be defined BEFORE the routes are registered (no effect otherwise)

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,13 @@ import cors from "cors"
 const server: Express = express()
 const PORT = 3000
 
-require('./routes/health')(server)
-require('./routes/open-api-specification')(server)
-require('./routes/car-models')(server)
-
 // Allowing all CORS requests
 // TODO: CORS was enabled to allow the Swagger UI to load the API Spec. Does this have any side-effects or introduce security risks?
 server.use(cors())
+
+require('./routes/health')(server)
+require('./routes/open-api-specification')(server)
+require('./routes/car-models')(server)
 
 server.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`)
