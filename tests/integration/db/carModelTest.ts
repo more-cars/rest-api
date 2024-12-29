c67da7e68d4bf0f2ec7543690e7aa4b2e6db3958
Dennis Gerike <info@dennis-gerike.de>
2024-12-29 16:42:32 +0100

    MCA-9 adding integration test for unhappy path (database level)

--- tests/integration/db/carModelTest.ts
+++ tests/integration/db/carModelTest.ts
@@ -1,8 +1,23 @@
 import {getCarModelNodeById} from "../../../src/db/getCarModelNodeById"
 import {createCarModelNode} from "../../../src/db/createCarModelNode"
 import {faker} from "@faker-js/faker"
+import {Neo4jError} from "neo4j-driver"
 
 describe('Car Model', () => {
+    test('Creating a car model with invalid data should not be possible', async () => {
+        // Our first line of defense is TypeScript itself.
+        // When providing invalid data (in this case a non-existing field name)
+        // the type checker would terminate the program instantly with error code TS2353.
+        // When circumventing this type check via @ts-ignore then Neo4j is the second line of defense.
+        // The provided data should not be accepted, because the attribute mapping will not work anymore.
+        await expect(createCarModelNode({
+            mc_id: 987654,
+            // @ts-ignore
+            bad_name: faker.vehicle.model(),
+        })).rejects
+            .toThrow(Neo4jError)
+    })
+
     test('Creating a car model with valid data should result in a new database node', async () => {
         const carModelData = {
             mc_id: faker.number.int({max: 10000}),
