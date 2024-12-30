1efe68e1a1e58492b8ed51cbe34a89250eee9899
Dennis Gerike <info@dennis-gerike.de>
2024-12-30 10:39:37 +0100

    MCA-8 adding integration test for happy path (models level)

--- tests/integration/models/carModelTest.ts
+++ tests/integration/models/carModelTest.ts
@@ -1,4 +1,5 @@
 import {CarModel} from "../../../src/models/CarModel"
+import {faker} from "@faker-js/faker"
 
 describe('Car Model', () => {
     test('Fetching a car model that does not exist should return "false"', async () => {
@@ -7,5 +8,16 @@ describe('Car Model', () => {
 
         expect(actualCarModel)
             .toBe(expectedCarModel)
+    })
+
+    test('Creating a car model with valid data should return the new node', async () => {
+        const carModelData = {
+            mc_id: faker.number.int({max: 10000}),
+            name: faker.vehicle.model(),
+        }
+        const createdNode = await CarModel.create(carModelData)
+
+        expect(createdNode)
+            .toEqual(carModelData)
     })
 })
