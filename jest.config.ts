6db953246b70c8d8b746196325f4eb10351f5d9b
Dennis Gerike <info@dennis-gerike.de>
2025-01-05 11:28:07 +0100

    making sure Jest is able to parse the environment variables from the ".env" file

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,12 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
-
 import type {Config} from 'jest'
+
+// Making the variables in the ".env" file available as regular environment variables.
+// Only relevant for local development, because the CI/CD pipeline doesn't use ".env" files.
+// (Command will not fail when the file doesn't exist.)
+require('dotenv').config({path: '.env'})
 
 const config: Config = {
     preset: 'ts-jest',
