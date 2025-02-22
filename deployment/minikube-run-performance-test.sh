2cf8d7079b7f3716d1a8c5905d129d0c8c631a29
Dennis Gerike <info@dennis-gerike.de>
2025-02-22 11:42:56 +0100

    making the load tests available in minikube

---
+++ deployment/minikube-run-performance-test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# determining the path to this script,
+# to find the kubernetes scripts,
+# so this script can be called from any folder
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")/$1
+
+# making sure we are actually in the Minikube cluster, not in the Google cluster
+kubectl config use-context mc-api
+
+# switching to the correct namespace
+kubectl config set-context --current --namespace=$1
+
+# starting the tests (replacing the previous job if there was already one)
+kubectl replace --force -f $SCRIPT_PATH/performance-test-job.yaml
