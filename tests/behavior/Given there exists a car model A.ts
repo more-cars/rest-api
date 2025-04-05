6b963defc85443749f16f7a309f40cd5b42f05d1
Dennis Gerike <info@dennis-gerike.de>
2025-04-05 11:47:17 +0200

    the ID is not needed in the payload anymore

--- tests/behavior/Given there exists a car model A.ts
+++ tests/behavior/Given there exists a car model A.ts
@@ -5,7 +5,6 @@ import {faker} from "@faker-js/faker"
 Given('there exists a car model A', async function () {
     const carModelData = {
         name: faker.vehicle.model(),
-        id: faker.number.int({max: 10000}),
     }
     this.carModelA = await createCarModelNode(carModelData)
 })
