0e012b2075d0a1ca9a303d0254426d6dd1ac0705
Dennis Gerike <info@dennis-gerike.de>
2025-04-05 12:36:07 +0200

    introducing a database seeder to be able to create preconditions for the tests easier

--- tests/behavior/Given there exists a car model A.ts
+++ tests/behavior/Given there exists a car model A.ts
@@ -1,6 +1,5 @@
 import {Given} from "@cucumber/cucumber"
-import {createCarModelNode} from "../../src/db/createCarModelNode"
-import {faker} from "@faker-js/faker"
+import {seedCarModel} from "../dbSeeding/seedCarModel"
 
 Given('there exists a car model A', async function () {
     this.carModelA = await seedCarModel()
