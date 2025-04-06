c96bbe6f71ab8d67b00097d7180a9410afd76667
Dennis Gerike <info@dennis-gerike.de>
2025-04-06 08:57:34 +0200

    adding a marshaling/unmarshaling process when handling api requests and api responses

--- src/routes/car-models.ts
+++ src/routes/car-models.ts
@@ -1,11 +1,12 @@
 import {Express} from "express"
 import {CarModel} from "../models/CarModel"
+import {CarModelNode} from "../types/CarModelNode"
 
 module.exports = (app: Express) => {
     app.get('/car-models/:id', async (req, res) => {
-        const carModel = await CarModel.findById(parseInt(req.params.id))
+        const foundCarModel = await CarModel.findById(parseInt(req.params.id))
 
-        if (!carModel) {
+        if (!foundCarModel) {
             res.status(404)
             res.set('Content-Type', 'text/plain')
             return res.send(`A "Car Model" with ID ${req.params.id} could not be found.`)
@@ -13,19 +14,43 @@ module.exports = (app: Express) => {
 
         res.status(200)
         res.set('Content-Type', 'application/json')
-        res.send(carModel)
+        res.send(marshalResponseBody(foundCarModel))
     })
 
     app.post('/car-models', async (req, res) => {
         try {
-            const createdNode = await CarModel.create(req.body)
+            const createdNode = await CarModel.create(unmarshalRequestBody(req.body))
             res.status(201)
             res.set('Content-Type', 'application/json')
-            res.send(createdNode)
+            res.send(marshalResponseBody(createdNode))
         } catch (e) {
             res.status(422)
             res.set('Content-Type', 'text/plain')
             res.send('Request failed. Node could not be created.')
         }
     })
+}
+
+/**
+ * Selects those attributes from the given request body that are legitimate in the context of creating a car model.
+ * All other attributes that might exist in the object will be ignored.
+ */
+function unmarshalRequestBody(body: any) {
+    const carModel: CarModelNode = {
+        name: body.name,
+    }
+
+    return carModel
+}
+
+/**
+ * Creates a valid response body from the given car model.
+ */
+function marshalResponseBody(carModel: CarModelNode) {
+    const responseBody = {
+        id: carModel.id,
+        name: carModel.name,
+    }
+
+    return responseBody
 }
