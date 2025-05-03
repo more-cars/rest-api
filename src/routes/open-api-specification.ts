e276167f57e41bf1d44d3df71697d0f563343224
Dennis Gerike <info@dennis-gerike.de>
2025-05-03 09:58:38 +0200

    fixing linting error (require vs import)

--- src/routes/open-api-specification.ts
+++ src/routes/open-api-specification.ts
@@ -1,7 +1,8 @@
 import express from "express"
 
+import apiSpec from "../../specification/OpenAPI/more-cars.openapi.json"
+
 export async function openApiSpec(req: express.Request, res: express.Response) {
-    const apiSpec = require('../../specification/OpenAPI/more-cars.openapi.json')
     res.status(200)
     res.set('Content-Type', 'application/json')
     res.send(apiSpec)
