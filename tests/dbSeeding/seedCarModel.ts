2019cfb455fa57e0385f79e88f876dd5be11c54d
Dennis Gerike <info@dennis-gerike.de>
2025-05-05 12:33:21 +0200

    MCA-16 extending the db seeder to create car models with all core properties (not just the name)

--- tests/dbSeeding/seedCarModel.ts
+++ tests/dbSeeding/seedCarModel.ts
@@ -11,6 +11,11 @@ import {createCarModelNode} from "../../src/db/createCarModelNode"
 export async function seedCarModel() {
     const carModelData = {
         name: faker.vehicle.model(),
+        built_from: faker.number.int({min: 1000, max: 3000}),
+        built_to: faker.number.int({min: 1000, max: 3000}),
+        generation: faker.number.int({min: 1, max: 10}),
+        internal_code: faker.commerce.isbn(),
+        total_production: faker.number.int({min: 100, max: 10000000}),
     }
 
     return await createCarModelNode(carModelData)
