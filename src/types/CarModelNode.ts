804a5f0cbdb7f0bf8fb5e09c6545595cbf579052
Dennis Gerike <info@dennis-gerike.de>
2025-05-05 11:58:00 +0200

    MCA-16 extending the type definition of a "car model" to contain every core property

--- src/types/CarModelNode.ts
+++ src/types/CarModelNode.ts
@@ -1,4 +1,9 @@
 export type CarModelNode = {
     id?: number
     name: string
+    built_from?: number
+    built_to?: number
+    generation?: number
+    internal_code?: string
+    total_production?: number
 }
