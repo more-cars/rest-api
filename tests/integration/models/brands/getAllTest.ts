33935a75df14914a31791e624b974108f872d565
Dennis Gerike <info@dennis-gerike.de>
2025-05-07 11:11:32 +0200

    MCA-19 adding integration tests for requesting all "brand" nodes

---
+++ tests/integration/models/brands/getAllTest.ts
@@ -0,0 +1,27 @@
+import {deleteAllBrands} from "../../../dbSeeding/deleteAllBrands"
+import {BrandNode} from "../../../../src/types/BrandNode"
+import {Brand} from "../../../../src/models/Brand"
+import {seedBrands} from "../../../dbSeeding/seedBrands"
+
+describe('Brands', () => {
+    test('When there are no brands then an empty array should be returned', async () => {
+        await deleteAllBrands()
+
+        const expectedBrands: Array<BrandNode> = []
+        const actualBrands = await Brand.findAll()
+
+        expect(actualBrands)
+            .toEqual(expectedBrands)
+    })
+
+    test('When brands exist then all of them should be returned', async () => {
+        await deleteAllBrands()
+        const amount = Math.ceil(Math.random() * 50)
+        await seedBrands(amount)
+
+        const actualBrands = await Brand.findAll()
+
+        expect(actualBrands.length)
+            .toEqual(amount)
+    })
+})
