bf304fa9addd4639d70a6d2c4825840753b92e21
Dennis Gerike <info@dennis-gerike.de>
2025-05-27 17:00:14 +0200

    MCA-24 adjusting the test, so it can handle the new node type "image"

--- tests/behavior/Given/Given there exists a {node_type} {label}.ts
+++ tests/behavior/Given/Given there exists a {node_type} {label}.ts
@@ -1,14 +1,18 @@
 import {Given} from "@cucumber/cucumber"
 import {seedBrand} from "../../dbSeeding/seedBrand"
 import {seedCarModel} from "../../dbSeeding/seedCarModel"
+import {seedImage} from "../../dbSeeding/seedImage"
 
 Given('there exists a {string} {string}', async function (nodeType: string, label: string) {
-    switch (nodeType) {
+    switch (nodeType.toLowerCase()) {
         case 'brand':
             this.brand[label] = await seedBrand()
             break
         case 'car model':
             this.carModel[label] = await seedCarModel()
+            break
+        case 'image':
+            this.image[label] = await seedImage()
             break
     }
 })
