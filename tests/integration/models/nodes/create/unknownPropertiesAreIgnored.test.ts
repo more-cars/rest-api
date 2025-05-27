2c64b387a7cbf1d759db3029cd756ada170280ca
Dennis Gerike <info@dennis-gerike.de>
2025-05-27 15:32:14 +0200

    MCA-112 checking that unknown properties cannot be added by the user

---
+++ tests/integration/models/nodes/create/unknownPropertiesAreIgnored.test.ts
@@ -0,0 +1,35 @@
+import FakeCarModel from "../../../../fixtures/nodes/FakeCarModel"
+import FakeBrand from "../../../../fixtures/nodes/FakeBrand"
+import FakeImage from "../../../../fixtures/nodes/FakeImage"
+import {CarModel} from "../../../../../src/models/CarModel"
+import {Brand} from "../../../../../src/models/Brand"
+import {Image} from "../../../../../src/models/Image"
+
+describe('Create Node', () => {
+    test('Unknown properties are ignored', async () => {
+        let createdNode
+
+        // CAR MODEL
+        const carModelData = Object.assign(FakeCarModel, {
+            "my_property": "NOT_ALLOWED_TO_ADD"
+        })
+        createdNode = await CarModel.create(carModelData)
+        expect(createdNode)
+            .not.toContain("my_property")
+
+        // BRAND
+        const brandData = Object.assign(FakeBrand, {
+            "my_property": "NOT_ALLOWED_TO_ADD"
+        })
+        createdNode = await Brand.create(brandData)
+        expect(createdNode)
+            .not.toContain("my_property")
+
+        const imageData = Object.assign(FakeImage, {
+            "my_property": "NOT_ALLOWED_TO_ADD"
+        })
+        createdNode = await Image.create(imageData)
+        expect(createdNode)
+            .not.toContain("my_property")
+    })
+})
