02f5f07575f3ce8f84e080046408ac0922cebb77
Dennis Gerike <info@dennis-gerike.de>
2025-05-27 17:02:04 +0200

    MCA-141 fixing the test

--- tests/behavior/When/When the user creates a {node_type}.ts
+++ tests/behavior/When/When the user creates a {node_type}.ts
@@ -1,19 +1,25 @@
 import {When} from "@cucumber/cucumber"
 import axios from "axios"
+import FakeBrand from "../../fixtures/nodes/FakeBrand"
+import FakeCarModel from "../../fixtures/nodes/FakeCarModel"
+import FakeImage from "../../fixtures/nodes/FakeImage"
 
 When('the user creates a(n) {string}', async function (nodeType: string) {
-    const data: any = {}
+    let data: any = {}
     let path: string
 
     switch (nodeType.toLowerCase()) {
         case 'brand':
             path = 'brands'
+            data = FakeBrand
             break
         case 'car model':
             path = 'car-models'
+            data = FakeCarModel
             break
         case 'image':
             path = 'images'
+            data = FakeImage
             break
         default:
             return
