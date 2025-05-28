7e310b9f35fb026a8d6ffc58428a5a6dbf315a67
Dennis Gerike <info@dennis-gerike.de>
2025-05-28 17:52:41 +0200

    MCA-132 implementing the Gherkin scenario

---
+++ tests/behavior/Then/Then the property {property_key} of all items in the response should be a number greater than {value}.ts
@@ -0,0 +1,10 @@
+import {Then} from "@cucumber/cucumber"
+import assert from "assert"
+
+Then('the property {string} of all items in the response should be a number greater than {int}', function (key: string, value: string) {
+    const responseData = this.latestResponse.data
+
+    responseData.forEach((item: any) => {
+        assert.equal(parseInt(item[key]) > parseInt(value), true, `"${item[key]}" is not greater than "${value}"`)
+    })
+})
---
+++ tests/behavior/When/When the user requests an existing {node_type}.ts
@@ -0,0 +1,31 @@
+import {When} from "@cucumber/cucumber"
+import axios from "axios"
+import {BaseNode} from "../../../src/types/BaseNode"
+import {getAllNodesOfType as getAllBrands} from "../../../src/db/brands/getAllNodesOfType"
+import {getAllNodesOfType as getAllCarModels} from "../../../src/db/car-models/getAllNodesOfType"
+import {getAllNodesOfType as getAllImages} from "../../../src/db/images/getAllNodesOfType"
+
+When('the user requests an existing {string}', async function (nodeType: string) {
+    let path: string
+    let node: BaseNode
+
+    switch (nodeType.toLowerCase()) {
+        case 'brand':
+            path = 'brands'
+            node = (await getAllBrands())[0]
+            break
+        case 'car model':
+            path = 'car-models'
+            node = (await getAllCarModels())[0]
+            break
+        case 'image':
+            path = 'images'
+            node = (await getAllImages())[0]
+            break
+        default:
+            return
+    }
+
+    this.latestResponse = await axios
+        .get(`${process.env.API_URL}/${path}/${node.id}`)
+})
