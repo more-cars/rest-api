de9b41e9b605308c7c5bd710c75f05487cde849b
Dennis Gerike <info@dennis-gerike.de>
2025-05-29 10:55:30 +0200

    Revert "refactoring: simplifying dotenv configuration in jest tests"
    This reverts commit 344b24c7e9239b06627a335c1aeb0594aef6b817.

--- tests/integration/jest.config.ts
+++ tests/integration/jest.config.ts
@@ -1,3 +1,10 @@
+import * as path from "node:path"
+
+// Making the variables in the ".env" file available as regular environment variables.
+// Only relevant for local development, because the CI/CD pipeline doesn't use ".env" files.
+// (Command will not fail when the file doesn't exist.)
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')})
+
 module.exports = {
     displayName: 'Integration Tests',
     testMatch: ['<rootDir>/**/*.test.ts'],
@@ -5,9 +12,6 @@ module.exports = {
     maxWorkers: 1,
     forceExit: true,
     preset: 'ts-jest',
-    setupFiles: [
-        'dotenv/config',
-    ],
     reporters: [
         "default",
         ["../../node_modules/jest-html-reporter", {
