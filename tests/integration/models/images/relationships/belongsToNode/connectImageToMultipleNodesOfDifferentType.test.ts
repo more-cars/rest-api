e9580ef9e02e1c11c0d79b0a35d4b4738e0260ad
Dennis Gerike <info@dennis-gerike.de>
2025-05-30 13:33:10 +0200

    MCA-153 checking that multiple nodes of different type can be attached to an image

---
+++ tests/integration/models/images/relationships/belongsToNode/connectImageToMultipleNodesOfDifferentType.test.ts
@@ -0,0 +1,26 @@
+import {Image} from "../../../../../../src/models/Image"
+import {seedImage} from "../../../../../dbSeeding/seedImage"
+import {seedBrand} from "../../../../../dbSeeding/seedBrand"
+import {seedCarModel} from "../../../../../dbSeeding/seedCarModel"
+
+describe('Image', () => {
+    test('An image can be connected to multiple other nodes from different types', async () => {
+        const imageNode = await seedImage()
+        const brandNode1 = await seedBrand()
+        const brandNode2 = await seedBrand()
+        const carModelNode1 = await seedCarModel()
+        const carModelNode2 = await seedCarModel()
+
+        const createdRelationship1 = await Image.createBelongsToNodeRelationship(imageNode.id, brandNode1.id)
+        expect(createdRelationship1).not.toBeFalsy()
+
+        const createdRelationship2 = await Image.createBelongsToNodeRelationship(imageNode.id, brandNode2.id)
+        expect(createdRelationship2).not.toBeFalsy()
+
+        const createdRelationship3 = await Image.createBelongsToNodeRelationship(imageNode.id, carModelNode1.id)
+        expect(createdRelationship3).not.toBeFalsy()
+
+        const createdRelationship4 = await Image.createBelongsToNodeRelationship(imageNode.id, carModelNode2.id)
+        expect(createdRelationship4).not.toBeFalsy()
+    })
+})
