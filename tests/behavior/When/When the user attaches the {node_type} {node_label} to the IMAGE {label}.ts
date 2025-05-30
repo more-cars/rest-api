af256094b99392133c324f475461e7f4802ceef2
Dennis Gerike <info@dennis-gerike.de>
2025-05-30 15:07:43 +0200

    MCA-164 implementing the Gherkin scenario

---
+++ tests/behavior/When/When the user attaches the {node_type} {node_label} to the IMAGE {label}.ts
@@ -0,0 +1,26 @@
+import {When} from "@cucumber/cucumber"
+import axios from "axios"
+import {ImageNode} from "../../../src/types/images/ImageNode"
+import {BaseNode} from "../../../src/types/BaseNode"
+
+When('the user attaches the {string} {string} to the IMAGE {string}',
+    async function (partnerNodeType: string, partnerNodeLabel: string, imageLabel: string) {
+        const imageNode: ImageNode = this.image[imageLabel]
+        const partnerNode: BaseNode = this[mapNodeTypeNameToInternalReference(partnerNodeType)][partnerNodeLabel]
+
+        this.latestResponse = await axios
+            .post(`${process.env.API_URL}/images/${imageNode.id}/belongs-to-node/${partnerNode.id}`)
+            .catch(error => {
+                console.error(error)
+            })
+    })
+
+function mapNodeTypeNameToInternalReference(nodeType: string): string {
+    const mapping = {
+        'brand': 'brand',
+        'car model': 'carModel'
+    }
+
+    // @ts-ignore
+    return mapping[nodeType.toLowerCase()]
+}
