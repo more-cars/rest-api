6c324adbe501267578c7257d5fcf254251ec1fa7
Dennis Gerike <info@dennis-gerike.de>
2025-06-01 18:29:03 +0200

    MCA-173 implementing the Gherkin scenario

---
+++ tests/behavior/When/When the user tries to request all relationships for IMAGE {label}.ts
@@ -0,0 +1,18 @@
+import {When} from "@cucumber/cucumber"
+import axios from "axios"
+import {ImageNode} from "../../../src/types/images/ImageNode"
+
+When('the user tries to request all relationships for IMAGE {string}',
+    async function (imageLabel: string) {
+        const imageNode: ImageNode = this.image[imageLabel]
+
+        this.latestResponse = await axios
+            .get(`${process.env.API_URL}/images/${imageNode.id}/belongs-to-node`, {
+                validateStatus: function (status) {
+                    return status === 404 // treating 404 as a "good" status code, so axios does not fail the request
+                }
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    })
