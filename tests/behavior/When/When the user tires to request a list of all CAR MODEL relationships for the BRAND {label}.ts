5cddc832bdbe466909274f42db5d9344e508e0ca
Dennis Gerike <info@dennis-gerike.de>
2025-06-13 16:43:10 +0200

    MCA-200 implementing the Gherkin scenario

---
+++ tests/behavior/When/When the user tires to request a list of all CAR MODEL relationships for the BRAND {label}.ts
@@ -0,0 +1,18 @@
+import {When} from "@cucumber/cucumber"
+import axios from "axios"
+import {BrandNode} from "../../../src/types/brands/BrandNode"
+
+When('the user tries to request a list of all CAR MODEL relationships for the BRAND {string}',
+    async function (label: string) {
+        const brandNode: BrandNode = this.brand[label]
+
+        this.latestResponse = await axios
+            .get(`${process.env.API_URL}/brands/${brandNode.id}/has-car-model`, {
+                validateStatus: function (status) {
+                    return status === 404 // treating 404 as a "good" status code, so axios does not fail the request
+                }
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    })
