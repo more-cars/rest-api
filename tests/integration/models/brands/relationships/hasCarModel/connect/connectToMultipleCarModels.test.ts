fa635d0f162e34067c63800e637b95ee300131ee
Dennis Gerike <info@dennis-gerike.de>
2025-06-14 11:26:53 +0200

    MCA-85 checking that multiple car models can be connected the same brand

---
+++ tests/integration/models/brands/relationships/hasCarModel/connect/connectToMultipleCarModels.test.ts
@@ -0,0 +1,23 @@
+import {seedBrand} from "../../../../../../dbSeeding/brands/nodes/seedBrand"
+import {Brand} from "../../../../../../../src/models/brands/Brand"
+import {seedCarModels} from "../../../../../../dbSeeding/car-models/nodes/seedCarModels"
+import assert from "assert"
+
+test('A BRAND can have multiple CAR MODELs attached to it', async () => {
+    const brand = await seedBrand()
+    const carModelAmount = 3
+    const carModels = await seedCarModels(carModelAmount)
+
+    for (const carModel of carModels) {
+        await Brand.createHasCarModelRelationship(brand, carModel)
+    }
+
+    const relationships = await Brand.getRelationshipsForHasCarModel(brand.id)
+
+    if (!relationships) {
+        assert.fail(`Brand #${brand.id} not found.`)
+    }
+
+    expect(relationships.length)
+        .toBe(carModelAmount)
+})
