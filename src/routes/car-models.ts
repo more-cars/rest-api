b3e932d54552727affe925287b870908f4a19320
Dennis Gerike <info@dennis-gerike.de>
2025-06-14 17:20:17 +0200

    MCA-189 implementing "get car model belongs to brand relationship" on presentation layer

--- src/routes/car-models.ts
+++ src/routes/car-models.ts
@@ -3,6 +3,7 @@ import {create} from "./controllers/carModels/create"
 import {getById} from "./controllers/carModels/getById"
 import {getAll} from "./controllers/carModels/getAll"
 import {createBelongsToBrandRelation} from "./controllers/carModels/createBelongsToBrandRelation"
+import {getBelongsToBrandRelation} from "./controllers/carModels/getBelongsToBrandRelation"
 
 const router = express.Router()
 
@@ -10,5 +11,6 @@ router.post('/car-models', create)
 router.get('/car-models/:id', getById)
 router.get('/car-models', getAll)
 router.post('/car-models/:carModelId/belongs-to-brand/:brandId', createBelongsToBrandRelation)
+router.get('/car-models/:carModelId/belongs-to-brand', getBelongsToBrandRelation)
 
 export default router
---
+++ src/routes/controllers/carModels/getBelongsToBrandRelation.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import {CarModel} from "../../../models/car-models/CarModel"
+import {marshalRelationship} from "./marshalRelationship"
+
+export async function getBelongsToBrandRelation(req: express.Request, res: express.Response) {
+    const carModelId = parseInt(req.params.carModelId)
+
+    try {
+        const relationship = await CarModel.getRelationshipForBelongsToBrand(carModelId)
+
+        res.status(200)
+        res.set('Content-Type', 'application/json')
+        res.send(marshalRelationship(relationship))
+    } catch (e) {
+        console.error(e)
+        res.status(404)
+        res.set('Content-Type', 'text/plain')
+        res.send('Request failed. Did you provide a valid "Car Model" ID?')
+    }
+}
