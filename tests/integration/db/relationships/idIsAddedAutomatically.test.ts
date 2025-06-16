1430fb05b4150de26ef1c185725314691a4facd3
Dennis Gerike <info@dennis-gerike.de>
2025-06-16 11:31:40 +0200

    MCA-103 checking that relationships automatically get an ID when created

---
+++ tests/integration/db/relationships/idIsAddedAutomatically.test.ts
@@ -0,0 +1,22 @@
+import {createRelationship} from "../../../../src/db/createRelationship"
+import {DbRelationship} from "../../../../src/types/DbRelationship"
+import assert from "assert"
+import FakeBrand from "../../../fixtures/nodes/FakeBrand"
+import FakeCarModel from "../../../fixtures/nodes/FakeCarModel"
+import {createNode as createBrandNode} from "../../../../src/db/brands/createNode"
+import {createNode as createCarModelNode} from "../../../../src/db/car-models/createNode"
+
+test('ID is added when creating a relationship', async () => {
+    const brand = await createBrandNode(FakeBrand)
+    const carModel = await createCarModelNode(FakeCarModel)
+    const relationship = await createRelationship(brand.id, carModel.id, DbRelationship.BrandHasCarModel)
+
+    if (!relationship) {
+        assert.fail('Relationship creation failed')
+    }
+
+    expect(relationship.relationship_id)
+        .toBeGreaterThan(0)
+    expect(relationship.relationship_id)
+        .toBeLessThan(4294967296)
+})
---
+++ tests/integration/models/relationships/idIsAddedAutomatically.test.ts
@@ -0,0 +1,21 @@
+import {createNode as createBrandNode} from "../../../../src/db/brands/createNode"
+import {createNode as createCarModelNode} from "../../../../src/db/car-models/createNode"
+import assert from "assert"
+import FakeBrand from "../../../fixtures/nodes/FakeBrand"
+import FakeCarModel from "../../../fixtures/nodes/FakeCarModel"
+import {Brand} from "../../../../src/models/brands/Brand"
+
+test('ID is added when creating a relationship', async () => {
+    const brand = await createBrandNode(FakeBrand)
+    const carModel = await createCarModelNode(FakeCarModel)
+    const relationship = await Brand.createHasCarModelRelationship(brand, carModel)
+
+    if (!relationship) {
+        assert.fail('Relationship creation failed')
+    }
+
+    expect(relationship.relationship_id)
+        .toBeGreaterThan(0)
+    expect(relationship.relationship_id)
+        .toBeLessThan(4294967296)
+})
