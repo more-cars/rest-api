9d351199bad23b5118600b23c9a26ebe7fcde9ca
Dennis Gerike <info@dennis-gerike.de>
2025-06-20 10:35:49 +0200

    adjusting/fixing the marshalling function: undefined case cannot happen anymore, wrong input type

--- src/controllers/brands/marshal.ts
+++ src/controllers/brands/marshal.ts
@@ -1,21 +1,22 @@
 import {CreateBrandResponse} from "./types/CreateBrandResponse"
+import {BrandNode} from "../../models/brands/types/BrandNode"
 
 /**
- * Based on the given "brand" node this creates a response object
+ * Based on the given BRAND node this creates a response object
  * which conform to the API specification.
  */
-export function marshal(brand: CreateBrandResponse) {
-    const responseBody: CreateBrandResponse = {
+export function marshal(brand: BrandNode) {
+    const marshalledData: CreateBrandResponse = {
         id: brand.id,
         name: brand.name,
-        full_name: brand.full_name ?? null,
-        founded: brand.founded ?? null,
-        defunct: brand.defunct ?? null,
-        wmi: brand.wmi ?? null,
-        hsn: brand.hsn ?? null,
+        full_name: brand.full_name,
+        founded: brand.founded,
+        defunct: brand.defunct,
+        wmi: brand.wmi,
+        hsn: brand.hsn,
         created_at: brand.created_at,
         updated_at: brand.updated_at,
     }
 
-    return responseBody
+    return marshalledData
 }
