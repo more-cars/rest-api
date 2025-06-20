8d40f7b0edd180154f63a98a15a0e8d18b6dd8dd
Dennis Gerike <info@dennis-gerike.de>
2025-06-20 10:32:55 +0200

    adding unit tests for validating the data for a "create brand" request

---
+++ tests/unit/controllers/brands/create/validation/validatingCompleteAndValidInput.test.ts
@@ -0,0 +1,20 @@
+import {validate} from "../../../../../../src/controllers/brands/create"
+
+/**
+ * @group happyPath
+ */
+test('validating a complete and valid request', async () => {
+    const data: any = {
+        name: "BMW",
+        full_name: "Bayerische Motoren Werke",
+        founded: 1916,
+        defunct: 2222,
+        wmi: "WBA",
+        hsn: "0005",
+    }
+
+    const result = validate(data)
+
+    expect(result)
+        .toBeTruthy()
+})
