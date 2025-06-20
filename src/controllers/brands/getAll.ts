38d98a877f5aaad47e50f0ade05fc3cf6d9feec3
Dennis Gerike <info@dennis-gerike.de>
2025-06-20 11:40:49 +0200

    refactoring the "get all brands" controller (marshalling, typing, separating response code)

--- src/controllers/brands/getAll.ts
+++ src/controllers/brands/getAll.ts
@@ -1,11 +1,17 @@
 import express from "express"
 import {Brand} from "../../models/brands/Brand"
 import {marshalAll} from "./marshalAll"
+import {BrandResponse} from "./types/BrandResponse"
 
 export async function getAll(req: express.Request, res: express.Response) {
-    const foundNodes = await Brand.findAll()
+    const nodes = await Brand.findAll()
+    const marshalledData = marshalAll(nodes)
 
+    send200response(marshalledData, res)
+}
+
+function send200response(data: Array<BrandResponse>, res: express.Response) {
     res.status(200)
     res.set('Content-Type', 'application/json')
-    res.send(marshalAll(foundNodes))
+    res.send(data)
 }
--- src/controllers/brands/marshalAll.ts
+++ src/controllers/brands/marshalAll.ts
@@ -1,10 +1,11 @@
 import {BrandNode} from "../../models/brands/types/BrandNode"
 import {marshal} from "./marshal"
+import {BrandResponse} from "./types/BrandResponse"
 
 /**
  * Creates a valid response object from the given collection of brands.
  */
-export function marshalAll(brands: Array<BrandNode>) {
+export function marshalAll(brands: Array<BrandNode>): Array<BrandResponse> {
     const responseObjects: any[] = []
 
     brands.forEach((brand: BrandNode) => {
