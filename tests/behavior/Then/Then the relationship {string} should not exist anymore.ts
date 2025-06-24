f643841f69698f11cefc74cda5b8de0f97752fe2
Dennis Gerike <info@dennis-gerike.de>
2025-06-24 17:06:58 +0200

    MCA-63, MCA-185 implementing the final test step

--- tests/behavior/Then/Then the relationship {string} should not exist anymore.ts
+++ tests/behavior/Then/Then the relationship {string} should not exist anymore.ts
@@ -1,23 +1,24 @@
 import {Then} from "@cucumber/cucumber"
-// import {getNodeById} from "../../../src/db/car-models/getNodeById"
-// import axios from "axios"
-// import assert from "assert"
+import axios from "axios"
+import assert from "assert"
 
-// TODO on hold until it is possible to request relationships via the API
 Then('the relationship {string} should not exist anymore',
     async function (relationshipLabel: string) {
-        // const brand = await getNodeById(this.relationship[relationshipLabel].start_node_id)
-        // const carModel = await getNodeById(this.relationship[relationshipLabel].end_node_id)
-        //
-        // if (!brand || !carModel) {
-        //     assert.ok(false)
-        // }
-        //
-        // const response = await axios
-        //     .get(`${process.env.API_URL}/car-models/${carModel.id}/belongs-to-brand/${brand.id}`)
-        //     .catch(error => {
-        //         console.error(error)
-        //     })
-        //
-        // assert.equal(response.status, 404)
+        const rel = this.relationship[relationshipLabel]
+
+        const response = await axios
+            .get(`${process.env.API_URL}/car-models/${rel.car_model_id}/belongs-to-brand/${rel.brand_id}`, {
+                validateStatus: function (status) {
+                    return status === 404 // treating the 404 as a "good" status code, so axios does not fail the request
+                }
+            })
+            .catch(error => {
+                console.error(error)
+            })
+
+        if (!response) {
+            assert.fail('Request failed')
+        }
+
+        assert.equal(response.status, 404)
     })
