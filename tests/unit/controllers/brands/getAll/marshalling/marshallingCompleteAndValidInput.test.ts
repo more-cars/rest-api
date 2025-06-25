62763627c4833f1a9ec624440de28f4ed08485e2
Dennis Gerike <info@dennis-gerike.de>
2025-06-25 13:18:43 +0200

    adding missing unit tests for marshalling and mapping

---
+++ tests/unit/controllers/brands/getAll/marshalling/marshallingCompleteAndValidInput.test.ts
@@ -0,0 +1,82 @@
+import {BrandNode} from "../../../../../../src/models/brands/types/BrandNode"
+import {marshalAll} from "../../../../../../src/controllers/brands/marshalAll"
+
+/**
+ * @group happyPath
+ */
+test('marshalling a complete and valid request', async () => {
+    const nodes: Array<BrandNode> = [
+        {
+            id: 1,
+            created_at: "2025-05-14T11:05:07.793Z",
+            updated_at: "2025-05-14T11:05:07.793Z",
+            name: "Porsche",
+            full_name: null,
+            founded: 1931,
+            defunct: null,
+            wmi: "WP0",
+            hsn: "0583",
+        },
+        {
+            id: 2,
+            created_at: "2025-05-14T11:05:07.793Z",
+            updated_at: "2025-05-14T11:05:07.793Z",
+            name: "Oldsmobile",
+            full_name: null,
+            founded: 1897,
+            defunct: 2004,
+            wmi: "2G3",
+            hsn: null,
+        },
+        {
+            id: 3,
+            created_at: "2025-05-14T11:05:07.793Z",
+            updated_at: "2025-05-14T11:05:07.793Z",
+            name: "BMW",
+            full_name: "Bayerische Motoren Werke",
+            founded: 1916,
+            defunct: null,
+            wmi: "WBA",
+            hsn: "0005",
+        },
+    ]
+
+    const mappedNodes = marshalAll(nodes)
+
+    expect(mappedNodes)
+        .toStrictEqual([
+            {
+                id: 1,
+                created_at: "2025-05-14T11:05:07.793Z",
+                updated_at: "2025-05-14T11:05:07.793Z",
+                name: "Porsche",
+                full_name: null,
+                founded: 1931,
+                defunct: null,
+                wmi: "WP0",
+                hsn: "0583",
+            },
+            {
+                id: 2,
+                created_at: "2025-05-14T11:05:07.793Z",
+                updated_at: "2025-05-14T11:05:07.793Z",
+                name: "Oldsmobile",
+                full_name: null,
+                founded: 1897,
+                defunct: 2004,
+                wmi: "2G3",
+                hsn: null,
+            },
+            {
+                id: 3,
+                created_at: "2025-05-14T11:05:07.793Z",
+                updated_at: "2025-05-14T11:05:07.793Z",
+                name: "BMW",
+                full_name: "Bayerische Motoren Werke",
+                founded: 1916,
+                defunct: null,
+                wmi: "WBA",
+                hsn: "0005",
+            },
+        ])
+})
---
+++ tests/unit/db/nodes/mappingDatabaseNodeToMoreCarsNode.test.ts
@@ -0,0 +1,24 @@
+import {Integer, Node} from "neo4j-driver"
+import {mapDbNodeToModelNode} from "../../../../src/db/nodes/mapDbNodeToModelNode"
+
+test('the Neo4j node is correctly mapped to a More Cars node', async () => {
+    const dbNode: Node = {
+        identity: new Integer,
+        labels: [],
+        properties: {
+            mc_id: 987654,
+            created_at: "2025-05-14T11:05:07.793Z",
+            updated_at: "2025-05-14T11:05:07.793Z",
+        },
+        elementId: ""
+    }
+
+    const mappedNode = mapDbNodeToModelNode(dbNode)
+
+    expect(mappedNode)
+        .toStrictEqual({
+            id: 987654,
+            created_at: "2025-05-14T11:05:07.793Z",
+            updated_at: "2025-05-14T11:05:07.793Z",
+        })
+})
