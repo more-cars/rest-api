c050aac3dfbdee931a104be3b4981910db3c56f0
Dennis Gerike <info@dennis-gerike.de>
2025-06-25 17:05:15 +0200

    adding scripts to run unit and integration tests combined

---
+++ jest.config.ts
@@ -0,0 +1,36 @@
+// Making the variables in the ".env" file available as regular environment variables.
+// Only relevant for local development, because the CI/CD pipeline doesn't use ".env" files.
+// (Command will not fail when the file doesn't exist.)
+require('dotenv').config()
+
+module.exports = {
+    displayName: 'Unit + Integration Tests',
+    preset: 'ts-jest',
+    testMatch: [
+        '<rootDir>/tests/unit/**/*.test.ts',
+        '<rootDir>/tests/integration/**/*.test.ts'
+    ],
+    maxConcurrency: 1,
+    maxWorkers: 1,
+    reporters: [
+        "default",
+        ["<rootDir>/node_modules/jest-html-reporter", {
+            "pageTitle": "Unit + Integration Tests - Report",
+            "outputPath": "./test-reports/unit+integration/report.html",
+        }]
+    ],
+    collectCoverage: false,
+    coverageProvider: "v8",
+    coverageDirectory: '<rootDir>/test-reports/unit+integration/code-coverage',
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    coveragePathIgnorePatterns: [
+        '<rootDir>/src/server.ts',
+        '<rootDir>/src/types',
+    ],
+    coverageThreshold: {
+        global: {
+            statements: 75,
+            branches: 75
+        }
+    }
+}
