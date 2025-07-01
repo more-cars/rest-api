cf90b8dac17d1c8c2fddf7eda94f6eaf5af0859b
Dennis Gerike <info@dennis-gerike.de>
2025-07-01 16:12:17 +0200

    extending migration script for "car models"

--- migration/migrateNodes.ts
+++ migration/migrateNodes.ts
@@ -1,17 +1,24 @@
 import {getDriver} from "../src/db/driver-mc1.ts"
 import {Driver, Node, Session} from "neo4j-driver"
 import {deleteAllBrands} from "../tests/dbSeeding/brands/nodes/deleteAllBrands.ts"
-import {createNode} from "../src/db/nodes/brands/createNode.ts"
+import {deleteAllCarModels} from "../tests/dbSeeding/car-models/nodes/deleteAllCarModels.ts"
+import {createNode as createBrandNode} from "../src/db/nodes/brands/createNode.ts"
+import {createNode as createCarModelNode} from "../src/db/nodes/car-models/createNode.ts"
 import {InputBrandCreate} from "../src/db/nodes/brands/types/InputBrandCreate.ts"
+import {InputCarModelCreate} from "../src/db/nodes/car-models/types/InputCarModelCreate.ts"
+import cliProgress from "cli-progress"
 
 (async function migrate() {
     console.log('Brands')
     await deleteAllBrands()
     await migrateBrands()
+
+    console.log('Car Models')
+    await deleteAllCarModels()
+    await migrateCarModels()
 })()
 
 async function migrateBrands() {
-    const cliProgress = require('cli-progress')
     const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
 
     const mc1driver: Driver = getDriver()
@@ -21,8 +28,8 @@ async function migrateBrands() {
     progress.start(records.length, 0)
     records.forEach(record => {
         const node: Node = record.get('node')
-        const data: InputBrandCreate = convertBrand(node)
-        createNode(data)
+        const data = convertBrand(node)
+        createBrandNode(data)
         progress.increment()
     })
     progress.stop()
@@ -31,7 +38,27 @@ async function migrateBrands() {
     await mc1driver.close()
 }
 
-function convertBrand(node: Node) {
+async function migrateCarModels() {
+    const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+
+    const mc1driver: Driver = getDriver()
+    const session: Session = mc1driver.session()
+
+    const {records} = await mc1driver.executeQuery(getNodeQuery('carmodel'))
+    progress.start(records.length, 0)
+    records.forEach(record => {
+        const node: Node = record.get('node')
+        const data = convertCarModel(node)
+        createCarModelNode(data)
+        progress.increment()
+    })
+    progress.stop()
+
+    await session.close()
+    await mc1driver.close()
+}
+
+function convertBrand(node: Node): InputBrandCreate {
     return {
         name: node.properties.name,
         full_name: node.properties.full_name,
@@ -39,6 +66,17 @@ function convertBrand(node: Node) {
         defunct: node.properties.defunct?.low,
         wmi: node.properties.wmi,
         hsn: node.properties.hsn,
+    }
+}
+
+function convertCarModel(node: Node): InputCarModelCreate {
+    return {
+        name: node.properties.name,
+        built_from: node.properties.built_from,
+        built_to: node.properties.built_to,
+        generation: node.properties.generation_number,
+        internal_code: node.properties.generation,
+        total_production: node.properties.total_production,
     }
 }
 
