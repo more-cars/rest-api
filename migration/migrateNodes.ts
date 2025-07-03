93c3e5231fa6ad194f3c4929dc8a9d67e46688b7
Dennis Gerike <info@dennis-gerike.de>
2025-07-03 09:45:57 +0200

    stabilizing the migration script and re-integrating the progress bar

--- migration/migrateNodes.ts
+++ migration/migrateNodes.ts
@@ -9,6 +9,7 @@ import {mapImage} from "./nodes/mapImage.ts"
 import {createNode as createBrandNode} from "../src/db/nodes/brands/createNode.ts"
 import {createNode as createCarModelNode} from "../src/db/nodes/car-models/createNode.ts"
 import {createNode as createImageNode} from "../src/db/nodes/images/createNode.ts"
+import cliProgress from "cli-progress"
 
 (async function migrate() {
     await deleteAllBrands()
@@ -21,30 +22,55 @@ import {createNode as createImageNode} from "../src/db/nodes/images/createNode.t
 })()
 
 async function migrateNodes(nodeType: string, mapFunc: any, createFunc: any) {
-    const mc1driver = getDriver()
-    const session = mc1driver.session()
-    let counter = 0
+    const driver = getDriver()
+    const session = driver.session()
+
+    let finished = false
+    const progress = new cliProgress.SingleBar({
+        format: `${nodeType} {bar} {percentage}% | ETA: {eta}s | {value}/{total}`
+    }, cliProgress.Presets.shades_classic)
+    progress.start(300, 0)
 
     session
         .run(getNodeQuery(nodeType))
         .subscribe({
             onNext: record => {
+                waitSync(25)
                 const node: Node = record.get('node')
                 const data = mapFunc(node)
+                progress.increment()
                 createFunc(data)
-                counter++
             },
             onCompleted: () => {
-                console.log(`${counter} ${nodeType}s migrated`)
                 session.close()
-                mc1driver.close()
+                driver.close()
+                progress.stop()
+                finished = true
             },
             onError: error => {
                 console.log(error)
             }
         })
+
+    while (!finished) {
+        await asyncWait(500)
+    }
+
+    return
 }
 
 function getNodeQuery(nodeType: string) {
     return `MATCH (node:${nodeType}) RETURN node ORDER BY id(node) LIMIT 300`
+}
+
+function waitSync(ms: number) {
+    const start = Date.now()
+    let now = start
+    while (now - start < ms) {
+        now = Date.now()
+    }
+}
+
+async function asyncWait(ms: number) {
+    await new Promise(resolve => setTimeout(resolve, ms))
 }
