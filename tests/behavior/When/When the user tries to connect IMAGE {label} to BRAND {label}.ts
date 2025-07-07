e615bfa5fdfa70859521bb701cbaf2bd6a8e3985
Dennis Gerike <info@dennis-gerike.de>
2025-07-07 17:43:50 +0200

    MCA-274 implementing the Gherkin scenario "Trying to connect non-existent nodes

---
+++ tests/behavior/When/When the user tries to connect IMAGE {label} to BRAND {label}.ts
@@ -0,0 +1,20 @@
+import {When} from "@cucumber/cucumber"
+import axios from "axios"
+import {BrandNode} from "../../../src/models/brands/types/BrandNode"
+import {ImageNode} from "../../../src/models/images/types/ImageNode.ts"
+
+When('the user tries to connect IMAGE {string} to BRAND {string}',
+    async function (imageLabel: string, brandLabel: string) {
+        const brand: BrandNode = this.brand[brandLabel]
+        const image: ImageNode = this.image[imageLabel]
+
+        this.latestResponse = await axios
+            .post(`${process.env.API_URL}/brands/${brand.id}/has-image/${image.id}`, null, {
+                validateStatus: function (status) {
+                    return status === 404 // treating the 404 as a "good" status code, so axios does not fail the request
+                }
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    })
