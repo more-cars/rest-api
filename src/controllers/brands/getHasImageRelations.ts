6d5bd99aa232d89afbf05a29e8cf49012c4bd61e
Dennis Gerike <info@dennis-gerike.de>
2025-07-08 11:36:14 +0200

    MCA-242 enabling "get brand-has-image relationships" for the api user

---
+++ src/controllers/brands/getHasImageRelations.ts
@@ -0,0 +1,30 @@
+import express from "express"
+import {Brand} from "../../models/brands/Brand"
+import {BrandHasImageResponse} from "./types/BrandHasImageResponse.ts"
+import {marshalHasImageRelationships} from "./marshalling/marshalHasImageRelationships.ts"
+
+export async function getHasImageRelations(req: express.Request, res: express.Response) {
+    const brandId = parseInt(req.params.brandId)
+
+    try {
+        const relationships = await Brand.getRelationshipsForHasImage(brandId)
+        const marshalledRelationships = marshalHasImageRelationships(relationships)
+
+        send200response(marshalledRelationships, res)
+    } catch (e) {
+        console.error(e)
+        send404response(res)
+    }
+}
+
+function send200response(data: Array<BrandHasImageResponse>, res: express.Response) {
+    res.status(200)
+    res.set('Content-Type', 'application/json')
+    res.send(data)
+}
+
+function send404response(res: express.Response) {
+    res.status(404)
+    res.set('Content-Type', 'text/plain')
+    res.send('Request failed. Did you provide a valid "Brand" ID?')
+}
--- src/routes/brands.ts
+++ src/routes/brands.ts
@@ -5,6 +5,7 @@ import {getAll} from "../controllers/brands/getAll"
 import {createHasCarModelRelation} from "../controllers/brands/createHasCarModelRelation"
 import {getHasCarModelRelations} from "../controllers/brands/getHasCarModelRelations"
 import {createHasImageRelation} from "../controllers/brands/createHasImageRelation.ts"
+import {getHasImageRelations} from "../controllers/brands/getHasImageRelations.ts"
 
 const router = express.Router()
 
@@ -14,5 +15,6 @@ router.get('/brands', getAll)
 router.post('/brands/:brandId/has-car-model/:carModelId', createHasCarModelRelation)
 router.get('/brands/:brandId/has-car-model', getHasCarModelRelations)
 router.post('/brands/:brandId/has-image/:imageId', createHasImageRelation)
+router.get('/brands/:brandId/has-image', getHasImageRelations)
 
 export default router
