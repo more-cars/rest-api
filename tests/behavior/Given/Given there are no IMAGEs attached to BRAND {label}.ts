64da0f8924b1649bcdd61ce9b43fee8410fb04f9
Dennis Gerike <info@dennis-gerike.de>
2025-07-08 12:09:45 +0200

    MCA-281 implementing the Gherkin scenario "Requesting the attached IMAGEs when there are none"

---
+++ tests/behavior/Given/Given there are no IMAGEs attached to BRAND {label}.ts
@@ -0,0 +1,6 @@
+import {Given} from "@cucumber/cucumber"
+
+Given('there are no IMAGEs attached to BRAND {string}',
+    async function (label: string) {
+        // TODO delete relationship if it exists
+    })
