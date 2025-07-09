cf06f92b4a19010850b152f6f4626f839fbdef9e
Dennis Gerike <info@dennis-gerike.de>
2025-07-09 11:56:14 +0200

    making the database persistent in the kubernetes cluster (local db is still ephemeral)

--- deployment/minikube-app-deploy.sh
+++ deployment/minikube-app-deploy.sh
@@ -14,6 +14,7 @@ kubectl apply -f $SCRIPT_PATH/namespace.yaml
 kubectl config set-context --current --namespace=$1
 
 # creating the database
+kubectl apply -f $SCRIPT_PATH/db-pvc.yaml
 kubectl apply -f $SCRIPT_PATH/db-deployment.yaml
 kubectl apply -f $SCRIPT_PATH/db-service.yaml
 
