49e6e51910514956db01637bbdd653bfb70e8ab8
Dennis Gerike <info@dennis-gerike.de>
2025-07-15 15:39:51 +0200

    MCA-256 implementing the Gherkin scenario "Connecting multiple IMAGEs to a CAR MODEL"

---
+++ tests/behavior/Then/Then the IMAGE {label} should be connected to the CAR MODEL {label}.ts
@@ -0,0 +1,23 @@
+import {Then} from "@cucumber/cucumber"
+import assert from "assert"
+import axios from "axios"
+import {ImageNode} from "../../../src/models/images/types/ImageNode.ts"
+import {CarModelNode} from "../../../src/models/car-models/types/CarModelNode.ts"
+
+Then('the IMAGE {string} should be connected to the CAR MODEL {string}',
+    async function (imageLabel: string, carModelLabel: string) {
+        const image: ImageNode = this.image[imageLabel]
+        const carModel: CarModelNode = this.carmodel[carModelLabel]
+
+        const response = await axios
+            .get(`${process.env.API_URL}/car-models/${carModel.id}/has-image`)
+            .catch(error => {
+                console.error(error)
+            })
+
+        if (!response) {
+            assert.fail('Failed to fetch the car model belongs to brand relationship.')
+        }
+
+        assert(response.data.image_id === image.id)
+    })
