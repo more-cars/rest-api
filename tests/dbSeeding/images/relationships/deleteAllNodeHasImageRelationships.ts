f3ebe8a1b4c9a9f3b2290ba14a20fbb65ed6f9b7
Dennis Gerike <info@dennis-gerike.de>
2025-07-15 12:37:42 +0200

    adding function to delete all existing "Node has Image" relationships

---
+++ tests/dbSeeding/images/relationships/deleteAllNodeHasImageRelationships.ts
@@ -0,0 +1,17 @@
+import neo4j, {Driver, Session} from "neo4j-driver"
+import {getDriver} from "../../../../src/db/driver.ts"
+
+export async function deleteAllNodeHasImageRelationships() {
+    const driver: Driver = getDriver()
+    const session: Session = driver.session({defaultAccessMode: neo4j.session.WRITE})
+
+    await session.executeWrite(async txc => {
+        await txc.run(`
+            MATCH (a)-[rel:HAS_IMAGE]->(b:Image)
+            DELETE rel
+        `)
+    })
+
+    await session.close()
+    await driver.close()
+}
