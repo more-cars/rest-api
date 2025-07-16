d7df9708a41e61f3fdae3d6383e249bf5eb43cf8
Dennis Gerike <info@dennis-gerike.de>
2025-07-16 10:37:43 +0200

    MCA-241 adding performance test

---
+++ tests/performance/_testdata/createCarModelHasImageRelationships.ts
@@ -0,0 +1,10 @@
+import http from "k6/http"
+import {createImage} from "./createImage.ts"
+
+export function createCarModelHasImageRelationships(carModelId: number, amount: number) {
+    for (let i = 0; i < amount; i++) {
+        const imageId = createImage()
+        const url = `${__ENV.API_URL}/car-models/${carModelId}/has-image/${imageId}`
+        http.post(url)
+    }
+}
---
+++ tests/performance/car-models/get-has-image-relationships.ts
@@ -0,0 +1,50 @@
+import http from 'k6/http'
+import {check} from "k6"
+import {Trend} from "k6/metrics"
+import {createCarModel} from "../_testdata/createCarModel.ts"
+import {createCarModelHasImageRelationships} from "../_testdata/createCarModelHasImageRelationships.ts"
+
+const trendDuration = new Trend('duration', true)
+
+export const options = {
+    summaryTrendStats: ['count', 'min', 'p(1)', 'p(90)', 'p(95)', 'p(98)'],
+    thresholds: {
+        http_req_failed: ['rate<=0.0'],
+        duration: ['p(1)<=10', 'p(90)<=40', 'p(95)<=100', 'p(98)<=500'],
+    },
+    scenarios: {
+        getHasImageRelationships: {
+            executor: 'constant-arrival-rate',
+            duration: '5m',
+            rate: 1,
+            timeUnit: '1s',
+            preAllocatedVUs: 5,
+            maxVUs: 5,
+            gracefulStop: '10s',
+        }
+    }
+}
+
+export function setup() {
+    const carModelId = createCarModel()
+    createCarModelHasImageRelationships(carModelId, 9)
+
+    return {
+        carModelId
+    }
+}
+
+export default function (data: { carModelId: number }) {
+    const url = `${__ENV.API_URL}/car-models/${data.carModelId}/has-image`
+
+    const response = http.get(url)
+
+    check(response, {
+        'returns with status code 200': (r) => r.status === 200,
+        'content-type is JSON': (r) => r.headers['Content-Type'].includes('application/json'),
+        // @ts-expect-error TS2531
+        'response is an Array': (r) => r.json().length > 0,
+    })
+
+    trendDuration.add(response.timings.duration)
+}
