a26f1bd9e5cb8c5d78d3ab5412c6884c8a1034f5
Dennis Gerike <info@dennis-gerike.de>
2025-08-05 18:26:25 +0200

    deleting dead code

--- src/db/relationships/deleteRelationshipsByNodeAndType.ts
+++
@@ -1,9 +0,0 @@
-import {getCypherQueryTemplate} from "../getCypherQueryTemplate"
-import {DbRelationship} from "../types/DbRelationship"
-
-export function deleteRelationshipByNodeAndNameQuery(nodeId: number, relationshipName: DbRelationship) {
-    return getCypherQueryTemplate('relationships/_cypher/deleteRelationshipByNodeAndType.cypher')
-        .trim()
-        .replace('$nodeId', nodeId.toString())
-        .replace('relationshipName', relationshipName)
-}
