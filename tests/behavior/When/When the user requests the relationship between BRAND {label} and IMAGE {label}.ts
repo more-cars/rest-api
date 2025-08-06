3cad404dceef059fabf8f7039f3d9d0e74220e6c
Dennis Gerike <info@dennis-gerike.de>
2025-08-06 11:58:18 +0200

    MCA-335 implementing the Gherkin scenario "The relationship between BRAND and IMAGE is returned when it exists"

---
+++ tests/behavior/When/When the user requests the relationship between BRAND {label} and IMAGE {label}.ts
@@ -0,0 +1,16 @@
+import {When} from "@cucumber/cucumber"
+import axios from "axios"
+import type {ImageNode} from "../../../src/models/images/types/ImageNode"
+import type {BrandNode} from "../../../src/models/brands/types/BrandNode"
+
+When('the user requests the relationship between BRAND {string} and IMAGE {string}',
+    async function (brandLabel: string, imageLabel: string) {
+        const image: ImageNode = this.image[imageLabel]
+        const brand: BrandNode = this.brand[brandLabel]
+
+        this.latestResponse = await axios
+            .get(`${process.env.API_URL}/brands/${brand.id}/has-image/${image.id}`)
+            .catch(error => {
+                console.error(error)
+            })
+    })
