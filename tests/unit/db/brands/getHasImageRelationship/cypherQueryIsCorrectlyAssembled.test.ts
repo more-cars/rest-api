e4a29ab5d1f8d0058f663917b84dd0a852f78639
Dennis Gerike <info@dennis-gerike.de>
2025-08-07 11:23:24 +0200

    fixing unit tests

--- tests/unit/db/brands/getHasImageRelationship/cypherQueryIsCorrectlyAssembled.test.ts
+++ tests/unit/db/brands/getHasImageRelationship/cypherQueryIsCorrectlyAssembled.test.ts
@@ -1,16 +1,16 @@
 import {expect, test} from 'vitest'
-import {getRelationshipForSpecificNodeQuery} from "../../../../../src/db/relationships/getRelationshipForSpecificNode"
+import {getSpecificRelationshipQuery} from "../../../../../src/db/relationships/getSpecificRelationship"
 import {DbRelationship} from "../../../../../src/db/types/DbRelationship"
 
 test('cypher query is correctly assembled for "get relationship" request', async () => {
-    const query = getRelationshipForSpecificNodeQuery(
+    const query = getSpecificRelationshipQuery(
         12002001,
         DbRelationship.NodeHasImage,
+        12002002,
     )
 
     expect(query)
         .toEqual(
-            "MATCH (a {mc_id: 12002001})-[r:HAS_IMAGE]-(b)\n" +
-            "RETURN r, b\n" +
-            "  LIMIT 1")
+            "MATCH (a {mc_id: 12002001})-[r:HAS_IMAGE]-(b {mc_id: 12002002})\n" +
+            "RETURN r")
 })
