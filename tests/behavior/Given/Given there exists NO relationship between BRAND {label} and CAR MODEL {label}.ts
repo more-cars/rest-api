8f8deeaa5858763a47b011a2e3e5c6bf8f789f26
Dennis Gerike <info@dennis-gerike.de>
2025-08-07 15:32:15 +0200

    MCA-331 implementing the Gherkin scenario "Requesting a non-existing relationship between BRAND and CAR MODEL"

---
+++ tests/behavior/Given/Given there exists NO relationship between BRAND {label} and CAR MODEL {label}.ts
@@ -0,0 +1,6 @@
+import {Given} from "@cucumber/cucumber"
+
+Given('there exists NO relationship between BRAND {string} and CAR MODEL {string}',
+    async function (brandLabel: string, carModelLabel: string) {
+        // TODO delete relationship if it exists
+    })
