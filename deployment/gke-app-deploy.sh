69213c28e58831ada62d261ef562e13d336de4f5
Dennis Gerike <info@dennis-gerike.de>
2025-08-08 09:56:51 +0200

    simplifying the deployment scripts by using the "kustomize" approach

--- deployment/gke-app-deploy.sh
+++ deployment/gke-app-deploy.sh
@@ -4,7 +4,7 @@
 # to find the kubernetes scripts,
 # so this script can be called from any folder
 SCRIPT=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT")/$1
+SCRIPT_PATH=$(dirname "$SCRIPT")
 
 # connecting to the Google Cloud and selecting the correct cluster
 gcloud auth login
@@ -14,18 +14,9 @@ gcloud container clusters get-credentials more-cars --region=europe-west1-b
 # the previous command already selects the correct context, but only implicitly
 kubectl config use-context gke_more-cars_europe-west1-b_more-cars
 
-# creating a namespace
-kubectl apply -f $SCRIPT_PATH/namespace.yaml
+# making sure we are in the correct namespace
 kubectl config set-context --current --namespace=$1
 
-# creating the database
-kubectl apply -f $SCRIPT_PATH/db-deployment.yaml
-kubectl apply -f $SCRIPT_PATH/db-service.yaml
-
-# creating the app
-kubectl apply -f $SCRIPT_PATH/api-deployment.yaml
-kubectl apply -f $SCRIPT_PATH/api-service.yaml
-
-# creating the Open API specification
-kubectl apply -f $SCRIPT_PATH/openapi-spec-deployment.yaml
-kubectl apply -f $SCRIPT_PATH/openapi-spec-service.yaml
+# letting "kustomize" assemble all snippets into a whole kubernetes configuration
+# and then rolling it out
+kubectl apply -k $SCRIPT_PATH/overlays/$1
--- deployment/minikube-app-deploy.sh
+++ deployment/minikube-app-deploy.sh
@@ -4,24 +4,14 @@
 # to find the kubernetes scripts,
 # so this script can be called from any folder
 SCRIPT=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT")/$1
+SCRIPT_PATH=$(dirname "$SCRIPT")
 
 # making sure we are actually in the Minikube cluster, not in the Google cluster
 kubectl config use-context morecars
 
-# creating a namespace
-kubectl apply -f $SCRIPT_PATH/namespace.yaml
+# making sure we are in the correct namespace
 kubectl config set-context --current --namespace=$1
 
-# creating the database
-kubectl apply -f $SCRIPT_PATH/db-pvc.yaml
-kubectl apply -f $SCRIPT_PATH/db-deployment.yaml
-kubectl apply -f $SCRIPT_PATH/db-service.yaml
-
-# creating the app
-kubectl apply -f $SCRIPT_PATH/api-deployment.yaml
-kubectl apply -f $SCRIPT_PATH/api-service.yaml
-
-# creating the Open API specification
-kubectl apply -f $SCRIPT_PATH/openapi-spec-deployment.yaml
-kubectl apply -f $SCRIPT_PATH/openapi-spec-service.yaml
+# letting "kustomize" assemble all snippets into a whole kubernetes configuration
+# and then rolling it out
+kubectl apply -k $SCRIPT_PATH/overlays/$1
