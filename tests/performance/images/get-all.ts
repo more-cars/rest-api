b9d42da2ef1ff401d6134ec1ef23c9f6279d4b99
Dennis Gerike <info@dennis-gerike.de>
2025-08-11 18:03:58 +0200

    MCA-77 adding performance test for the endpoint "get all Images"

---
+++ tests/performance/images/get-all.ts
@@ -0,0 +1,38 @@
+import http from 'k6/http'
+import {check} from "k6"
+import {Trend} from "k6/metrics"
+
+const trendDuration = new Trend('duration', true)
+
+export const options = {
+    summaryTrendStats: ['count', 'min', 'p(1)', 'p(90)', 'p(95)', 'p(98)'],
+    thresholds: {
+        http_req_failed: ['rate<=0.0'],
+        duration: ['p(1)<=30', 'p(90)<=125', 'p(95)<=150', 'p(98)<=200'],
+    },
+    scenarios: {
+        getAllImages: {
+            executor: 'constant-arrival-rate',
+            duration: '5m',
+            rate: 1,
+            timeUnit: '1s',
+            preAllocatedVUs: 5,
+            maxVUs: 5,
+            gracefulStop: '10s',
+        }
+    }
+}
+
+export default function () {
+    const url = `${__ENV.API_URL}/images`
+    const response = http.get(url)
+
+    check(response, {
+        'returns with status code 200': (r) => r.status === 200,
+        'content-type is JSON': (r) => r.headers['Content-Type'].includes('application/json'),
+        // @ts-expect-error TS2531
+        'response is an Array': (r) => r.json().length > 0,
+    })
+
+    trendDuration.add(response.timings.duration)
+}
