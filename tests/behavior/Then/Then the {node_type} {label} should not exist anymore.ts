0dcc1ef11ad6032b9418a31aa08636779174f1c4
Dennis Gerike <info@dennis-gerike.de>
2025-08-19 17:27:09 +0200

    MCA-361 implementing the Gherkin scenario "Hard deleting an IMAGE"

---
+++ tests/behavior/Then/Then the {node_type} {label} should not exist anymore.ts
@@ -0,0 +1,43 @@
+import {Then} from "@cucumber/cucumber"
+import assert from "assert"
+import axios from "axios"
+import type {BaseNode} from "../../../src/db/types/BaseNode"
+
+Then('the {string} {string} should not exist anymore',
+    async function (nodeType: string, label: string) {
+        let node: BaseNode
+        let path: string
+
+        switch (nodeType.toLowerCase()) {
+            case 'brand':
+                node = this.brand[label]
+                path = 'brands'
+                break
+            case 'car model':
+                node = this.carmodel[label]
+                path = 'car-models'
+                break
+            case 'image':
+                node = this.image[label]
+                path = 'images'
+                break
+            default:
+                return
+        }
+
+        const response = await axios
+            .get(`${process.env.API_URL}/${path}/${node.id}`, {
+                validateStatus: function (status) {
+                    return status === 404 // treating the 404 as a "good" status code, so axios does not fail the request
+                }
+            })
+            .catch(error => {
+                console.error(error)
+            })
+
+        if (!response) {
+            assert.fail('Request failed')
+        }
+
+        assert.equal(response.status, 404)
+    })
---
+++ tests/behavior/When/When the user hard deletes the {node_type} {label}.ts
@@ -0,0 +1,32 @@
+import {When} from "@cucumber/cucumber"
+import axios from "axios"
+import type {BaseNode} from "../../../src/db/types/BaseNode"
+
+When('the user hard deletes the {string} {string}',
+    async function (nodeType: string, label: string) {
+        let node: BaseNode
+        let path: string
+
+        switch (nodeType.toLowerCase()) {
+            case 'brand':
+                node = this.brand[label]
+                path = 'brands'
+                break
+            case 'car model':
+                node = this.carmodel[label]
+                path = 'car-models'
+                break
+            case 'image':
+                node = this.image[label]
+                path = 'images'
+                break
+            default:
+                return
+        }
+
+        this.latestResponse = await axios
+            .delete(`${process.env.API_URL}/${path}/${node.id}`)
+            .catch(error => {
+                console.error(error)
+            })
+    })
