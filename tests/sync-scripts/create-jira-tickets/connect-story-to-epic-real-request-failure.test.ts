dfa6c71415cfa9e22766c252f4fad73dc68f53f1
Dennis Gerike <info@dennis-gerike.de>
2025-08-25 14:39:04 +0200

    connecting the story to an epic

---
+++ tests/sync-scripts/create-jira-tickets/connect-story-to-epic-real-request-failure.test.ts
@@ -0,0 +1,8 @@
+import {expect, test} from "vitest"
+import {connectStoryToEpic} from "../../../specification/sync-scripts/create-jira-tickets/connectStoryToEpic"
+
+test.skip('Connecting a "story" to an "epic" in Jira - real request - failure', async () => {
+    await expect(connectStoryToEpic('TEST-2', 'TEST-1'))
+        .rejects
+        .toThrow(Error)
+})
---
+++ tests/sync-scripts/create-jira-tickets/connect-story-to-epic-real-request-success.test.ts
@@ -0,0 +1,9 @@
+import {expect, test} from "vitest"
+import {connectStoryToEpic} from "../../../specification/sync-scripts/create-jira-tickets/connectStoryToEpic"
+
+test.skip('Connecting a "story" to an "epic" in Jira - real request - success', async () => {
+    const response = await connectStoryToEpic('MCA-434', 'MCA-201')
+
+    expect(response)
+        .toEqual(undefined)
+})
