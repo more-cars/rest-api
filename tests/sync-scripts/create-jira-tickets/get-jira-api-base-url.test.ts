873a2d9d3b7d533944b797b94e28cfe3646fbb93
Dennis Gerike <info@dennis-gerike.de>
2025-08-25 12:28:07 +0200

    assembling the url for the Jira API

---
+++ tests/sync-scripts/create-jira-tickets/get-jira-api-base-url.test.ts
@@ -0,0 +1,26 @@
+import {describe, expect, test, vi} from "vitest"
+import {getJiraApiBaseUrl} from "../../../specification/sync-scripts/create-jira-tickets/getJiraApiBaseUrl"
+
+describe('Get Jira API base url', () => {
+    test('missing Jira Domain', () => {
+        vi.stubEnv('JIRA_DOMAIN', undefined)
+
+        const url = getJiraApiBaseUrl()
+
+        vi.unstubAllEnvs()
+
+        expect(url)
+            .toBe(false)
+    })
+
+    test('valid Jira Domain', () => {
+        vi.stubEnv('JIRA_DOMAIN', 'https://test.atlassian.net')
+
+        const url = getJiraApiBaseUrl()
+
+        vi.unstubAllEnvs()
+
+        expect(url)
+            .toBe('https://test.atlassian.net/rest/api/3/')
+    })
+})
