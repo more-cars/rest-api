c344d55119d9177dffd7bfa1f0d6b59758b4ecdd
Dennis Gerike <info@dennis-gerike.de>
2025-08-25 16:06:44 +0200

    connecting the test to an acceptance criterion

---
+++ tests/sync-scripts/create-jira-tickets/connect-test-to-acceptance-criterion-real-request-failure.test.ts
@@ -0,0 +1,10 @@
+import {expect, test} from "vitest"
+import {
+    connectTestToAcceptanceCriterion
+} from "../../../specification/sync-scripts/create-jira-tickets/connectTestToAcceptanceCriterion"
+
+test.skip('Connecting a "test" to an "acceptance criterion" in Jira - real request - failure', async () => {
+    await expect(connectTestToAcceptanceCriterion('TEST-4', 'TEST-3'))
+        .rejects
+        .toThrow(Error)
+})
---
+++ tests/sync-scripts/create-jira-tickets/connect-test-to-acceptance-criterion-real-request-success.test.ts
@@ -0,0 +1,11 @@
+import {expect, test} from "vitest"
+import {
+    connectTestToAcceptanceCriterion
+} from "../../../specification/sync-scripts/create-jira-tickets/connectTestToAcceptanceCriterion"
+
+test.skip('Connecting a "test" to an "acceptance criterion" in Jira - real request - success', async () => {
+    const response = await connectTestToAcceptanceCriterion('MCA-436', 'MCA-435')
+
+    expect(response)
+        .toEqual(undefined)
+})
