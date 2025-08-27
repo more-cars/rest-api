f2a45133a806d2effe3cada56f4a6c2c816f15d6
Dennis Gerike <info@dennis-gerike.de>
2025-08-27 11:14:50 +0200

    removing inaccurate type hints

--- tests/behavior/Then/Then the response should contain the relationship {label}.ts
+++ tests/behavior/Then/Then the response should contain the relationship {label}.ts
@@ -1,13 +1,12 @@
 import {Then, world} from "@cucumber/cucumber"
 import assert from "assert"
-import {BrandHasCarModelRelationship} from "../../../src/models/brands/types/BrandHasCarModelRelationship"
 
 Then('the response should contain the relationship {string}',
     (label: string) => {
-        const rememberedRelationship: BrandHasCarModelRelationship = world.recallRelationship(label)
+        const rememberedRelationship = world.recallRelationship(label)
         const responseData = world.recallResponse().data
 
-        const found = responseData.find((relationship: BrandHasCarModelRelationship) => {
+        const found = responseData.find((relationship: any) => {
             return JSON.stringify(relationship) === JSON.stringify(rememberedRelationship)
         })
 
