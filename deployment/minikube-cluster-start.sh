99e45f25ff22932110dee913f2d8d559bb9f6ad4
Dennis Gerike <info@dennis-gerike.de>
2025-08-29 15:07:24 +0200

    increasing memory for the minikube cluster

--- deployment/minikube-cluster-start.sh
+++ deployment/minikube-cluster-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-minikube start --memory 4000 --cpus 2 -p morecars
+minikube start --memory 8000 --cpus 2 -p morecars
 minikube addons enable metrics-server -p morecars
 minikube dashboard -p morecars
