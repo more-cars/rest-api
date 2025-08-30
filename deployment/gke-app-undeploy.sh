7ade4f87f53b897d747939d457b12d7ee8baf275
Dennis Gerike <info@dennis-gerike.de>
2025-08-30 19:08:54 +0200

    removing dead code

--- deployment/gke-app-undeploy.sh
+++ deployment/gke-app-undeploy.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# determining the path to this script,
-# to find the kubernetes scripts,
-# so this script can be called from any folder
-SCRIPT=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT")/$1
-
 # making sure we are actually in the Google cluster, not in the Minikube cluster
 kubectl config use-context gke_more-cars_europe-west1-b_more-cars
 
--- deployment/minikube-app-undeploy.sh
+++ deployment/minikube-app-undeploy.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# determining the path to this script,
-# to find the kubernetes scripts,
-# so this script can be called from any folder
-SCRIPT=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT")/$1
-
 # making sure we are actually in the Minikube cluster, not in the Google cluster
 kubectl config use-context morecars
 
