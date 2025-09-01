449f2cf1af864806c70956404f05776847f5a0c6
Dennis Gerike <info@dennis-gerike.de>
2025-09-01 12:14:33 +0200

    deleting obsolete deployment scripts

--- deployment/gke-app-undeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# making sure we are actually in the Google cluster, not in the Minikube cluster
-kubectl config use-context gke_more-cars_europe-west1-b_more-cars
-
-# deleting the namespace, incl. all deployments, services and jobs
-kubectl delete namespace "$1"
--- deployment/minikube-app-undeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# making sure we are actually in the Minikube cluster, not in the Google cluster
-kubectl config use-context morecars
-
-# deleting the namespace, incl. all deployments, services and jobs
-kubectl delete namespace "$1"
