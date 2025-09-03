53189deea7435cc3bb37d69ed4fa1ca160adeeb4
Dennis Gerike <info@dennis-gerike.de>
2025-09-03 16:43:15 +0200

    fixing linting error

--- src/controllers/responses/sendResponse200.ts
+++ src/controllers/responses/sendResponse200.ts
@@ -1,6 +1,6 @@
 import express from "express"
 
-export function sendResponse200(data: any, res: express.Response) {
+export function sendResponse200(data: unknown, res: express.Response) {
     res.status(200)
     res.set('Content-Type', 'application/json')
     res.send(data)
