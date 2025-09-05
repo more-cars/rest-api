e72e0c958b5ea8e07019f3b8d8624f06ccecfa73
Dennis Gerike <info@dennis-gerike.de>
2025-09-05 17:07:15 +0200

    MCA-418 implementing the Gherkin scenario "Requesting a specific prime IMAGE relationship for a CAR MODEL"

---
+++ tests/behavior/When/When the user requests the {rel} relationship between {node_label} and {label}.ts
@@ -0,0 +1,23 @@
+import {When, world} from "@cucumber/cucumber"
+import axios from "axios"
+import {BaseNode} from "../../../src/db/types/BaseNode"
+import type {NodeType} from "../../_toolbox/NodeType"
+import {getBasePathFragmentForNodeType} from "../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
+import {getBasePathFragmentForRelationshipName} from "../../_toolbox/dbSeeding/getBasePathFragmentForRelationshipName"
+
+When('the user requests the {string} relationship between {string} and {string}',
+    async (relationshipName: string, startNodeLabel: string, endNodeLabel: string) => {
+        const startNode: BaseNode = world.recallNode(startNodeLabel).data
+        const startNodeType: NodeType = world.recallNode(startNodeLabel).nodeType
+        const endNode: BaseNode = world.recallNode(endNodeLabel).data
+        const nodePathFragment = getBasePathFragmentForNodeType(startNodeType)
+        const relPathFragment = getBasePathFragmentForRelationshipName(relationshipName)
+
+        const response = await axios
+            .get(`${process.env.API_URL}/${nodePathFragment}/${startNode.id}/${relPathFragment}/${endNode.id}`)
+            .catch(error => {
+                console.error(error)
+            })
+
+        world.rememberResponse(response)
+    })
