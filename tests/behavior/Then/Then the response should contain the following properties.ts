aeca9d1365fc4d5f4836f19327a29511ad4f3aa8
Dennis Gerike <info@dennis-gerike.de>
2025-09-12 18:50:23 +0200

    MCA-547 implementing Gherkin scenario "Expecting all properties to be returned when creating a COMPANY"

---
+++ tests/behavior/Then/Then the response should contain the following properties.ts
@@ -0,0 +1,20 @@
+import {DataTable, Then, world} from "@cucumber/cucumber"
+import assert from "assert"
+
+Then('the response should contain the following properties',
+    (dataTable: DataTable) => {
+        const rows = dataTable.hashes()
+        const responseData = world.recallResponse().data
+
+        rows.forEach((row) => {
+            if (row.datatype === 'string') {
+                assert(responseData[row.key] === row.value, `Property "${row.key}" does not have value "${row.value}"`)
+            } else if (row.datatype === 'number') {
+                assert(responseData[row.key] === (row.value === '' ? null : parseInt(row.value)), `Property "${row.key}" does not have value "${row.value}"`)
+            } else if (row.datatype === 'boolean') {
+                assert(responseData[row.key] === row.value, `Property "${row.key}" does not have value "${row.value}"`)
+            } else {
+                assert.fail(`Datatype information missing for property "${row.key}"`)
+            }
+        })
+    })
