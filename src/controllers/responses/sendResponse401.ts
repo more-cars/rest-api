80daa5892ad90393636e29754956fc212b7c2069
Dennis Gerike <info@dennis-gerike.de>
2025-09-12 18:22:40 +0200

    fixing error message for 401 response

--- src/controllers/responses/sendResponse401.ts
+++ src/controllers/responses/sendResponse401.ts
@@ -3,7 +3,7 @@ import express from "express"
 export function sendResponse401(res: express.Response, next: express.NextFunction) {
     res.status(401)
     res.setHeader('WWW-Authenticate', 'Basic')
-    res.send('Request failed. Provided data was invalid or malformed.')
+    res.send('Request failed. Authentication information is missing.')
 
     return next(new Error('Authentication missing'))
 }
