f616008f978fd095671c60b978f6028f3c3bdfb3
Dennis Gerike <info@dennis-gerike.de>
2025-09-13 15:56:38 +0200

    implementing option to cache the downloaded jira tickets

---
+++ specification/sync-scripts/download-specification/lib/cacheJiraTickets.ts
@@ -0,0 +1,9 @@
+import fs from "fs"
+
+export function cacheJiraTickets(ticketType: 'epic' | 'story' | 'acceptance_criteria' | 'test', ticketCollection: [], path: string = './_temp') {
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path, {recursive: true})
+    }
+
+    fs.writeFileSync(path + '/jira_' + ticketType + '_collection.json', JSON.stringify(ticketCollection, null, 2))
+}
