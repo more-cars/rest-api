3a50f03abe0b9860bb4f23fffdfd43db6fc4bd12
Dennis Gerike <info@dennis-gerike.de>
2025-09-15 11:08:07 +0200

    extending the specification downloader to download the Jira stories

---
+++ specification/sync-scripts/download-specification/lib/schemas/GetStoriesResponseSchema.ts
@@ -0,0 +1,56 @@
+export const GetStoriesResponseSchema = {
+    type: "array",
+    items: {
+        type: "object",
+        properties: {
+            key: {
+                type: "string"
+            },
+            fields: {
+                type: "object",
+                properties: {
+                    summary: {
+                        type: "string",
+                    },
+                    description: {
+                        type: ["object", "null"],
+                    },
+                    customfield_10691: {
+                        type: ["object", "null"],
+                    },
+                    customfield_10732: {
+                        type: ["object", "null"],
+                    },
+                    customfield_10731: {
+                        type: ["string", "null"],
+                    },
+                    customfield_10767: {
+                        type: ["array", "null"],
+                    },
+                    updated: {
+                        type: "string"
+                    },
+                    created: {
+                        type: "string"
+                    },
+                },
+                required: [
+                    "summary",
+                    "description",
+                    "customfield_10691",
+                    "customfield_10732",
+                    "customfield_10731",
+                    "customfield_10767",
+                    "created",
+                    "updated",
+                ],
+                additionalProperties: false,
+            },
+        },
+        required: [
+            "key",
+            "fields",
+        ],
+        additionalProperties: true,
+    },
+}
