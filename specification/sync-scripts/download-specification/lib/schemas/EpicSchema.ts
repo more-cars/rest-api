d5e2d9d20570dd6bc40b6b3309ae2b5cf94a42de
Dennis Gerike <info@dennis-gerike.de>
2025-09-15 12:52:24 +0200

    minor cleanup

--- specification/sync-scripts/download-specification/lib/schemas/EpicSchema.ts
+++ specification/sync-scripts/download-specification/lib/schemas/EpicSchema.ts
@@ -1,11 +1,21 @@
 export const EpicSchema = {
     type: "object",
     properties: {
-        id: {type: "string"},
-        title: {type: "string"},
-        data_structure: {type: ["object", "null"]},
-        updated_at: {type: "string"},
-        created_at: {type: "string"},
+        id: {
+            type: "string"
+        },
+        title: {
+            type: "string"
+        },
+        data_structure: {
+            type: ["object", "null"]
+        },
+        updated_at: {
+            type: "string"
+        },
+        created_at: {
+            type: "string"
+        },
     },
     required: [
         "id",
