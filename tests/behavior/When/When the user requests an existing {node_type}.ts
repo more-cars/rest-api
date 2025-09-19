b672933428717751621b7380e533dfbe5da1fef1
Dennis Gerike <info@dennis-gerike.de>
2025-09-19 11:13:51 +0200

    fixing tests

--- tests/behavior/When/When the user requests an existing {node_type}.ts
+++ tests/behavior/When/When the user requests an existing {node_type}.ts
@@ -1,6 +1,7 @@
 import {When, world} from "@cucumber/cucumber"
 import axios from "axios"
 import {BaseNode} from "../../../src/db/types/BaseNode"
+import {getAllNodesOfType as getAllCompanies} from "../../../src/db/nodes/companies/getAllNodesOfType"
 import {getAllNodesOfType as getAllBrands} from "../../../src/db/nodes/brands/getAllNodesOfType"
 import {getAllNodesOfType as getAllCarModels} from "../../../src/db/nodes/car-models/getAllNodesOfType"
 import {getAllNodesOfType as getAllImages} from "../../../src/db/nodes/images/getAllNodesOfType"
@@ -13,6 +14,9 @@ When('the user requests an existing {string}',
         const path = getBasePathFragmentForNodeType(nodeType.toLowerCase() as NodeType)
 
         switch (nodeType.toLowerCase()) {
+            case 'company':
+                node = (await getAllCompanies())[0]
+                break
             case 'brand':
                 node = (await getAllBrands())[0]
                 break
