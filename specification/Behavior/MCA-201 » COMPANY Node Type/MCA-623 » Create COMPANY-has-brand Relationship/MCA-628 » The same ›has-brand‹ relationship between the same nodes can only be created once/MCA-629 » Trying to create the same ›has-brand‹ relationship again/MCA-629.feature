0d767be915ad811ffeb4c2cf806457295b32505a
Dennis Gerike <info@dennis-gerike.de>
2025-09-26 10:37:13 +0200

    MCA-629 implementing the Gherkin scenario "Trying to create the same ›has-brand‹ relationship again"

---
+++ specification/Behavior/MCA-201 » COMPANY Node Type/MCA-623 » Create COMPANY-has-brand Relationship/MCA-628 » The same ›has-brand‹ relationship between the same nodes can only be created once/MCA-629 » Trying to create the same ›has-brand‹ relationship again/MCA-629.feature
@@ -0,0 +1,14 @@
+@REQ_MCA-623
+Feature: Create COMPANY-has-brand Relationship
+
+  @RULE_MCA-628
+  Rule: The same ›has-brand‹ relationship between the same nodes can only be created once
+
+    @TEST_MCA-629 @implemented
+    Scenario: Trying to create the same ›has-brand‹ relationship again
+      Given there exists a "COMPANY" "Volkswagen AG"
+      And there exists a "BRAND" "VW"
+      And there exists a "has-brand" relationship between "Volkswagen AG" and "VW"
+      When the user creates a "has-brand" relationship between "Volkswagen AG" and "VW"
+      Then the response should return with status code 304
+      And the response should return an empty body
---
+++ tests/behavior/Given/Given there exists a {rel_name} relationship between {node_1_label} and {node_2_label}.ts
@@ -0,0 +1,18 @@
+import {Given, world} from "@cucumber/cucumber"
+import axios from "axios"
+import type {BaseNode} from "../../../src/db/types/BaseNode"
+import {getBasePathFragmentForNodeType} from "../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
+import {dasherize} from "inflection"
+
+Given('there exists a {string} relationship between {string} and {string}',
+    async (relationshipName: string, startNodeLabel: string, endNodeLabel: string) => {
+        const startNode: BaseNode = world.recallNode(startNodeLabel).data
+        const endNode = world.recallNode(endNodeLabel).data
+        const nodePathFragment = getBasePathFragmentForNodeType(world.recallNode(startNodeLabel).nodeType)
+
+        await axios
+            .post(`${process.env.API_URL}/${nodePathFragment}/${startNode.id}/${dasherize(relationshipName)}/${endNode.id}`)
+            .catch(error => {
+                console.error(error)
+            })
+    })
