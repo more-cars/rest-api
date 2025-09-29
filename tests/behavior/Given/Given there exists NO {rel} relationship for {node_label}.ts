5baa5ae704e7169da457abdc21ca10128b31fa71
Dennis Gerike <info@dennis-gerike.de>
2025-09-29 15:20:41 +0200

    MCA-654 implementing the Gherkin scenario "Requesting the ›has-prime-image‹ relationship when there exists none"

---
+++ tests/behavior/Given/Given there exists NO {rel} relationship for {node_label}.ts
@@ -0,0 +1,6 @@
+import {Given} from "@cucumber/cucumber"
+
+Given('there exists NO {string} relationship for {string}',
+    async function (relationshipName: string, startNodeLabel: string) {
+        // TODO delete relationship if it exists
+    })
