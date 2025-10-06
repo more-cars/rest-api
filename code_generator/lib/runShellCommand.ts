e12f73e6e45560609d9f1e064c22c1a71e2d60a0
Dennis Gerike <info@dennis-gerike.de>
2025-10-06 11:06:46 +0200

    directly logging the output of the executed shell command (instead of returning the string to the caller)

--- code_generator/lib/runShellCommand.ts
+++ code_generator/lib/runShellCommand.ts
@@ -1,14 +1,14 @@
 import {promisify} from "util"
 import {exec} from "child_process"
 
-export async function runShellCommand(command: string): Promise<string> {
+export async function runShellCommand(command: string) {
     const execute = promisify(exec)
     try {
         const {stdout, stderr} = await execute(command)
         if (stderr) {
             console.error(`Error: ${stderr}`)
         }
-        return stdout
+        console.log(stdout)
     } catch (error) {
         console.error(`Command failed: ${error}`)
         throw error
