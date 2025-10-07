3ad43a706b9b935b917fc90df92f68f28e76368b
Dennis Gerike <info@dennis-gerike.de>
2025-10-07 17:17:37 +0200

    MCA-730 implementing the Gherkin scenario "Expecting a default collection to be sorted by ID in ascending order"

--- tests/behavior/steps/Then/Then the returned nodes should be sorted {direction} by {property}.ts
+++ tests/behavior/steps/Then/Then the returned nodes should be sorted {direction} by {property}.ts
@@ -5,11 +5,6 @@ Then('the returned nodes should be sorted {string} by {string}',
     (sortDirection: string, property: string) => {
         const responseData = world.recallResponse().data
         const returnedPropertyValues = responseData.map((node: any) => node[property])
-        const expectedPropertyValues = returnedPropertyValues.toSorted()
-
-        console.log(returnedPropertyValues)
-        console.log(expectedPropertyValues)
-        console.log(expectedPropertyValues.toReversed())
 
         if (sortDirection === 'asc') {
             assert.deepEqual(returnedPropertyValues, returnedPropertyValues.toSorted())
