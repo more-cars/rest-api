1e00ffc6af67116bbc502c6eb319e04c26ab5ad2
Dennis Gerike <info@dennis-gerike.de>
2025-10-11 19:15:50 +0200

    updating the final two "get specific relationship" controllers regarding the error handling and status codes

--- src/controllers/brands/getSpecificHasCarModelRelation.ts
+++ src/controllers/brands/getSpecificHasCarModelRelation.ts
@@ -6,6 +6,9 @@ import {marshalRelationship} from "../relationships/marshalRelationship"
 import {sendResponse200} from "../responses/sendResponse200"
 import {sendResponse404} from "../responses/sendResponse404"
 import type {BaseRelationship} from "../relationships/types/BaseRelationship"
+import {NodeNotFoundError} from "../../models/types/NodeNotFoundError"
+import {RelationshipNotFoundError} from "../../models/types/RelationshipNotFoundError"
+import {sendResponse500} from "../responses/sendResponse500"
 
 export async function getSpecificHasCarModelRelation(req: express.Request, res: express.Response) {
     const brandId = parseInt(req.params.brandId)
@@ -13,17 +16,18 @@ export async function getSpecificHasCarModelRelation(req: express.Request, res: 
 
     try {
         const relationship = await Brand.getSpecificHasCarModelRelationship(brandId, carModelId)
-
-        if (!relationship) {
-            return sendResponse404(res)
-        }
-
         const relationshipPartner = await CarModel.findById(relationship.car_model_id)
         const marshalledData = marshalRelationship(relationship as BaseRelationship, relationshipPartner as CarModelNode, 'car model')
 
         return sendResponse200(marshalledData, res)
     } catch (e) {
-        console.error(e)
-        return sendResponse404(res)
+        if (e instanceof NodeNotFoundError) {
+            return sendResponse404(res)
+        } else if (e instanceof RelationshipNotFoundError) {
+            return sendResponse404(res)
+        } else {
+            console.error(e)
+            return sendResponse500(res)
+        }
     }
 }
--- src/controllers/brands/getSpecificHasImageRelation.ts
+++ src/controllers/brands/getSpecificHasImageRelation.ts
@@ -6,6 +6,9 @@ import {marshalRelationship} from "../relationships/marshalRelationship"
 import {sendResponse200} from "../responses/sendResponse200"
 import {sendResponse404} from "../responses/sendResponse404"
 import type {BaseRelationship} from "../relationships/types/BaseRelationship"
+import {NodeNotFoundError} from "../../models/types/NodeNotFoundError"
+import {RelationshipNotFoundError} from "../../models/types/RelationshipNotFoundError"
+import {sendResponse500} from "../responses/sendResponse500"
 
 export async function getSpecificHasImageRelation(req: express.Request, res: express.Response) {
     const brandId = parseInt(req.params.brandId)
@@ -13,17 +16,18 @@ export async function getSpecificHasImageRelation(req: express.Request, res: exp
 
     try {
         const relationship = await Brand.getSpecificHasImageRelationship(brandId, imageId)
-
-        if (!relationship) {
-            return sendResponse404(res)
-        }
-
         const relationshipPartner = await Image.findById(relationship.image_id)
         const marshalledData = marshalRelationship(relationship as BaseRelationship, relationshipPartner as ImageNode, 'image')
 
         return sendResponse200(marshalledData, res)
     } catch (e) {
-        console.error(e)
-        return sendResponse404(res)
+        if (e instanceof NodeNotFoundError) {
+            return sendResponse404(res)
+        } else if (e instanceof RelationshipNotFoundError) {
+            return sendResponse404(res)
+        } else {
+            console.error(e)
+            return sendResponse500(res)
+        }
     }
 }
