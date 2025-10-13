7f8a302a1dab6c712a3b8d4d41274501fb23eabb
Dennis Gerike <info@dennis-gerike.de>
2025-10-13 10:57:04 +0200

    updating all "get sole relationship" integration tests -> uniform naming and code style, fixing tests after error handling changed, updating the code generator accordingly

---
+++ tests/integration/controllers/car-models/relationships/belongs-to-brand/get-sole.test.ts
@@ -0,0 +1,63 @@
+import {describe, expect, test, vi} from 'vitest'
+import request from 'supertest'
+import {app} from '../../../../../../src/app'
+import {CarModel} from "../../../../../../src/models/car-models/CarModel"
+import {Brand} from "../../../../../../src/models/brands/Brand"
+import {NodeNotFoundError} from "../../../../../../src/models/types/NodeNotFoundError"
+import {RelationshipNotFoundError} from "../../../../../../src/models/types/RelationshipNotFoundError"
+
+describe('Requesting the ›belongs-to-brand‹ relationship', () => {
+    test('Providing valid data', async () => {
+        CarModel.getBelongsToBrandRelationship = vi.fn().mockReturnValue({
+            relationship_id: 4,
+            relationship_name: 'belongs-to-brand',
+        })
+
+        Brand.findById = vi.fn().mockReturnValue(null)
+
+        const response = await request(app)
+            .get('/car-models/123/belongs-to-brand')
+
+        expect(response.statusCode)
+            .toBe(200)
+    })
+
+    test('Providing valid data, but no relationships exist', async () => {
+        vi.spyOn(CarModel, 'getBelongsToBrandRelationship')
+            .mockImplementation(async () => {
+                throw new RelationshipNotFoundError('belongs-to-brand', 123)
+            })
+
+        const response = await request(app)
+            .get('/car-models/123/belongs-to-brand')
+
+        expect(response.statusCode)
+            .toBe(200)
+    })
+
+    test('Providing valid data, but the database call randomly fails', async () => {
+        vi.spyOn(CarModel, 'getBelongsToBrandRelationship')
+            .mockImplementation(async () => {
+                throw new Error('Arbitrary error')
+            })
+
+        const response = await request(app)
+            .get('/car-models/123/belongs-to-brand')
+
+        expect(response.statusCode)
+            .toBe(500)
+    })
+
+    test('Providing invalid data', async () => {
+        vi.spyOn(CarModel, 'getBelongsToBrandRelationship')
+            .mockImplementation(async () => {
+                throw new NodeNotFoundError(123)
+            })
+
+        const response = await request(app)
+            .get('/car-models/123/belongs-to-brand')
+
+        expect(response.statusCode)
+            .toBe(404)
+    })
+})
