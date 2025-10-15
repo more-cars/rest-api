bb9abd16403a23e14939380ad35efe34b8c93c11
Dennis Gerike <info@dennis-gerike.de>
2025-10-15 11:07:32 +0200

    deleting obsolete gherkin step implementations

--- tests/behavior/steps/Given/Given there are no IMAGEs attached to BRAND {label}.ts
+++
@@ -1,6 +0,0 @@
-import {Given} from "@cucumber/cucumber"
-
-Given('there are no IMAGEs attached to BRAND {string}',
-    async function (label: string) {
-        // TODO delete relationship if it exists
-    })
