4ff7b4f450ea518c4104d9c42c40b331202b71a1
Dennis Gerike <info@dennis-gerike.de>
2025-10-16 12:19:37 +0200

    quickfix

--- tests/behavior/steps/Then/Then the response should return the relationship {label}.ts
+++ tests/behavior/steps/Then/Then the response should return the relationship {label}.ts
@@ -1,10 +1,9 @@
 import {Then, world} from "@cucumber/cucumber"
 import assert from "assert"
-import {BrandHasCarModelRelationship} from "../../../../src/models/brands/types/BrandHasCarModelRelationship"
 
 Then('the response should return the relationship {string}',
     (label: string) => {
-        const expectedRelationship: BrandHasCarModelRelationship = world.recallRelationship(label)
+        const expectedRelationship = world.recallRelationship(label)
         const actualRelationship = world.recallResponse().data.data
 
         if ('start_node_id' in expectedRelationship) { // relationship was created via db seeder
