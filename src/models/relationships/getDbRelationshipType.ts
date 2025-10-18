e887f02c06b8f784d37a1eacbd0ab92ce87c86de
Dennis Gerike <info@dennis-gerike.de>
2025-10-18 15:57:35 +0200

    adding a mapper for the models to be able to determine the name of a relationship in the database layer

---
+++ src/models/relationships/getDbRelationshipType.ts
@@ -0,0 +1,29 @@
+import {RelationshipType} from "./types/RelationshipType"
+import {DbRelationship} from "../../db/types/DbRelationship"
+import {RelationshipTypeNotFoundError} from "../types/RelationshipTypeNotFoundError"
+
+export function getDbRelationshipType(relationshipType: RelationshipType): DbRelationship {
+    const mapping = new Map<RelationshipType, DbRelationship>([
+        [RelationshipType.CompanyHasBrand, DbRelationship.CompanyHasBrand],
+        [RelationshipType.CompanyHasImage, DbRelationship.CompanyHasImage],
+        [RelationshipType.CompanyHasPrimeImage, DbRelationship.CompanyHasPrimeImage],
+        [RelationshipType.BrandBelongsToCompany, DbRelationship.BrandBelongsToCompany],
+        [RelationshipType.BrandHasCarModel, DbRelationship.BrandHasCarModel],
+        [RelationshipType.BrandHasImage, DbRelationship.BrandHasImage],
+        [RelationshipType.BrandHasPrimeImage, DbRelationship.BrandHasPrimeImage],
+        [RelationshipType.CarModelBelongsToBrand, DbRelationship.CarModelBelongsToBrand],
+        [RelationshipType.CarModelHasSuccessor, DbRelationship.CarModelHasSuccessor],
+        [RelationshipType.CarModelIsSuccessorOf, DbRelationship.CarModelIsSuccessorOf],
+        [RelationshipType.CarModelHasImage, DbRelationship.CarModelHasImage],
+        [RelationshipType.CarModelHasPrimeImage, DbRelationship.CarModelHasPrimeImage],
+        [RelationshipType.ImageBelongsToNode, DbRelationship.ImageBelongsToNode],
+    ])
+
+    const mappedRel = mapping.get(relationshipType)
+
+    if (!mappedRel) {
+        throw new RelationshipTypeNotFoundError(relationshipType)
+    }
+
+    return mappedRel
+}
