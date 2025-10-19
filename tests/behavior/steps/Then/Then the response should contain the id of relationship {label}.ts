ea4187268e6222e9a0b3244d2776c3da57d77ad1
Dennis Gerike <info@dennis-gerike.de>
2025-10-19 17:21:50 +0200

    deleting obsolete relationship types

--- tests/behavior/steps/Then/Then the response should contain the id of relationship {label}.ts
+++ tests/behavior/steps/Then/Then the response should contain the id of relationship {label}.ts
@@ -1,10 +1,9 @@
 import {Then, world} from "@cucumber/cucumber"
 import assert from "assert"
-import {BrandHasCarModelRelationship} from "../../../../src/models/brands/types/BrandHasCarModelRelationship"
 
 Then('the response should contain the id of relationship {string}',
     (label: string) => {
-        const rememberedRelationship: BrandHasCarModelRelationship = world.recallRelationship(label)
+        const rememberedRelationship = world.recallRelationship(label)
 
         assert.equal(
             world.recallResponse().data.data.relationship_id,
