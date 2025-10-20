1aff210cbe7c87ea974e03029e00e43cc13464d4
Dennis Gerike <info@dennis-gerike.de>
2025-10-20 14:54:28 +0200

    minor refactoring

--- tests/_toolbox/fixtures/nodes/FakeNode.ts
+++ tests/_toolbox/fixtures/nodes/FakeNode.ts
@@ -1,4 +1,5 @@
-import type {NodeTypeEnum} from "../../../../src/controllers/nodes/types/NodeTypeEnum"
+import assert from "assert"
+import {NodeTypeEnum} from "../../../../src/controllers/nodes/types/NodeTypeEnum"
 import FakeCompany from "./FakeCompany"
 import FakeBrand from "./FakeBrand"
 import FakeCarModel from "./FakeCarModel"
@@ -7,15 +8,17 @@ import FakeImage from "./FakeImage"
 
 export function FakeNode(nodeType: NodeTypeEnum) {
     switch (nodeType) {
-        case 'company':
+        case NodeTypeEnum.COMPANY:
             return FakeCompany
-        case 'brand':
+        case NodeTypeEnum.BRAND:
             return FakeBrand
-        case 'car model':
+        case NodeTypeEnum.CAR_MODEL:
             return FakeCarModel
-        case 'race track':
+        case NodeTypeEnum.RACE_TRACK:
             return FakeRaceTrack
-        case 'image':
+        case NodeTypeEnum.IMAGE:
             return FakeImage
+        default:
+            assert.fail(`Node type "${nodeType}" is invalid or unknown`)
     }
 }
