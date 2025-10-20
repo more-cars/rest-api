537a9d7eafb9b4f807d91d21cfc179d26858946c
Dennis Gerike <info@dennis-gerike.de>
2025-10-20 10:07:00 +0200

    stabilizing code

--- src/controllers/nodes/marshalSingleNode.ts
+++ src/controllers/nodes/marshalSingleNode.ts
@@ -2,8 +2,9 @@ import type {NodeResponse} from "./types/NodeResponse"
 
 // TODO return only those fields that are defined in the API spec
 export function marshalSingleNode(node: object) {
-    // @ts-expect-error TS2339
-    delete node.mc_id
+    if (node && 'mc_id' in node) {
+        delete node.mc_id
+    }
 
     return {
         data: node,
