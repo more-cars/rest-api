29b3cdecbeabcb23b33b9aed6776a45bd266f451
Dennis Gerike <info@dennis-gerike.de>
2025-10-20 14:04:02 +0200

    fixing behavior tests regarding schema validation

--- tests/behavior/steps/Then/Then the response should return a collection of {amount} {node_type}s.ts
+++ tests/behavior/steps/Then/Then the response should return a collection of {amount} {node_type}s.ts
@@ -1,8 +1,10 @@
 import {Then, world} from "@cucumber/cucumber"
 import assert from "assert"
+import {NodeTypeEnum} from "../../../../src/controllers/nodes/types/NodeTypeEnum"
 import {CompanySchema} from "../../../_toolbox/schemas/CompanySchema"
 import {BrandSchema} from "../../../_toolbox/schemas/BrandSchema"
 import {CarModelSchema} from "../../../_toolbox/schemas/CarModelSchema"
+import {RaceTrackSchema} from "../../../_toolbox/schemas/RaceTrackSchema"
 import {ImageSchema} from "../../../_toolbox/schemas/ImageSchema"
 import {validateJson} from "../../../_toolbox/validateJson"
 
@@ -10,26 +12,28 @@ Then('the response should return a collection of {int} {string}s',
     (amount: number, nodeType: string) => {
         assert.equal(world.recallResponse().data.data.length, amount)
 
-        let validationSchema = {}
-
+        let schema
         switch (nodeType.toLowerCase()) {
-            case 'company':
-                validationSchema = CompanySchema
+            case NodeTypeEnum.COMPANY:
+                schema = CompanySchema
                 break
-            case 'brand':
-                validationSchema = BrandSchema
+            case NodeTypeEnum.BRAND:
+                schema = BrandSchema
                 break
-            case 'car model':
-                validationSchema = CarModelSchema
+            case NodeTypeEnum.CAR_MODEL:
+                schema = CarModelSchema
                 break
-            case 'image':
-                validationSchema = ImageSchema
+            case NodeTypeEnum.IMAGE:
+                schema = ImageSchema
+                break
+            case NodeTypeEnum.RACE_TRACK:
+                schema = RaceTrackSchema
                 break
             default:
                 assert.fail(`Node type "${nodeType}" is invalid or unknown`)
         }
 
         world.recallResponse().data.data.forEach((item: any) => {
-            assert.ok(validateJson(item.data, validationSchema))
+            assert.ok(validateJson(item.data, schema))
         })
     })
