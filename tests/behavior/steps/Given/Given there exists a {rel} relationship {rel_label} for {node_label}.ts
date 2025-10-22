b5cb84bada80bf420f24b46ff6742b3f28f3e1cd
Dennis Gerike <info@dennis-gerike.de>
2025-10-22 11:55:13 +0200

    avoiding the "seedRelationship" helper as long as the reverse relationships are not handled properly

--- tests/behavior/steps/Given/Given there exists a {rel} relationship {rel_label} for {node_label}.ts
+++ tests/behavior/steps/Given/Given there exists a {rel} relationship {rel_label} for {node_label}.ts
@@ -1,15 +1,25 @@
 import {Given, world} from "@cucumber/cucumber"
-import type {NodeTypeEnum} from "../../../../src/controllers/nodes/types/NodeTypeEnum"
-import * as changeCase from "change-case"
-import type {DbRelationship} from "../../../../src/db/types/DbRelationship"
-import {seedRelationshipForNode} from "../../../_toolbox/dbSeeding/seedRelationshipForNode"
+import axios from "axios"
 import type {BaseNode} from "../../../../src/db/types/BaseNode"
+import type {NodeTypeEnum} from "../../../../src/controllers/nodes/types/NodeTypeEnum"
+import {getTargetNodeTypeForRelationship} from "../../../_toolbox/dbSeeding/getTargetNodeTypeForRelationship"
+import {seedNode} from "../../../_toolbox/dbSeeding/seedNode"
+import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
+import {dasherize} from "inflection"
 
 Given('there exists a {string} relationship {string} for {string}',
     async (relationshipName: string, relationshipLabel: string, startNodeLabel: string) => {
         const startNode: BaseNode = world.recallNode(startNodeLabel).data
         const startNodeType: NodeTypeEnum = world.recallNode(startNodeLabel).nodeType
-        const relationship = await seedRelationshipForNode(startNode, startNodeType, changeCase.constantCase(relationshipName) as DbRelationship)
+        const endNodeType = getTargetNodeTypeForRelationship(startNodeType, relationshipName)
+        const endNode = await seedNode(endNodeType)
+        const nodePathFragment = getBasePathFragmentForNodeType(world.recallNode(startNodeLabel).nodeType)
 
-        world.rememberRelationship(relationship, relationshipLabel)
+        const response = await axios
+            .post(`${process.env.API_URL}/${nodePathFragment}/${startNode.id}/${dasherize(relationshipName)}/${endNode.id}`)
+            .catch(error => {
+                console.error(error)
+            })
+
+        world.rememberRelationship(response?.data.data, relationshipLabel)
     })
