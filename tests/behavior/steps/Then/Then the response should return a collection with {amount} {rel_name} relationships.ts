0f483800b95417ba393351837232a6a27f170292
Dennis Gerike <info@dennis-gerike.de>
2025-10-22 10:23:52 +0200

    quickfix

--- tests/behavior/steps/Then/Then the response should return a collection with {amount} {rel_name} relationships.ts
+++ tests/behavior/steps/Then/Then the response should return a collection with {amount} {rel_name} relationships.ts
@@ -1,5 +1,6 @@
 import {Then, world} from "@cucumber/cucumber"
 import assert from "assert"
+import {kebabCase} from "change-case"
 
 Then('the response should return a collection with {int} {string} relationships',
     (amount: number, relationshipName: string) => {
@@ -8,6 +9,6 @@ Then('the response should return a collection with {int} {string} relationships'
         assert.equal(relationships.length, amount)
 
         relationships.forEach((relationship: any) => {
-            assert.equal(relationship.data.relationship_name, relationshipName)
+            assert.equal(relationship.data.relationship_name, kebabCase(relationshipName))
         })
     })
