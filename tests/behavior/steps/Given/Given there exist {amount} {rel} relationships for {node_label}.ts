f121b898b4a85323326da9926276d3c7ce96e996
Dennis Gerike <info@dennis-gerike.de>
2025-10-22 12:32:13 +0200

    avoiding the "seedRelationship" helper as long as the reverse relationships are not handled properly

--- tests/behavior/steps/Given/Given there exist {amount} {rel} relationships for {node_label}.ts
+++ tests/behavior/steps/Given/Given there exist {amount} {rel} relationships for {node_label}.ts
@@ -1,16 +1,26 @@
 import {Given, world} from "@cucumber/cucumber"
 import type {NodeTypeEnum} from "../../../../src/controllers/nodes/types/NodeTypeEnum"
-import * as changeCase from "change-case"
-import type {DbRelationship} from "../../../../src/db/types/DbRelationship"
-import {seedRelationshipForNode} from "../../../_toolbox/dbSeeding/seedRelationshipForNode"
 import type {BaseNode} from "../../../../src/db/types/BaseNode"
+import {getTargetNodeTypeForRelationship} from "../../../_toolbox/dbSeeding/getTargetNodeTypeForRelationship"
+import {seedNode} from "../../../_toolbox/dbSeeding/seedNode"
+import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
+import axios from "axios"
+import {dasherize} from "inflection"
 
 Given('there exist {int} {string} relationships for {string}',
     async (amount: number, relationshipName: string, startNodeLabel: string) => {
         const startNode: BaseNode = world.recallNode(startNodeLabel).data
         const startNodeType: NodeTypeEnum = world.recallNode(startNodeLabel).nodeType
+        const endNodeType = getTargetNodeTypeForRelationship(startNodeType, relationshipName)
+        const nodePathFragment = getBasePathFragmentForNodeType(world.recallNode(startNodeLabel).nodeType)
 
         for (let i = 0; i < amount; i++) {
-            await seedRelationshipForNode(startNode, startNodeType, changeCase.constantCase(relationshipName) as DbRelationship)
+            const endNode = await seedNode(endNodeType)
+
+            await axios
+                .post(`${process.env.API_URL}/${nodePathFragment}/${startNode.id}/${dasherize(relationshipName)}/${endNode.id}`)
+                .catch(error => {
+                    console.error(error)
+                })
         }
     })
