f1d0f10d915144823a53e42435b99c3bb4297472
Dennis Gerike <info@dennis-gerike.de>
2025-10-23 18:33:07 +0200

    adding todo

--- tests/behavior/steps/When/When the user creates a set of {amount} {relationship}s.ts
+++ tests/behavior/steps/When/When the user creates a set of {amount} {relationship}s.ts
@@ -3,5 +3,6 @@ import {seedRelationships} from "../../../_toolbox/dbSeeding/brands/relationship
 
 When('the user creates a set of {int} relationships {string}',
     async (amount: number, label: string) => {
+        // TODO seeding is not allowed in a "When" step -> needs to be a proper API call
         world.rememberRelationship(await seedRelationships(amount), label)
     })
