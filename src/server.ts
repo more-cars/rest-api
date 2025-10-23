08246c04af15257d4390588c75806cab12f47d8e
Dennis Gerike <info@dennis-gerike.de>
2025-10-23 17:36:25 +0200

    MCA-1525 running the API as HTTP and HTTPS server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,32 @@
+import fs from 'fs'
+import http from 'http'
+import https from 'https'
 import {app} from "./app"
 
-const PORT = 3000
+startHttpServer()
+startHttpsServer()
 
-app.listen(PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${PORT}`)
-})
+function startHttpServer() {
+    const HTTP_PORT = 3000
+
+    const httpServer = http.createServer(app)
+    httpServer.listen(HTTP_PORT, () => {
+        console.log(`[HTTP] More Cars API started and running at http://localhost:${HTTP_PORT}`)
+    })
+}
+
+function startHttpsServer() {
+    if (!fs.existsSync(__dirname + '/../deployment/certificates/server.key') || !fs.existsSync(__dirname + '/../deployment/certificates/server.cert')) {
+        console.error(`[HTTPS] More Cars API could not be started. No SSL certificate found.`)
+        return
+    }
+
+    const HTTPS_PORT = 3443
+    const privateKey = fs.readFileSync(__dirname + '/../deployment/certificates/server.key', 'utf8')
+    const certificate = fs.readFileSync(__dirname + '/../deployment/certificates/server.cert', 'utf8')
+
+    const httpsServer = https.createServer({key: privateKey, cert: certificate}, app)
+    httpsServer.listen(HTTPS_PORT, () => {
+        console.log(`[HTTPS] More Cars API started and running at https://localhost:${HTTPS_PORT}`)
+    })
+}
