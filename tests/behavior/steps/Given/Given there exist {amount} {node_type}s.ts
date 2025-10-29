affd29f35fd29b0c99d93929abe084e62a64f3c1
Dennis Gerike <info@dennis-gerike.de>
2025-10-29 13:08:10 +0100

    increasing timeout for "heavy" cucumber test

--- tests/behavior/steps/Given/Given there exist {amount} {node_type}s.ts
+++ tests/behavior/steps/Given/Given there exist {amount} {node_type}s.ts
@@ -3,7 +3,7 @@ import {deleteAllNodesOfType} from "../../../_toolbox/dbSeeding/deleteAllNodesOf
 import {seedNodes} from "../../../_toolbox/dbSeeding/seedNodes"
 import {NodeTypeEnum} from "../../../../src/controllers/nodes/types/NodeTypeEnum"
 
-Given('there exist {int} {string}s', async function (amount: number, nodeType: string) {
+Given('there exist {int} {string}s', {timeout: 30000}, async function (amount: number, nodeType: string) {
     await deleteAllNodesOfType(nodeType.toLowerCase() as NodeTypeEnum)
     await seedNodes(nodeType.toLowerCase() as NodeTypeEnum, amount)
 })
