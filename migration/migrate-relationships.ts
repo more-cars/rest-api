452f53fb521954bf784cbe61894d8044aa729ae2
Dennis Gerike <info@dennis-gerike.de>
2025-11-01 14:07:49 +0100

    quickfix for migrating "Track Layout has LapTime"

--- migration/migrate-relationships.ts
+++ migration/migrate-relationships.ts
@@ -20,7 +20,12 @@ async function migrateRelationshipsOfType() {
     const newEndNodeType = await determineEndNodeType()
     const oldEndNodeType = NodeTypeMapping.get(newEndNodeType) as NodeTypeLabelOld
     const newRelationshipType = await determineRelationshipType()
-    const oldRelationshipType = RelationshipTypeMapping.get(newRelationshipType) as RelationshipTypeLabelOld
+    let oldRelationshipType
+    if (newStartNodeType === 'TrackLayout' && newRelationshipType === 'HAS_LAP_TIME') { // TODO make mapping unique
+        oldRelationshipType = RelationshipTypeLabelOld.TrackLayoutHasLapTime
+    } else {
+        oldRelationshipType = RelationshipTypeMapping.get(newRelationshipType) as RelationshipTypeLabelOld
+    }
 
     const deleteRelationships = await determineDeleteRelationships()
     if (deleteRelationships) {
