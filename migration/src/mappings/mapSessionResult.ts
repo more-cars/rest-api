aaa698cf2390e25fdde0855b4a49abdc81d563ba
Dennis Gerike <info@dennis-gerike.de>
2025-11-01 13:25:24 +0100

    fixing session result migration mapping

--- migration/src/mappings/mapSessionResult.ts
+++ migration/src/mappings/mapSessionResult.ts
@@ -6,10 +6,10 @@ export function mapSessionResult(oldNode: Node): InputSessionResultCreate {
         position: oldNode.properties.position,
         race_number: oldNode.properties.start_number,
         driver_name: oldNode.properties.driver_name,
-        team_name: oldNode.properties.driver_name,
-        race_time: oldNode.properties.date,
-        laps: oldNode.properties.date,
-        status: oldNode.properties.date,
-        points: oldNode.properties.date,
+        team_name: oldNode.properties.team_name,
+        race_time: oldNode.properties.race_time,
+        laps: oldNode.properties.laps,
+        status: oldNode.properties.status,
+        points: oldNode.properties.points,
     }
 }
