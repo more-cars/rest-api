484491fbe429389a3ecbc388b0ec2aa34199fa3c
Dennis Gerike <dennis.gerike+github@gmail.com>
2025-12-21 17:31:58 +0100

    when starting the dev version of the app check that the hostname exists in the environment -> if not, add a warning to the console output

--- deployment/local-app-start.sh
+++ deployment/local-app-start.sh
@@ -15,5 +15,16 @@ if ! [ -f .env ]; then
   echo
 fi
 
+hostname_exists() {
+  if ! getent hosts $1 >/dev/null 2>&1; then
+      RED='\033[0;33m'
+      NC='\033[0m'
+      echo ⚠️ Hostname ${RED}$1${NC} was not found in the environment. Consider running ${RED}npm run local:hostnames:add${NC}.
+  fi
+}
+
+hostname_exists "api.more-cars.internal"
+hostname_exists "db.more-cars.internal"
+
 echo 🟢 Starting application...
 node --watch --env-file=.env -r ts-node/register src/server.ts
