2f4b88e5f0a595d79ca8b318fb7cf57698eacbd8
Dennis Gerike <dennis.gerike+github@gmail.com>
2025-12-24 17:31:31 +0100

    injecting the correct ingress hostnames when deploying the app to a minikube environment

--- deployment/lib/create-patch-file.ts
+++ deployment/lib/create-patch-file.ts
@@ -7,6 +7,13 @@ createPatchFile()
         fs.writeFileSync(path + filename, JSON.stringify(data, null, 2))
     })
 
+createIngressPatchFile()
+    .then((data) => {
+        const path = __dirname + '/../app/'
+        const filename = 'ingress.patch.json'
+        fs.writeFileSync(path + filename, JSON.stringify(data, null, 2))
+    })
+
 async function createPatchFile() {
     const packageName = process.env.PACKAGE_NAME
     const packageVersion = process.env.PACKAGE_VERSION
@@ -16,6 +23,43 @@ async function createPatchFile() {
             "op": "replace",
             "path": "/spec/template/spec/containers/0/image",
             "value": `ghcr.io/more-cars/${packageName}:${packageVersion}`
+        }
+    ]
+}
+
+async function createIngressPatchFile() {
+    const targetEnvironment = process.env.TARGET_ENVIRONMENT
+
+    return [
+        {
+            "op": "replace",
+            "path": "/spec/rules/0/host",
+            "value": targetEnvironment + ".api.more-cars.internal",
+        },
+        {
+            "op": "replace",
+            "path": "/spec/rules/1/host",
+            "value": targetEnvironment + ".swagger.more-cars.internal",
+        },
+        {
+            "op": "replace",
+            "path": "/spec/rules/2/host",
+            "value": targetEnvironment + ".db.more-cars.internal",
+        },
+        {
+            "op": "replace",
+            "path": "/spec/tls/0/hosts/0",
+            "value": targetEnvironment + ".api.more-cars.internal",
+        },
+        {
+            "op": "replace",
+            "path": "/spec/tls/1/hosts/0",
+            "value": targetEnvironment + ".swagger.more-cars.internal",
+        },
+        {
+            "op": "replace",
+            "path": "/spec/tls/2/hosts/0",
+            "value": targetEnvironment + ".db.more-cars.internal",
         }
     ]
 }
