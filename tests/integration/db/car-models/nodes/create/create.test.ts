f4ee43640708bc32ba645d467638e2d14df9aef1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-08 19:31:11 +0100

    uniform naming

--- tests/integration/db/car-models/nodes/create/create.test.ts
+++ tests/integration/db/car-models/nodes/create/create.test.ts
@@ -2,7 +2,7 @@ import {expect, test} from 'vitest'
 import {FakeCarModel} from "../../../../../_toolbox/fixtures/nodes/FakeCarModel"
 import {createNode} from "../../../../../../src/db/nodes/car-models/createNode"
 
-test('When providing valid data the new node can be created', async () => {
+test('Creating node with valid data', async () => {
     const inputData = FakeCarModel.dbInput()
     const createdNode = await createNode(inputData)
 
--- tests/integration/db/images/nodes/create/create.test.ts
+++ tests/integration/db/images/nodes/create/create.test.ts
@@ -2,7 +2,7 @@ import {expect, test} from 'vitest'
 import {createNode} from "../../../../../../src/db/nodes/images/createNode"
 import FakeImageFull from "../../../../../_toolbox/fixtures/nodes/FakeImageFull"
 
-test('When providing valid data the new node can be created', async () => {
+test('Creating node with valid data', async () => {
     const createdNode = await createNode(FakeImageFull)
 
     expect(createdNode)
