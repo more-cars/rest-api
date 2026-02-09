358e577ffdcf4362b6324a8986cfde387a5a2338
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-09 14:47:41 +0100

    adding all Bruno tests for node type COMPANY

--- tests/smoke/bruno/lib/node-types/Companies.js
+++ tests/smoke/bruno/lib/node-types/Companies.js
@@ -9,3 +9,15 @@ exports.create = async function () {
 
     return company
 }
+
+exports.createHasBrandRelationship = async function () {
+    await post("/companies/" + bru.getEnvVar('validCompanyId') + "/has-brand/" + bru.getEnvVar('validBrandId'))
+}
+
+exports.createHasImageRelationship = async function () {
+    await post("/companies/" + bru.getEnvVar('validCompanyId') + "/has-image/" + bru.getEnvVar('validImageId'))
+}
+
+exports.createHasPrimeImageRelationship = async function () {
+    await post("/companies/" + bru.getEnvVar('validCompanyId') + "/has-prime-image/" + bru.getEnvVar('validImageId'))
+}
