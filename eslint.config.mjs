98250a1c04a111aa143edc3f18f5dbe74570fdb3
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-10 12:09:11 +0100

    ignoring the Bruno test code when running the linter (these are all js files, no ts files)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
 
 import eslint from '@eslint/js'
-import {defineConfig} from 'eslint/config'
+import {defineConfig, globalIgnores} from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.strict,
     tseslint.configs.stylistic,
+    globalIgnores(["tests/**/*.js"]),
     {
         rules: {
             "@typescript-eslint/no-duplicate-enum-values": "off",
             "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-        }
+        },
     }
 )
