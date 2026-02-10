55eb7aeb61d4123dd8c00f9d2a7299adf0daaa66
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-10 12:07:18 +0100

    adding all Bruno tests for node type CAR MODEL

--- tests/smoke/bruno/lib/node-types/CarModels.js
+++ tests/smoke/bruno/lib/node-types/CarModels.js
@@ -1,11 +1,35 @@
 const {post} = require("../apiRequest.js")
 
-exports.create = async function () {
+exports.create = async function (prefix = '') {
     const response = await post("/car-models", {
         name: 'Dummy Car Model',
     })
     const carModel = response.data
-    bru.setEnvVar('validCarModelId', carModel.id)
+    bru.setEnvVar('valid' + prefix + 'CarModelId', carModel.id)
 
     return carModel
+}
+
+exports.createBelongsToBrandRelationship = async function () {
+    await post("/car-models/" + bru.getEnvVar('validCarModelId') + "/belongs-to-brand/" + bru.getEnvVar('validBrandId'))
+}
+
+exports.createHasSuccessorRelationship = async function () {
+    await post("/car-models/" + bru.getEnvVar('validCarModelId') + "/has-successor/" + bru.getEnvVar('validSecondCarModelId'))
+}
+
+exports.createIsSuccessorOfRelationship = async function () {
+    await post("/car-models/" + bru.getEnvVar('validCarModelId') + "/is-successor-of/" + bru.getEnvVar('validSecondCarModelId'))
+}
+
+exports.createHasVariantRelationship = async function () {
+    await post("/car-models/" + bru.getEnvVar('validCarModelId') + "/has-variant/" + bru.getEnvVar('validCarModelVariantId'))
+}
+
+exports.createHasImageRelationship = async function () {
+    await post("/car-models/" + bru.getEnvVar('validCarModelId') + "/has-image/" + bru.getEnvVar('validImageId'))
+}
+
+exports.createHasPrimeImageRelationship = async function () {
+    await post("/car-models/" + bru.getEnvVar('validCarModelId') + "/has-prime-image/" + bru.getEnvVar('validImageId'))
 }
