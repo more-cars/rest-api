17b2de0e908aeea4ae15ab5035cc9bbcab7fe857
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-11 18:26:14 +0100

    adding all Bruno tests for node type RACING EVENT

---
+++ tests/smoke/bruno/lib/node-types/RacingEvents.js
@@ -0,0 +1,43 @@
+const {post} = require("../apiRequest")
+
+exports.createBelongsToRacingSeriesHasLayoutRelationship = async function () {
+    await post("/racing-events/" + bru.getEnvVar('validRacingEventId') + "/belongs-to-racing-series/" + bru.getEnvVar('validRacingSeriesId'))
+}
+
+exports.createFollowsEventRelationship = async function () {
+    await post("/racing-events/" + bru.getEnvVar('validRacingEventId') + "/follows-event/" + bru.getEnvVar('validSecondRacingEventId'))
+}
+
+exports.createIsFollowedByEventRelationship = async function () {
+    await post("/racing-events/" + bru.getEnvVar('validRacingEventId') + "/is-followed-by-event/" + bru.getEnvVar('validSecondRacingEventId'))
+}
+
+exports.createHasRacingSessionRelationship = async function () {
+    await post("/racing-events/" + bru.getEnvVar('validRacingEventId') + "/has-racing-session/" + bru.getEnvVar('validRacingSessionId'))
+}
+
+exports.createTookPlaceAtRaceTrackRelationship = async function () {
+    await post("/racing-events/" + bru.getEnvVar('validRacingEventId') + "/took-place-at-race-track/" + bru.getEnvVar('validRaceTrackId'))
+}
+
+exports.createUsedTheTrackLayoutRelationship = async function () {
+    await post("/racing-events/" + bru.getEnvVar('validRacingEventId') + "/used-the-track-layout/" + bru.getEnvVar('validTrackLayoutId'))
+}
+
+exports.createHasImageRelationship = async function () {
+    await post("/racing-events/" + bru.getEnvVar('validRacingEventId') + "/has-image/" + bru.getEnvVar('validImageId'))
+}
+
+exports.createHasPrimeImageRelationship = async function () {
+    await post("/racing-events/" + bru.getEnvVar('validRacingEventId') + "/has-prime-image/" + bru.getEnvVar('validImageId'))
+}
+
+exports.create = async function (prefix = '') {
+    const response = await post("/racing-events", {
+        name: 'Dummy Racing Event',
+    })
+    const racingEvent = response.data
+    bru.setEnvVar('valid' + prefix + 'RacingEventId', racingEvent.id)
+
+    return racingEvent
+}
