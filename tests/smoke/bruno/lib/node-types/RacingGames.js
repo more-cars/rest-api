91436a346cbe7f3c2ac89d192372a82db81f185e
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-11 12:03:00 +0100

    adding missing bruno tests

--- tests/smoke/bruno/lib/node-types/RacingGames.js
+++ tests/smoke/bruno/lib/node-types/RacingGames.js
@@ -1,5 +1,21 @@
 const {post} = require("../apiRequest.js")
 
+exports.createFeaturesCarModelVariantRelationship = async function () {
+    await post("/racing-games/" + bru.getEnvVar('validRacingGameId') + "/features-car-model-variant/" + bru.getEnvVar('validCarModelVariantId'))
+}
+
+exports.createFeaturesTrackLayoutRelationship = async function () {
+    await post("/racing-games/" + bru.getEnvVar('validRacingGameId') + "/features-track-layout/" + bru.getEnvVar('validTrackLayoutId'))
+}
+
+exports.createHasImageRelationship = async function () {
+    await post("/racing-games/" + bru.getEnvVar('validRacingGameId') + "/has-image/" + bru.getEnvVar('validImageId'))
+}
+
+exports.createHasPrimeImageRelationship = async function () {
+    await post("/racing-games/" + bru.getEnvVar('validRacingGameId') + "/has-prime-image/" + bru.getEnvVar('validImageId'))
+}
+
 exports.create = async function (prefix = '') {
     const response = await post("/racing-games", {
         name: 'Dummy Racing Game',
