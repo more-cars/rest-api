f67562c87514589a003e3ba8a535fb9a4060cb87
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-12 18:21:55 +0100

    adding all Bruno tests for node type SESSION RESULT

--- tests/smoke/bruno/lib/node-types/SessionResults.js
+++ tests/smoke/bruno/lib/node-types/SessionResults.js
@@ -1,5 +1,25 @@
 const {post} = require("../apiRequest.js")
 
+exports.createBelongsToRacingSessionRelationship = async function () {
+    await post("/session-results/" + bru.getEnvVar('validSessionResultId') + "/belongs-to-racing-session/" + bru.getEnvVar('validRacingSessionId'))
+}
+
+exports.createAchievedWithCarModelVariantRelationship = async function () {
+    await post("/session-results/" + bru.getEnvVar('validSessionResultId') + "/achieved-with-car-model-variant/" + bru.getEnvVar('validCarModelVariantId'))
+}
+
+exports.createHasLapTimeRelationship = async function () {
+    await post("/session-results/" + bru.getEnvVar('validSessionResultId') + "/has-lap-time/" + bru.getEnvVar('validLapTimeId'))
+}
+
+exports.createHasImageRelationship = async function () {
+    await post("/session-results/" + bru.getEnvVar('validSessionResultId') + "/has-image/" + bru.getEnvVar('validImageId'))
+}
+
+exports.createHasPrimeImageRelationship = async function () {
+    await post("/session-results/" + bru.getEnvVar('validSessionResultId') + "/has-prime-image/" + bru.getEnvVar('validImageId'))
+}
+
 exports.create = async function (prefix = '') {
     const response = await post("/session-results", {
         position: 1,
