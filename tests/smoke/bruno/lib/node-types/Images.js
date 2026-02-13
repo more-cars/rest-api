1eaa2b14171958f948efc4a315a002ff4ed605a1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-13 12:45:42 +0100

    adding all Bruno tests for node type IMAGE

--- tests/smoke/bruno/lib/node-types/Images.js
+++ tests/smoke/bruno/lib/node-types/Images.js
@@ -1,5 +1,13 @@
 const {post} = require("../apiRequest.js")
 
+exports.createIsPrimeImageOfNodeRelationship = async function () {
+    await post("/images/" + bru.getEnvVar('validImageId') + "/is-prime-image-of-node/" + bru.getEnvVar('validNodeId'))
+}
+
+exports.createBelongsToNodeRelationship = async function () {
+    await post("/images/" + bru.getEnvVar('validImageId') + "/belongs-to-node/" + bru.getEnvVar('validNodeId'))
+}
+
 exports.create = async function (prefix = '') {
     const response = await post("/images", {
         image_provider: "picci",
