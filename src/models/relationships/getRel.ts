fc16bbcf5861608e490cd6878c587bc9d7e7d5f5
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-14 19:38:25 +0100

    cleanup

--- src/models/relationships/getRel.ts
+++ src/models/relationships/getRel.ts
@@ -1,39 +1,41 @@
 import type {RelationshipType} from "./types/RelationshipType"
 import {getRelComposition} from "./getRelComposition"
-import type {DbRelationship} from "../../db/types/DbRelationship"
 import {getDbRelationshipType} from "./getDbRelationshipType"
 import {getDbNodeType} from "./getDbNodeType"
 import {getRelationship} from "../../db/relationships/getRelationship"
 import {RelationshipDirection} from "../../db/types/RelationshipDirection"
 import type {GenericRelation} from "./types/GenericRelation"
+import type {BaseNode} from "../../db/types/BaseNode"
 
 export async function getRel(
     startNodeId: number,
     relationshipType: RelationshipType,
 ) {
     const relComposition = getRelComposition(relationshipType)
-    const dbRelationshipType: DbRelationship = getDbRelationshipType(relationshipType)
+    const dbRelationshipType = getDbRelationshipType(relationshipType)
     const modelEndNodeType = relComposition.endNodeType
     const dbEndNodeType = getDbNodeType(modelEndNodeType)
-    const isReverseRelationship = relComposition.isReverseRelationship
+    const isReverseRel = relComposition.isReverseRelationship
 
     const dbRelationship = await getRelationship(
         startNodeId,
         dbRelationshipType,
         dbEndNodeType,
-        isReverseRelationship ? RelationshipDirection.REVERSE : RelationshipDirection.FORWARD,
+        isReverseRel ? RelationshipDirection.REVERSE : RelationshipDirection.FORWARD,
     )
 
     if (!dbRelationship) {
         return false
     }
 
-    return {
-        id: dbRelationship.id,
+    const modelRelationship: GenericRelation = {
+        id: dbRelationship.relationship_id,
         type: relationshipType,
-        origin: dbRelationship.start_node,
-        destination: dbRelationship.end_node,
+        origin: dbRelationship.start_node as BaseNode, // TODO remove type assertion
+        destination: dbRelationship.end_node as BaseNode, // TODO remove type assertion
         created_at: dbRelationship.created_at,
-        updated_at: dbRelationship.updated_at
-    } as GenericRelation
+        updated_at: dbRelationship.updated_at,
+    }
+
+    return modelRelationship
 }
