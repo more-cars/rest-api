986f1ee16c3875f3009a759b6bb351b061a4944c
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-14 10:18:41 +0100

    extending the relationship seeder to be able to create relationships in both directions

--- tests/_toolbox/dbSeeding/seedRelationship.ts
+++ tests/_toolbox/dbSeeding/seedRelationship.ts
@@ -3,14 +3,24 @@ import type {DbRelationship} from "../../../src/db/types/DbRelationship"
 import {seedNode} from "./seedNode"
 import {createRelationship} from "../../../src/db/relationships/createRelationship"
 
+export enum REL {
+    FORWARD = 0,
+    REVERSE = 1,
+}
+
 export async function seedRelationship(
     startNodeType: NodeTypeEnum,
     endNodeType: NodeTypeEnum,
-    relationshipName: DbRelationship
+    relationshipName: DbRelationship,
+    reverse: REL = 0,
 ) {
     const startNode = await seedNode(startNodeType)
     const endNode = await seedNode(endNodeType)
-    const relationship = await createRelationship(startNode.id, endNode.id, relationshipName)
+    const relationship = await createRelationship(
+        reverse ? endNode.id : startNode.id,
+        reverse ? startNode.id : endNode.id,
+        relationshipName,
+    )
 
     if (!relationship) {
         throw new Error('Relationship could not be created')
