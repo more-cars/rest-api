fa016f9709154e7dec7f7066823d1779f341b77a
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-15 19:03:37 +0100

    fixing test that was broken because of bad copy'n'paste

--- tests/integration/models/images/nodes/get-all/requesting-a-paginated-node-collection.test.ts
+++ tests/integration/models/images/nodes/get-all/requesting-a-paginated-node-collection.test.ts
@@ -1,27 +1,27 @@
 import {describe, expect, test} from 'vitest'
 import {deleteAllNodesOfType} from "../../../../../_toolbox/dbSeeding/deleteAllNodesOfType"
 import {NodeTypeEnum} from "../../../../../../src/controllers/nodes/types/NodeTypeEnum"
-import type {CompanyNode} from "../../../../../../src/models/companies/types/CompanyNode"
-import {Company} from "../../../../../../src/models/companies/Company"
+import {ImageNode} from "../../../../../../src/models/images/types/ImageNode"
+import {Image} from "../../../../../../src/models/images/Image"
 import {seedNodes} from "../../../../../_toolbox/dbSeeding/seedNodes"
 
 describe('A paginated "get all IMAGE nodes" request returns the correct number of nodes', () => {
     test('when there exist no IMAGE nodes', async () => {
-        await deleteAllNodesOfType(NodeTypeEnum.COMPANY)
+        await deleteAllNodesOfType(NodeTypeEnum.IMAGE)
 
-        const expectedNodes: CompanyNode[] = []
-        const actualNodes = await Company.findAll({page: 1})
+        const expectedNodes: ImageNode[] = []
+        const actualNodes = await Image.findAll({page: 1})
 
         expect(actualNodes)
             .toEqual(expectedNodes)
     })
 
     test('when there exist IMAGE nodes', async () => {
-        await deleteAllNodesOfType(NodeTypeEnum.COMPANY)
+        await deleteAllNodesOfType(NodeTypeEnum.IMAGE)
         const amount = Math.ceil(Math.random() * 20)
         await seedNodes(NodeTypeEnum.IMAGE, amount)
 
-        const actualNodes = await Company.findAll({page: 1})
+        const actualNodes = await Image.findAll({page: 1})
 
         expect(actualNodes.length)
             .toEqual(amount)
