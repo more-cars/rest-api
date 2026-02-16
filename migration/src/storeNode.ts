62294695154e650681f3d26008786d1242e7cc09
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-16 17:59:23 +0100

    simplifying migration code

--- migration/src/storeNode.ts
+++ migration/src/storeNode.ts
@@ -14,21 +14,7 @@ import type {InputRacingGameCreate} from "../../src/db/nodes/racing-games/types/
 import type {InputGamingPlatformCreate} from "../../src/db/nodes/gaming-platforms/types/InputGamingPlatformCreate"
 import type {InputImageCreate} from "../../src/db/nodes/images/types/InputImageCreate"
 import {NodeTypeLabel} from "../../src/db/NodeTypeLabel"
-import {createNodeQuery as createCompanyQuery} from "../../src/db/nodes/companies/createNode"
-import {createNodeQuery as createBrandQuery} from "../../src/db/nodes/brands/createNode"
-import {createNodeQuery as createCarModelQuery} from "../../src/db/nodes/car-models/createNode"
-import {createNodeQuery as createCarModelVariantQuery} from "../../src/db/nodes/car-model-variants/createNode"
-import {createNodeQuery as createRaceTrackQuery} from "../../src/db/nodes/race-tracks/createNode"
-import {createNodeQuery as createTrackLayoutQuery} from "../../src/db/nodes/track-layouts/createNode"
-import {createNodeQuery as createRacingSeriesQuery} from "../../src/db/nodes/racing-series/createNode"
-import {createNodeQuery as createRacingEventQuery} from "../../src/db/nodes/racing-events/createNode"
-import {createNodeQuery as createRacingSessionQuery} from "../../src/db/nodes/racing-sessions/createNode"
-import {createNodeQuery as createSessionResultQuery} from "../../src/db/nodes/session-results/createNode"
-import {createNodeQuery as createLapTimeQuery} from "../../src/db/nodes/lap-times/createNode"
-import {createNodeQuery as createRacingGameQuery} from "../../src/db/nodes/racing-games/createNode"
-import {createNodeQuery as createGamingPlatformQuery} from "../../src/db/nodes/gaming-platforms/createNode"
-import {createNodeQuery as createImageQuery} from "../../src/db/nodes/images/createNode"
-import {createDbNode} from "../../src/db/nodes/createDbNode"
+import {createDbNode, createNodeQuery} from "../../src/db/nodes/createDbNode"
 import {addMoreCarsIdToNode} from "../../src/db/nodes/addMoreCarsIdToNode"
 import {addTimestampsToNode} from "../../src/db/nodes/addTimestampsToNode"
 
@@ -50,56 +36,14 @@ type InputTypes =
 
 
 export async function storeNode(data: InputTypes, newNodeType: NodeTypeLabel, oldNode: Node): Promise<void> {
-    let query = ''
-
-    switch (newNodeType) {
-        case NodeTypeLabel.Company:
-            query = createCompanyQuery(data as InputCompanyCreate)
-            break
-        case NodeTypeLabel.Brand:
-            query = createBrandQuery(data as InputBrandCreate)
-            break
-        case NodeTypeLabel.CarModel:
-            query = createCarModelQuery(data as InputCarModelCreate)
-            break
-        case NodeTypeLabel.CarModelVariant:
-            query = createCarModelVariantQuery(data as InputCarModelVariantCreate)
-            break
-        case NodeTypeLabel.RaceTrack:
-            query = createRaceTrackQuery(data as InputRaceTrackCreate)
-            break
-        case NodeTypeLabel.TrackLayout:
-            query = createTrackLayoutQuery(data as InputTrackLayoutCreate)
-            break
-        case NodeTypeLabel.RacingSeries:
-            query = createRacingSeriesQuery(data as InputRacingSeriesCreate)
-            break
-        case NodeTypeLabel.RacingEvent:
-            query = createRacingEventQuery(data as InputRacingEventCreate)
-            break
-        case NodeTypeLabel.RacingSession:
-            query = createRacingSessionQuery(data as InputRacingSessionCreate)
-            break
-        case NodeTypeLabel.SessionResult:
-            query = createSessionResultQuery(data as InputSessionResultCreate)
-            break
-        case NodeTypeLabel.LapTime:
-            query = createLapTimeQuery(data as InputLapTimeCreate)
-            break
-        case NodeTypeLabel.RacingGame:
-            query = createRacingGameQuery(data as InputRacingGameCreate)
-            break
-        case NodeTypeLabel.GamingPlatform:
-            query = createGamingPlatformQuery(data as InputGamingPlatformCreate)
-            break
-        case NodeTypeLabel.Image:
-            query = createImageQuery(data as InputImageCreate)
-            break
-    }
+    let query = createNodeQuery(newNodeType, data)
 
     try {
         const createdNode: Node = await createDbNode(newNodeType, query)
-        await addMoreCarsIdToNode(createdNode.elementId, parseInt(oldNode.elementId) + 10_000_000, newNodeType)
+        await addMoreCarsIdToNode(
+            createdNode.elementId,
+            parseInt(oldNode.elementId) + 10_000_000
+        )
         await addTimestampsToNode(
             createdNode.elementId,
             new Date(oldNode.properties.created_at).toISOString(),
