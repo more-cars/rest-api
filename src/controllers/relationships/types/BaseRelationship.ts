430afb8b69859e3e4b393cc13ec61f5a3594a2c1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-18 17:26:32 +0100

    deleting obsolete file

--- src/controllers/relationships/types/BaseRelationship.ts
+++
@@ -1,9 +0,0 @@
-import type {BaseNode} from "../../nodes/types/BaseNode"
-
-export type BaseRelationship = {
-    relationship_id: number
-    relationship_name: string
-    relationship_partner: BaseNode
-    created_at: string
-    updated_at: string
-}
