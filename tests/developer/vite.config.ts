abeca4575cf2e41c097d89ac6a7d0c2d9845a728
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-19 12:50:17 +0100

    adjusting the "namespace" logic, so that each developer test keep independent, no matter if the "namespace" variable is set or not

--- tests/developer/vite.config.ts
+++ tests/developer/vite.config.ts
@@ -11,8 +11,12 @@ export default defineConfig(({mode}) => ({
             'tests/unit/**/*.test.ts',
             'tests/integration/**/*.test.ts',
         ],
-        // giving vitest access to all environment variables, so the tests can for example find the database
-        env: loadEnv(mode, rootDir, ''),
+        // Whenever an integration tests needs a running API to test against, it will create one on the fly.
+        // For those API instances to be able to access the database they need to know its location and the credentials.
+        // For all Kubernetes deployments this information is provided via environment variables.
+        // In the local development environment this information is provided via the .env file.
+        // The following line allows vitest to read all variables that are DB-related (but nothing else).
+        env: loadEnv(mode, rootDir, 'DB_'),
         silent: true,
         reporters: [
             ['junit', {
