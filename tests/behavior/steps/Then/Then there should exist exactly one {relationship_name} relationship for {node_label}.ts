f9a51286650e9112efe920e3184612080e439287
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 19:26:26 +0100

    fixing tests

--- tests/behavior/steps/Then/Then there should exist exactly one {relationship_name} relationship for {node_label}.ts
+++ tests/behavior/steps/Then/Then there should exist exactly one {relationship_name} relationship for {node_label}.ts
@@ -3,14 +3,15 @@ import axios from "axios"
 import assert from "assert"
 import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
 import {dasherize} from "inflection"
+import type {DbNode} from "../../../../src/db/types/DbNode"
 
 Then('there should exist exactly one {string} relationship for {string}',
     async (relationshipName: string, nodeLabel: string) => {
-        const node = world.recallNode(nodeLabel).data
+        const node: DbNode = world.recallNode(nodeLabel).data
         const path = getBasePathFragmentForNodeType(world.recallNode(nodeLabel).nodeType)
 
         const response = await axios
-            .get(`${process.env.API_URL}/${path}/${node.id}/${dasherize(relationshipName)}`)
+            .get(`${process.env.API_URL}/${path}/${node.properties.id}/${dasherize(relationshipName)}`)
             .catch(error => {
                 console.error(error)
             })
