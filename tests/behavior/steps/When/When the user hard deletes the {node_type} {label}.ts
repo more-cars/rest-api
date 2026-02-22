e0ba665cf20b66cbc0f32da1473fae9a7952cd8d
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 16:50:55 +0100

    fixing "delete node" tests

--- tests/behavior/steps/When/When the user hard deletes the {node_type} {label}.ts
+++ tests/behavior/steps/When/When the user hard deletes the {node_type} {label}.ts
@@ -2,14 +2,15 @@ import {When, world} from "@cucumber/cucumber"
 import axios from "axios"
 import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
 import type {ControllerNodeType} from "../../../../src/controllers/nodes/types/ControllerNodeType"
+import type {DbNode} from "../../../../src/db/types/DbNode"
 
 When('the user hard-deletes the {string} {string}',
     async (nodeType: string, label: string) => {
-        const node = world.recallNode(label).data
+        const node: DbNode = world.recallNode(label).data
         const path = getBasePathFragmentForNodeType(nodeType.toLowerCase() as ControllerNodeType)
 
         const response = await axios
-            .delete(`${process.env.API_URL}/${path}/${node.id}`)
+            .delete(`${process.env.API_URL}/${path}/${node.properties.id}`)
             .catch(error => {
                 console.error(error)
             })
