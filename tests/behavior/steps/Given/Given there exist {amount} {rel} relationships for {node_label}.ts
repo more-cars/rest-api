6909c0cc5ee10237c5b3695f5ef3d422d4820767
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 18:07:43 +0100

    fixing tests

--- tests/behavior/steps/Given/Given there exist {amount} {rel} relationships for {node_label}.ts
+++ tests/behavior/steps/Given/Given there exist {amount} {rel} relationships for {node_label}.ts
@@ -1,24 +1,23 @@
 import {Given, world} from "@cucumber/cucumber"
-import type {ControllerNodeType} from "../../../../src/controllers/nodes/types/ControllerNodeType"
-import type {DbNode} from "../../../../src/db/types/DbNode"
-import {seedNode} from "../../../_toolbox/dbSeeding/seedNode"
-import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
 import axios from "axios"
 import {dasherize} from "inflection"
+import type {DbNode} from "../../../../src/db/types/DbNode"
 import {getRelationshipSpecification} from "../../../../src/db/relationships/getRelationshipSpecification"
 import {constantCase} from "change-case"
 import {RelationshipType} from "../../../../src/db/types/RelationshipType"
+import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
+import {seedNode} from "../../../_toolbox/dbSeeding/seedNode"
+import {DbNodeType} from "../../../../src/db/types/DbNodeType"
 
 Given('there exist {int} {string} relationships for {string}',
     async (amount: number, relationshipName: string, startNodeLabel: string) => {
         const startNode: DbNode = world.recallNode(startNodeLabel).data
-        const startNodeType: ControllerNodeType = world.recallNode(startNodeLabel).nodeType
-        const relationship = getRelationshipSpecification(constantCase(startNodeType + '_' + relationshipName) as RelationshipType)
+        const relationship = getRelationshipSpecification(constantCase(startNode.node_type + '_' + relationshipName) as RelationshipType)
         const endNodeType = relationship.endNodeType
         const nodePathFragment = getBasePathFragmentForNodeType(world.recallNode(startNodeLabel).nodeType)
 
         for (let i = 0; i < amount; i++) {
-            const endNode = await seedNode(endNodeType)
+            const endNode = await seedNode(endNodeType || DbNodeType.Brand)
 
             await axios
                 .post(`${process.env.API_URL}/${nodePathFragment}/${startNode.properties.id}/${dasherize(relationshipName)}/${endNode.properties.id}`)
