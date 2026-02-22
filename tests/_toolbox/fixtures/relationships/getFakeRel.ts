c7ae0f7f1f777af1f908ae9c0697f9bf9bcdf01d
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 11:48:42 +0100

    using the correct relationship partners when creating a fake relationship (instead of hard-coded ones)

--- tests/_toolbox/fixtures/relationships/getFakeRel.ts
+++ tests/_toolbox/fixtures/relationships/getFakeRel.ts
@@ -2,14 +2,14 @@ import {faker} from "@faker-js/faker"
 import type {Rel} from "../../../../src/models/relationships/types/Rel"
 import type {RelType} from "../../../../src/models/relationships/types/RelType"
 import {getFakeModelNodeType} from "../nodes/getFakeModelNode"
-import {ModelNodeType} from "../../../../src/models/types/ModelNodeType"
+import {getRelComposition} from "../../../../src/models/relationships/getRelComposition"
 
 export function getFakeRel(relType: RelType): Rel {
     return {
         id: faker.number.int({min: 12_000_000, max: 20_000_000}),
         type: relType,
-        origin: getFakeModelNodeType(ModelNodeType.TrackLayout),
-        destination: getFakeModelNodeType(ModelNodeType.RacingEvent),
+        origin: getFakeModelNodeType(getRelComposition(relType).startNodeType),
+        destination: getFakeModelNodeType(getRelComposition(relType).endNodeType),
         created_at: faker.date.past().toISOString(),
         updated_at: faker.date.past().toISOString(),
     } satisfies Rel
