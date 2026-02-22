ce36cb6b3bc9dbcee0a8fa9c9628ecb268a62be0
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 19:14:52 +0100

    reverting fix

--- tests/behavior/steps/When/When the user requests the {node_type} {label}.ts
+++ tests/behavior/steps/When/When the user requests the {node_type} {label}.ts
@@ -2,14 +2,15 @@ import {When, world} from "@cucumber/cucumber"
 import axios from "axios"
 import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
 import type {ControllerNodeType} from "../../../../src/controllers/nodes/types/ControllerNodeType"
+import type {DbNode} from "../../../../src/db/types/DbNode"
 
 When('the user requests the {string} {string}',
     async (nodeType: string, label: string) => {
-        const node = world.recallResponse(label).data
-        const path = getBasePathFragmentForNodeType(nodeType as ControllerNodeType)
+        const node: DbNode = world.recallNode(label).data
+        const path = getBasePathFragmentForNodeType(nodeType.toLowerCase() as ControllerNodeType)
 
         const response = await axios
-            .get(`${process.env.API_URL}/${path}/${node.data.id}`)
+            .get(`${process.env.API_URL}/${path}/${node.properties.id}`)
 
         world.rememberResponse(response)
     })
