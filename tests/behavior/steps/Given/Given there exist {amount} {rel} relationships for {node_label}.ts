166f744256f01f49d749e543e1a6cf349d7506af
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 15:49:27 +0100

    replacing (now redundant) test helper

--- tests/behavior/steps/Given/Given there exist {amount} {rel} relationships for {node_label}.ts
+++ tests/behavior/steps/Given/Given there exist {amount} {rel} relationships for {node_label}.ts
@@ -1,17 +1,20 @@
 import {Given, world} from "@cucumber/cucumber"
 import type {ControllerNodeType} from "../../../../src/controllers/nodes/types/ControllerNodeType"
 import type {DbNode} from "../../../../src/db/types/DbNode"
-import {getTargetNodeTypeForRelationship} from "../../../_toolbox/getTargetNodeTypeForRelationship"
 import {seedNode} from "../../../_toolbox/dbSeeding/seedNode"
 import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
 import axios from "axios"
 import {dasherize} from "inflection"
+import {getRelationshipSpecification} from "../../../../src/db/relationships/getRelationshipSpecification"
+import {constantCase} from "change-case"
+import {RelationshipType} from "../../../../src/db/types/RelationshipType"
 
 Given('there exist {int} {string} relationships for {string}',
     async (amount: number, relationshipName: string, startNodeLabel: string) => {
         const startNode: DbNode = world.recallNode(startNodeLabel).data
         const startNodeType: ControllerNodeType = world.recallNode(startNodeLabel).nodeType
-        const endNodeType = getTargetNodeTypeForRelationship(startNodeType, relationshipName)
+        const relationship = getRelationshipSpecification(constantCase(startNodeType + '_' + relationshipName) as RelationshipType)
+        const endNodeType = relationship.endNodeType
         const nodePathFragment = getBasePathFragmentForNodeType(world.recallNode(startNodeLabel).nodeType)
 
         for (let i = 0; i < amount; i++) {
--- tests/behavior/steps/Given/Given there exists a {rel} relationship {rel_label} for {node_label}.ts
+++ tests/behavior/steps/Given/Given there exists a {rel} relationship {rel_label} for {node_label}.ts
@@ -2,16 +2,19 @@ import {Given, world} from "@cucumber/cucumber"
 import axios from "axios"
 import type {DbNode} from "../../../../src/db/types/DbNode"
 import {ControllerNodeType} from "../../../../src/controllers/nodes/types/ControllerNodeType"
-import {getTargetNodeTypeForRelationship} from "../../../_toolbox/getTargetNodeTypeForRelationship"
 import {seedNode} from "../../../_toolbox/dbSeeding/seedNode"
 import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
 import {dasherize} from "inflection"
+import {getRelationshipSpecification} from "../../../../src/db/relationships/getRelationshipSpecification"
+import {constantCase} from "change-case"
+import {RelationshipType} from "../../../../src/db/types/RelationshipType"
 
 Given('there exists a {string} relationship {string} for {string}',
     async (relationshipName: string, relationshipLabel: string, startNodeLabel: string) => {
         const startNode: DbNode = world.recallNode(startNodeLabel).data
         const startNodeType: ControllerNodeType = world.recallNode(startNodeLabel).nodeType
-        const endNodeType = getTargetNodeTypeForRelationship(startNodeType, relationshipName)
+        const relationship = getRelationshipSpecification(constantCase(startNodeType + '_' + relationshipName) as RelationshipType)
+        const endNodeType = relationship.endNodeType
         const endNode = await seedNode(endNodeType)
         const nodePathFragment = getBasePathFragmentForNodeType(world.recallNode(startNodeLabel).nodeType)
 
