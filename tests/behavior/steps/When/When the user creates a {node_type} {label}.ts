844952ce5025043f2198cbd6b1da585436f86ba3
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 19:34:24 +0100

    fixing tests

--- tests/behavior/steps/When/When the user creates a {node_type} {label}.ts
+++ tests/behavior/steps/When/When the user creates a {node_type} {label}.ts
@@ -3,11 +3,12 @@ import axios from "axios"
 import {getBasePathFragmentForNodeType} from "../../../_toolbox/dbSeeding/getBasePathFragmentForNodeType"
 import {FakeNodeInput} from "../../../_toolbox/fixtures/nodes/FakeNodeInput"
 import type {ControllerNodeType} from "../../../../src/controllers/nodes/types/ControllerNodeType"
+import {dasherize, pluralize} from "inflection"
 
 When('the user creates a {string} {string}',
     async (nodeType: string, label: string) => {
-        const path = getBasePathFragmentForNodeType(nodeType.toLowerCase() as ControllerNodeType)
-        const data = FakeNodeInput(nodeType.toLowerCase() as ControllerNodeType)
+        const path = getBasePathFragmentForNodeType(nodeType as ControllerNodeType)
+        const data = FakeNodeInput(dasherize(pluralize(nodeType.toLowerCase())) as ControllerNodeType)
 
         const response = await axios
             .post(`${process.env.API_URL}/${path}`, data)
