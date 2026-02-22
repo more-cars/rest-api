755ca2413a6a20a3455db4c67dc6031e071395f1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 19:20:04 +0100

    fixing tests

--- tests/behavior/steps/When/When the user requests a grouped list of all relationships for the IMAGE {label}.ts
+++ tests/behavior/steps/When/When the user requests a grouped list of all relationships for the IMAGE {label}.ts
@@ -1,13 +1,12 @@
 import {When, world} from "@cucumber/cucumber"
 import axios from "axios"
-import {ImageNode} from "../../../../src/models/node-types/images/types/ImageNode"
 
 When('the user requests a grouped list of all relationships for the IMAGE {string}',
     async (imageLabel: string) => {
-        const image: ImageNode = world.recallNode(imageLabel).data
+        const image = world.recallNode(imageLabel).data
 
         const response = await axios
-            .get(`${process.env.API_URL}/images/${image.attributes.id}/belongs-to-node-type`)
+            .get(`${process.env.API_URL}/images/${image.properties.id}/belongs-to-node-type`)
             .catch(error => {
                 console.error(error)
             })
