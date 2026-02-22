e0ad6cd20de9c34bbbe8689e8ab0688cd0bc33f3
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-22 17:10:59 +0100

    fixing tests

--- tests/behavior/steps/Given/Given {node_type} {label} does NOT exist.ts
+++ tests/behavior/steps/Given/Given {node_type} {label} does NOT exist.ts
@@ -3,8 +3,11 @@ import {Given, world} from "@cucumber/cucumber"
 Given('{string} {string} does NOT exist',
     async (nodeType: string, label: string) => {
         const node: any = {
-            id: -Math.ceil(Math.random() * 1000),
-            external_id: "Non-existing Image",
+            node_type: 'blubb',
+            properties: {
+                id: -Math.ceil(Math.random() * 1000),
+                external_id: "Non-existing Image",
+            }
         }
 
         world.rememberNode(node, label, nodeType.toLowerCase())
