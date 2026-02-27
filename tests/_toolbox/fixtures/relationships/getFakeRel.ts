ec8a19a7d1459ed027a98057a932921d63b575fe
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-27 10:53:33 +0100

    replacing mapper

--- tests/_toolbox/fixtures/relationships/getFakeRel.ts
+++ tests/_toolbox/fixtures/relationships/getFakeRel.ts
@@ -1,15 +1,16 @@
 import {faker} from "@faker-js/faker"
-import type {Rel} from "../../../../src/models/relationships/types/Rel"
 import type {RelType} from "../../../../src/models/relationships/types/RelType"
-import {getFakeModelNode} from "../nodes/getFakeModelNode"
+import type {Rel} from "../../../../src/models/relationships/types/Rel"
+import {getFakeNode} from "../nodes/getFakeNode"
+import {mapModelNodeTypeToNodeType} from "../../../../src/specification/mapModelNodeTypeToNodeType"
 import {getRelComposition} from "../../../../src/models/relationships/getRelComposition"
 
 export function getFakeRel(relType: RelType): Rel {
     return {
         id: faker.number.int({min: 12_000_000, max: 20_000_000}),
         type: relType,
-        origin: getFakeModelNode(getRelComposition(relType).startNodeType),
-        destination: getFakeModelNode(getRelComposition(relType).endNodeType),
+        origin: getFakeNode(mapModelNodeTypeToNodeType(getRelComposition(relType).startNodeType)).modelOutput,
+        destination: getFakeNode(mapModelNodeTypeToNodeType(getRelComposition(relType).endNodeType)).modelOutput,
         created_at: faker.date.past().toISOString(),
         updated_at: faker.date.past().toISOString(),
     } satisfies Rel
