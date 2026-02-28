b09fb9303b1e8ccf644c08055e6dfd359a971816
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-28 16:22:13 +0100

    deleting obsolete test step

--- tests/behavior/steps/Given/Given there exists a minimal CAR MODEL {label}.ts
+++
@@ -1,7 +0,0 @@
-import {Given, world} from "@cucumber/cucumber"
-import {seedCarModelMinimal} from "../../../_toolbox/dbSeeding/car-models/nodes/seedCarModelMinimal"
-
-Given('there exists a minimal CAR MODEL {string}',
-    async (label: string) => {
-        world.rememberNode(await seedCarModelMinimal(), label, 'car model')
-    })
