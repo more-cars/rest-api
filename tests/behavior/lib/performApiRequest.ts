7395677dcc5e5491d33cc210d1de147676134fc4
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-28 20:55:56 +0100

    moving the axios configuration to where axios is actually used

--- tests/behavior/lib/performApiRequest.ts
+++ tests/behavior/lib/performApiRequest.ts
@@ -2,6 +2,12 @@ import axios from "axios"
 import type {ApiResponse} from "./ApiResponse"
 import {ResponseManager} from "./ResponseManager"
 
+// By default, Axios fails every request that returns with a status code >= 400.
+// But for the tests we only want them to fail when a server error occurred (status code >= 500).
+axios.defaults.validateStatus = function (status) {
+    return status < 500
+}
+
 export async function performApiRequest(path: string, method: 'POST' | 'GET' | 'DELETE' = 'GET', data?: any): Promise<ApiResponse> {
     const axiosConfig = {
         baseURL: process.env.API_URL,
--- tests/behavior/steps/_Setup.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-// By default, Axios fails every request that returns with a status code >= 400.
-// But for the tests we only want them to fail when a server error occurred (status code >= 500).
-axios.defaults.validateStatus = function (status) {
-    return status < 500
-}
