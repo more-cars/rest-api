0115e96b0335980c1b8962ec36505155f60fce82
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-01 11:40:27 +0100

    re-implementing test step

--- tests/behavior/steps/Then/Then the relationship {label} should not exist anymore.ts
+++ tests/behavior/steps/Then/Then the relationship {label} should not exist anymore.ts
@@ -1,14 +1,14 @@
 import {Then} from "@cucumber/cucumber"
-// import assert from "assert"
-// import {RelationshipManager} from "../../lib/RelationshipManager"
-// import {performApiRequest} from "../../lib/performApiRequest"
+import assert from "assert"
+import {RelationshipManager} from "../../lib/RelationshipManager"
+import {performApiRequest} from "../../lib/performApiRequest"
 
 Then('the relationship {string} should not exist anymore',
     async (label: string) => {
-        // const rel = RelationshipManager.getRelationshipByLabel(label)
-        // const path = `/car-models/${rel.start_node.fields.id}/belongs-to-brand/${rel.data.relationship_partner.data.id}`
-        //
-        // const response = await performApiRequest(path, 'GET')
-        //
-        // assert.equal(response.status_code, 404)
+        const relationship = RelationshipManager.getRelationshipByLabel(label)
+        const path = `/relationships/${relationship.data.relationship_id}`
+
+        const response = await performApiRequest(path, 'GET')
+
+        assert.equal(response.status_code, 404)
     })
