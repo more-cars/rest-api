df5e28c37effe1eafa63e9736cbd46036384916f
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-04 11:01:23 +0100

    fixing edge case when converting between singular and plural node type

--- tests/behavior/lib/convertStringToRelationshipType.ts
+++ tests/behavior/lib/convertStringToRelationshipType.ts
@@ -1,6 +1,14 @@
 import {pascalCase} from "change-case"
+import {singularize} from "inflection"
 import {RelationshipType} from "../../../src/specification/RelationshipType"
 
 export function convertStringToRelationshipType(rawRelationshipType: string, rawStartNodeType: string) {
-    return RelationshipType[pascalCase(rawStartNodeType) + pascalCase(rawRelationshipType) as keyof typeof RelationshipType] as RelationshipType
+    let startNodeType = pascalCase(singularize(rawStartNodeType))
+    if (startNodeType === 'RacingSery') {
+        startNodeType = 'RacingSeries'
+    }
+
+    const relationshipType = (startNodeType + pascalCase(rawRelationshipType)) as keyof typeof RelationshipType
+
+    return RelationshipType[relationshipType] as RelationshipType
 }
