c6b06ecb66922ab807996250b69159a14dac72a3
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-05 18:48:50 +0100

    fixing behavior test

--- tests/behavior/steps/Then/Then the response should contain the following keys for each node.ts
+++ tests/behavior/steps/Then/Then the response should contain the following keys for each node.ts
@@ -1,16 +1,17 @@
 import {DataTable, Then} from "@cucumber/cucumber"
 import assert from "assert"
 import {ResponseManager} from "../../lib/ResponseManager"
+import type {NodeResponse} from "../../../../src/controllers/types/NodeResponse"
 
 Then('the response should contain the following keys for each node',
     (dataTable: DataTable) => {
         const response = ResponseManager.getPreviousResponse()
-        const data = response.body.attributes
+        const data = response.body.data
 
         const rows = dataTable.hashes()
-        data.forEach((item: any) => {
+        data.forEach((item: NodeResponse) => {
             rows.forEach((row) => {
-                assert(row.key in item.data, `"${row.key}" not found in the response`)
+                assert(row.key in item.attributes, `"${row.key}" not found in the response`)
             })
         })
     })
