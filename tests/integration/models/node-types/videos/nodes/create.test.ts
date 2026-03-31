016b237239dfadec72601e1ab3ad8fbc63e3dd35
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-31 10:28:39 +0200

    fixing tests

--- tests/integration/models/node-types/videos/nodes/create.test.ts
+++ tests/integration/models/node-types/videos/nodes/create.test.ts
@@ -1,8 +1,13 @@
-import {expect, test} from 'vitest'
+import {expect, test, vi} from 'vitest'
+import * as yt from "../../../../../../src/db/external/youtube/performYouTubeApiRequest"
+import {FakeGetVideoByIdResponse} from "../../../../../_toolbox/fixtures/external/youtube/FakeGetVideoByIdResponse"
 import {FakeVideo} from "../../../../../_toolbox/fixtures/nodes/FakeVideo"
 import {Video} from "../../../../../../src/models/node-types/videos/Video"
 
 test('Expecting node to be created when provided with valid data', async () => {
+    vi.spyOn(yt, 'performYouTubeApiRequest')
+        .mockImplementation(async () => FakeGetVideoByIdResponse)
+
     const inputData = FakeVideo.dbInput
     const createdNode = await Video.create(inputData)
 
@@ -11,6 +16,9 @@ test('Expecting node to be created when provided with valid data', async () => {
 })
 
 test('Trying to override read-only properties', async () => {
+    vi.spyOn(yt, 'performYouTubeApiRequest')
+        .mockImplementation(async () => FakeGetVideoByIdResponse)
+
     const validData = FakeVideo.dbInput
     const readOnlyData = {
         id: 9999,
