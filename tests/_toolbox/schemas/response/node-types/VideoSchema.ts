2113a4170427b3df217b11fac2f8b9bfa547f746
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-03 10:45:12 +0200

    fixing VideoSchema

--- tests/_toolbox/schemas/response/node-types/VideoSchema.ts
+++ tests/_toolbox/schemas/response/node-types/VideoSchema.ts
@@ -30,11 +30,18 @@ export const VideoSchema = {
             ],
             additionalProperties: false,
         },
+        links: {
+            type: "object",
+            properties: {
+                self: {type: ["string"]},
+            },
+        },
     },
     required: [
         "type",
         "id",
         "attributes",
+        "links",
     ],
     additionalProperties: false,
 }
