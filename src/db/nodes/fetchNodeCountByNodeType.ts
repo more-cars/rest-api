0698e06f25fda409cb4118b4e8db13bfc56653ea
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-11 13:55:00 +0200

    escaping input data

--- src/db/nodes/fetchNodeCountByNodeType.ts
+++ src/db/nodes/fetchNodeCountByNodeType.ts
@@ -5,6 +5,7 @@ import {getDriver} from "../driver"
 import {getNamespacedNodeTypeLabel} from "../getNamespacedNodeTypeLabel"
 import {mapDbNodeTypeToNeo4jNodeType} from "./mapDbNodeTypeToNeo4jNodeType"
 import {getCypherQueryTemplate} from "../getCypherQueryTemplate"
+import {escapeSingleQuotes} from "./escapeSingleQuotes"
 
 export async function fetchNodeCountByNodeType(nodeType: DbNodeType, params?: CollectionQueryParams) {
     const driver = getDriver()
@@ -24,11 +25,18 @@ export function fetchNodeCountByNodeTypeQuery(nodeType: DbNodeType, params?: Col
     const nodeTypeLabel = getNamespacedNodeTypeLabel(mapDbNodeTypeToNeo4jNodeType(nodeType))
 
     if (params?.filterByProperty && params?.filterValue) {
+        let filterValue = params.filterValue
+        if (typeof filterValue === "string") {
+            filterValue = `'${escapeSingleQuotes(filterValue)}'`
+        } else {
+            filterValue = `${filterValue}`
+        }
+
         return getCypherQueryTemplate('nodes/_cypher/getNodeCountByNodeTypeFiltered.cypher')
             .trim()
             .replace(':nodeLabel', `:${nodeTypeLabel}`)
             .replace('$filterByProperty', `${params.filterByProperty}`)
-            .replace('$filterValue', ['boolean', 'number'].includes(typeof params.filterValue) ? `${params.filterValue}` : `'${params.filterValue}'`)
+            .replace('$filterValue', filterValue)
             .replace('$filterOperator', `${params.filterOperator}`)
     } else { // the reason for this switch is to take advantage of the better database performance when an unfiltered query is executed
         return getCypherQueryTemplate('nodes/_cypher/getNodeCountByNodeType.cypher')
--- src/db/nodes/getNodeTypeCollectionQuery.ts
+++ src/db/nodes/getNodeTypeCollectionQuery.ts
@@ -3,11 +3,19 @@ import type {CollectionQueryParams} from "../types/CollectionQueryParams"
 import {getNamespacedNodeTypeLabel} from "../getNamespacedNodeTypeLabel"
 import {mapDbNodeTypeToNeo4jNodeType} from "./mapDbNodeTypeToNeo4jNodeType"
 import {getCypherQueryTemplate} from "../getCypherQueryTemplate"
+import {escapeSingleQuotes} from "./escapeSingleQuotes"
 
 export function getNodeTypeCollectionQuery(nodeType: DbNodeType, params: CollectionQueryParams) {
     const nodeTypeLabel = getNamespacedNodeTypeLabel(mapDbNodeTypeToNeo4jNodeType(nodeType))
 
     if (params?.filterByProperty && params?.filterValue) {
+        let filterValue = params.filterValue
+        if (typeof filterValue === "string") {
+            filterValue = `'${escapeSingleQuotes(filterValue)}'`
+        } else {
+            filterValue = `${filterValue}`
+        }
+
         return getCypherQueryTemplate('nodes/_cypher/getNodeTypeCollectionFiltered.cypher')
             .trim()
             .replace(':nodeLabel', `:${nodeTypeLabel}`)
@@ -15,7 +23,7 @@ export function getNodeTypeCollectionQuery(nodeType: DbNodeType, params: Collect
             .replace('$sortDirection', `${params.sortDirection}`)
             .replace('$filterByProperty', `${params.filterByProperty}`)
             .replace('$filterOperator', `${params.filterOperator}`)
-            .replace('$filterValue', ['boolean', 'number'].includes(typeof params.filterValue) ? `${params.filterValue}` : `'${params.filterValue}'`)
+            .replace('$filterValue', filterValue)
             .replace('$offset', `${params.offset}`)
             .replace('$limit', `${params.limit}`)
     } else {
