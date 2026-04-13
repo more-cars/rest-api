3bbc180841a929bd711133ccf4e158516b00ea98
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-13 13:50:55 +0200

    removing obsolete test steps

--- tests/behavior/steps/Given/Given there exists NO {rel_name} relationship for {node_label}.ts
+++ tests/behavior/steps/Given/Given there exists NO {rel_name} relationship for {node_label}.ts
@@ -1,7 +1,7 @@
 import {Given} from "@cucumber/cucumber"
 
-Given('there exists NO {string} relationship between {string} and {string}',
-    async function (relationshipName: string, startNodeLabel: string, endNodeLabel: string) {
+Given('there exists NO {string} relationship for {string}',
+    async function (relationshipName: string, startNodeLabel: string) {
         // Nothing to do here.
         // This step only exists to be able to better describe the intention of the scenario.
         // There never exists anything in the database that was not explicitly created by the scenario itself.
--- tests/behavior/steps/Given/Given there exists NO {rel} relationship {rel_label} between {node_label} and {node_label}.ts
+++
@@ -1,6 +0,0 @@
-import {Given} from "@cucumber/cucumber"
-
-Given('there exists NO {string} relationship {string} between {string} and {string}',
-    async function (relationshipName: string, relationshipLabel: string, startNodeLabel: string, endNodeLabel: string) {
-        // TODO delete relationship if it exists
-    })
