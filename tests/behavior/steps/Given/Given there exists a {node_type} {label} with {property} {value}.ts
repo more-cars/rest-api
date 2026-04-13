0ec8f99f4468ee1e4d582c88285d07b2f0412756
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-13 15:57:04 +0200

    fixing tests

--- tests/behavior/steps/Given/Given there exists a {node_type} {label} with {property} {value}.ts
+++ tests/behavior/steps/Given/Given there exists a {node_type} {label} with {property} {value}.ts
@@ -1,9 +1,14 @@
 import {Given} from "@cucumber/cucumber"
 import {NodeManager} from "../../lib/NodeManager"
+import {getFakeNode} from "../../../_toolbox/fixtures/nodes/getFakeNode"
+import {convertStringToExpectedNodeType} from "../../../_toolbox/convertStringToNodeType"
 
 Given('there exists a(n) {string} {string} with {string} {string}',
     async (nodeType: string, label: string, property: string, propertyValue: string) => {
-        await NodeManager.createNode(nodeType, label, {
-            [property]: propertyValue,
-        })
+        const node = getFakeNode(convertStringToExpectedNodeType(nodeType))
+
+        // @ts-ignore
+        node.dbInputMinimal[property] = isNaN(propertyValue) ? propertyValue : Number(propertyValue)
+
+        await NodeManager.createNode(nodeType, label, node.dbInputMinimal)
     })
--- tests/behavior/steps/When/When the user requests a {node_type} collection, sorted {direction} by {property}.ts
+++ tests/behavior/steps/When/When the user requests a {node_type} collection, sorted {direction} by {property}.ts
@@ -1,19 +1,9 @@
 import {When} from "@cucumber/cucumber"
 import {getBasePathFragmentForNodeType} from "../../lib/getBasePathFragmentForNodeType"
-import {ControllerNodeType} from "../../../../src/controllers/types/ControllerNodeType"
-import {dasherize, pluralize} from "inflection"
 import {performApiRequest} from "../../lib/performApiRequest"
 
 When('the user requests a {string} collection, sorted {string} by {string}',
     async (nodeTypeName: string, sortDirection: string, sortByProperty: string) => {
-        // TODO this is a temporary workaround -> the Gherkin scenarios should not assume that every node type has a property "name"
-        const nodeType = dasherize(pluralize(nodeTypeName.toLowerCase())) as ControllerNodeType
-        if (nodeType === ControllerNodeType.SessionResult) {
-            sortByProperty = 'position'
-        } else if (nodeType === ControllerNodeType.LapTime) {
-            sortByProperty = 'time'
-        }
-
         const nodePath = getBasePathFragmentForNodeType(nodeTypeName)
         const path = `/${nodePath}?sort_by_property=${sortByProperty}&sort_direction=${sortDirection}`
 
