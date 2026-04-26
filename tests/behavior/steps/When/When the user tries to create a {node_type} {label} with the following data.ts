3749574347318a15d5c8c9162dd75bda3e244d7d
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-26 10:58:18 +0200

    splitting test step to allow simplification of data tables in some of the Gherkin scenarios

--- tests/behavior/steps/When/When the user tries to create a {node_type} {label} with the following data.ts
+++ tests/behavior/steps/When/When the user tries to create a {node_type} {label} with the following data.ts
@@ -1,30 +1,21 @@
 import {DataTable, When} from "@cucumber/cucumber"
-import {getNodeTypeSpecification} from "../../../../src/specification/getNodeTypeSpecification"
-import {convertStringToNodeType} from "../../../_toolbox/convertStringToNodeType"
 import {getBasePathFragmentForNodeType} from "../../lib/getBasePathFragmentForNodeType"
 import {performApiRequest} from "../../lib/performApiRequest"
 import {NodeManager} from "../../lib/NodeManager"
 import {convertNodeResponseToNode} from "../../lib/convertNodeResponseToNode"
 
-When('the user creates a {string} {string} with the following data',
-    async (nodeType: string, nodeLabel: string, dataTable: DataTable) => {
+When('the user tries to create a {string} {string} with the following data',
+    async (nodeType: string, label: string, dataTable: DataTable) => {
         const rows = dataTable.hashes()
-        const specs = getNodeTypeSpecification(convertStringToNodeType(nodeType))
 
         const data: any = {}
         rows.forEach((row) => {
-            const spec = specs.properties.find(prop => prop.name === row.key)
-            if (!spec) {
-                data[row.key] = Number(row.value) ? Number(row.value) : row.value
-                return
-            }
-
-            switch (spec.datatype) {
+            switch (row.datatype) {
                 case 'string':
                     data[row.key] = row.value
                     break
                 case 'number':
-                    data[row.key] = parseFloat(row.value) || null
+                    data[row.key] = parseFloat(row.value)
                     break
                 case 'boolean':
                     data[row.key] = (row.value.toLowerCase() === 'true')
@@ -37,6 +28,6 @@ When('the user creates a {string} {string} with the following data',
 
         const response = await performApiRequest(path, 'POST', data)
         if (response.status_code < 400) {
-            NodeManager.cacheNode(convertNodeResponseToNode(response.body), nodeLabel)
+            NodeManager.cacheNode(convertNodeResponseToNode(response.body), label)
         }
     })
