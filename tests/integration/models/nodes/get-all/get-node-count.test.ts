b2590b51c7aa7f9ecb7be8eb8e6bbaac97a25cc9
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-01 14:44:29 +0200

    fixing linting error

--- tests/integration/models/nodes/get-all/get-node-count.test.ts
+++ tests/integration/models/nodes/get-all/get-node-count.test.ts
@@ -1,14 +1,12 @@
 import {describe, expect, test, vi} from 'vitest'
 import {getAllModelNodeTypes} from "../../../../_toolbox/getAllModelNodeTypes"
+import * as node from "../../../../../src/db/nodes/fetchNodeCountByNodeType"
 import {Node} from "../../../../../src/models/Node"
 
 describe('get total amount of nodes', () => {
     test.each(getAllModelNodeTypes())('for node type $0', async (nodeType) => {
-        vi.mock("../../../../../src/db/nodes/fetchNodeCountByNodeType", async () => {
-            return {
-                fetchNodeCountByNodeType: () => 7
-            }
-        })
+        vi.spyOn(node, 'fetchNodeCountByNodeType')
+            .mockImplementation(async () => 7)
 
         const actualNodeCount = await Node.getTotalAmount(nodeType)
 
