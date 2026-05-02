8abe81eecb2f3221104c47b5d2719630be2dd89b
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-02 13:49:10 +0200

    removing obsolete hostname (since introducing the gateway the database GUI is only available via port-forwarding)

--- deployment/add-minikube-hostnames.sh
+++ deployment/add-minikube-hostnames.sh
@@ -13,7 +13,3 @@ fi
 if ! getent hosts swagger."$TARGET_ENVIRONMENT".more-cars.internal >/dev/null 2>&1; then
   sudo "$(which node)" -r ts-node/register "$SCRIPT_PATH"/lib/addHostname.ts swagger."$TARGET_ENVIRONMENT".more-cars.internal $MINIKUBE_IP
 fi
-
-if ! getent hosts db."$TARGET_ENVIRONMENT".more-cars.internal >/dev/null 2>&1; then
-  sudo "$(which node)" -r ts-node/register "$SCRIPT_PATH"/lib/addHostname.ts db."$TARGET_ENVIRONMENT".more-cars.internal $MINIKUBE_IP
-fi
