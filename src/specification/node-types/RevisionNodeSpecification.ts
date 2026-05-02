251a6954fdb4ca8aa8da7622af5930860d3b00bd
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-02 12:16:30 +0200

    adding missing properties to REVISION specification

--- src/specification/node-types/RevisionNodeSpecification.ts
+++ src/specification/node-types/RevisionNodeSpecification.ts
@@ -3,5 +3,30 @@ import {NodeType} from "../NodeType"
 
 export const RevisionNodeSpecification: NodeSpecification = {
     type: NodeType.Revision,
-    properties: [],
+    properties: [
+        {
+            name: 'node_type',
+            datatype: 'string',
+            mandatory: true,
+            example: 'CarModel',
+        },
+        {
+            name: 'node_id',
+            datatype: 'number',
+            mandatory: true,
+            example: 12345678,
+        },
+        {
+            name: 'node_created_at',
+            datatype: 'string',
+            mandatory: true,
+            example: '2024-04-14T11:04:04.493Z',
+        },
+        {
+            name: 'node_updated_at',
+            datatype: 'string',
+            mandatory: true,
+            example: '2024-04-14T11:04:04.493Z',
+        },
+    ],
 }
