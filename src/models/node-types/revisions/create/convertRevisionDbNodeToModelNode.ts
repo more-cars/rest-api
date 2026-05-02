f23abbf1e67915a72ee8f3e43f7e4682c80beb14
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-02 14:26:44 +0200

    converting REVISION data

--- src/models/node-types/revisions/create/convertRevisionDbNodeToModelNode.ts
+++ src/models/node-types/revisions/create/convertRevisionDbNodeToModelNode.ts
@@ -5,17 +5,6 @@ import type {RevisionNode} from "../types/RevisionNode"
 export function convertRevisionDbNodeToModelNode(data: RevisionNodeInput): RevisionNode {
     return {
         node_type: ModelNodeType.Revision,
-        attributes: {
-            id: data.properties.id,
-
-            node_type: data.properties.node_type,
-            node_id: data.properties.node_id,
-            node_created_at: data.properties.node_created_at,
-            node_updated_at: data.properties.node_updated_at,
-            // TODO add remaining props
-
-            created_at: data.properties.created_at,
-            updated_at: data.properties.updated_at,
-        },
+        attributes: data.properties,
     } satisfies RevisionNode
 }
