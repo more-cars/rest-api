aba795ab86663a40eb8ae9679b4b6d0546913b5c
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-04 15:54:09 +0200

    adding a health check for the API service, so Kubernetes knows when the API is actually operational (after deployment, restart or scaling)

--- src/controllers/health/health.ts
+++ src/controllers/health/health.ts
@@ -3,5 +3,6 @@ import express from "express"
 export async function health(req: express.Request, res: express.Response) {
     res.status(200)
     res.set('Content-Type', 'text/plain')
+    res.set('Cache-Control', 'no-cache')
     res.send("healthy")
 }
