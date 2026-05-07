c5a4d211308fbae5b654636e16d4f50d18df9847
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-07 10:09:30 +0200

    fixing wrong node type parameter

--- src/controllers/node-types/race-tracks/create.ts
+++ src/controllers/node-types/race-tracks/create.ts
@@ -15,7 +15,7 @@ import {sendResponse400} from "../../responses/sendResponse400"
 import {sendResponse500} from "../../responses/sendResponse500"
 
 export async function create(req: express.Request, res: express.Response) {
-    const propertyNames = getNodeTypeSpecification(NodeType.ModelCar).properties.map(prop => prop.name)
+    const propertyNames = getNodeTypeSpecification(NodeType.RaceTrack).properties.map(prop => prop.name)
     const data = unmarshalInputData(req.body, propertyNames) as CreateRaceTrackInput
 
     if (!validate(data)) {
