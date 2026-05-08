97668a6a23dac5119bd617a08587c9bc7826d715
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-08 18:33:29 +0200

    MCA-5432 implementing gherkin scenario

---
+++ tests/behavior/steps/When/When the user updates the node {label} with the following data.ts
@@ -0,0 +1,26 @@
+import {DataTable, When} from "@cucumber/cucumber"
+import {getBasePathFragmentForNodeType} from "../../lib/getBasePathFragmentForNodeType"
+import {performApiRequest} from "../../lib/performApiRequest"
+import {NodeManager} from "../../lib/NodeManager"
+
+When('the user updates the node {string} with the following data',
+    async (nodeLabel: string, dataTable: DataTable) => {
+        const data: any = {}
+
+        const properties = dataTable.hashes()
+        properties.forEach((property) => {
+            if (property.value === '') {
+                data[property.key] = null
+            } else if (!isNaN(Number(property.value))) {
+                data[property.key] = Number(property.value)
+            } else {
+                data[property.key] = property.value
+            }
+        })
+
+        const node = NodeManager.getNodeByLabel(nodeLabel)
+        const nodePath = getBasePathFragmentForNodeType(node.node_type)
+        const path = `/${nodePath}/${node.fields.id}`
+
+        await performApiRequest(path, 'PATCH', data)
+    })
