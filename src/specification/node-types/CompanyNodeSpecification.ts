1edc71122b7ea03f9db081b4baa4d46447e375fa
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-09 11:08:39 +0200

    updating COMPANY node specification to contain fields "scope" and "validation" for every property

--- src/specification/node-types/CompanyNodeSpecification.ts
+++ src/specification/node-types/CompanyNodeSpecification.ts
@@ -9,6 +9,7 @@ export const CompanyNodeSpecification: NodeSpecification = {
             datatype: 'string',
             mandatory: true,
             example: 'BMW AG',
+            scope: 'user',
             validation: ['mandatory', 'string']
         },
         {
@@ -16,24 +17,31 @@ export const CompanyNodeSpecification: NodeSpecification = {
             datatype: 'number',
             mandatory: false,
             example: 1916,
+            scope: 'user',
+            validation: ['number']
         },
         {
             name: 'defunct',
             datatype: 'number',
             mandatory: false,
             example: null,
+            scope: 'user',
+            validation: ['number']
         },
         {
             name: 'headquarters_location',
             datatype: 'string',
             mandatory: false,
             example: 'Munich',
+            scope: 'user',
+            validation: ['string']
         },
         {
             name: 'hq_country_code',
             datatype: 'string',
             mandatory: false,
             example: 'DE',
+            scope: 'user',
             validation: ['isValidCountryCode'],
         },
         {
@@ -41,12 +49,15 @@ export const CompanyNodeSpecification: NodeSpecification = {
             datatype: 'string',
             mandatory: false,
             example: 'Munich',
+            scope: 'user',
+            validation: ['string']
         },
         {
             name: 'legal_hq_country_code',
             datatype: 'string',
             mandatory: false,
             example: 'DE',
+            scope: 'user',
             validation: ['isValidCountryCode'],
         },
     ],
