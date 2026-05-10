56ce919694e20e8cb31789176c3d57bd2ccdd130
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-10 12:39:38 +0200

    fixing "is mandatory" validator

--- src/controllers/validators/isMandatory.ts
+++ src/controllers/validators/isMandatory.ts
@@ -1,3 +1,19 @@
 export function isMandatory(value: unknown) {
-    return !!value
+    if (value === null) {
+        return false
+    }
+
+    if (value === undefined) {
+        return false
+    }
+
+    if (typeof value === 'string') {
+        return !!value
+    }
+
+    if (typeof value === 'number') {
+        return !isNaN(value)
+    }
+
+    return true
 }
