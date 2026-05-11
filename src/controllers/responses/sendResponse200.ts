7a339c10681bb8624c36b7772094329767087adf
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-11 18:14:07 +0200

    using JSON:API content type

--- src/controllers/responses/sendResponse200.ts
+++ src/controllers/responses/sendResponse200.ts
@@ -1,7 +1,7 @@
 import express from "express"
 
 export function sendResponse200(data: unknown, res: express.Response) {
-    res.set('Content-Type', 'application/json')
+    res.set('Content-Type', 'application/vnd.api+json')
     res.set('Cache-Control', 'public, max-age=60, stale-while-revalidate=300, stale-if-error=600')
     res.send(data)
 }
--- src/controllers/responses/sendResponse201.ts
+++ src/controllers/responses/sendResponse201.ts
@@ -2,7 +2,7 @@ import express from "express"
 
 export function sendResponse201(data: unknown, res: express.Response) {
     res.status(201)
-    res.set('Content-Type', 'application/json')
+    res.set('Content-Type', 'application/vnd.api+json')
     res.set('Cache-Control', 'no-store')
     res.send(data)
 }
