e600be46ab27c409532ad1eb41e9e64471bc28f1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-12 08:59:17 +0200

    merging validators for "mandatory string" and "mandatory number"

--- tests/unit/controllers/validators/is-mandatory.test.ts
+++ tests/unit/controllers/validators/is-mandatory.test.ts
@@ -1,32 +1,35 @@
 import {describe, expect, test} from "vitest"
-import {isMandatoryNumber} from "../../../../src/controllers/validators/isMandatoryNumber"
+import {isMandatory} from "../../../../src/controllers/validators/isMandatory"
 
-describe('Validating mandatory numbers', () => {
+describe('Validating mandatory data', () => {
     test.each([
+        ['test'],
+        ['0'],
+        ['true'],
+        ['false'],
+        ['null'],
+        ['undefined'],
         [0],
         [0.0],
         [0.01],
         [-0.01],
+        [-1],
         [1],
         [999999999],
+        [true],
+        [false],
     ])('valid value: $0', async (input) => {
-        expect(isMandatoryNumber(input))
+        expect(isMandatory(input))
             .toBeTruthy()
     })
 
     test.each([
-        ['test'],
         [''],
-        ['0'],
-        ['0.1'],
-        ['-1'],
-        [true],
-        [false],
-        [NaN],
         [null],
         [undefined],
+        [NaN],
     ])('invalid value: $0', async (input) => {
-        expect(isMandatoryNumber(input))
+        expect(isMandatory(input))
             .toBeFalsy()
     })
 })
