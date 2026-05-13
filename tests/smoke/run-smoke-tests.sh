093e418bee23a53f198cc606d4fd982e134096ff
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-13 09:30:19 +0200

    automatically starting the mock servers when running the smoke tests

--- tests/smoke/run-smoke-tests.sh
+++ tests/smoke/run-smoke-tests.sh
@@ -20,6 +20,7 @@ echo ----------------------------------------------------------
 
 if [ "$TEST_RUNNER" = local ]; then
   cd "$SCRIPT_PATH"/bruno || exit
+  npm run local:mockserver:start &
   if [ "$REPORTS_ENABLED" = true ]; then
     mkdir -p "$REPORTS_PATH"
     npx bru run --sandbox=developer --env "$TARGET_CLUSTER"-"$TARGET_ENVIRONMENT" --reporter-json "$REPORTS_PATH"/report.json --reporter-junit "$REPORTS_PATH"/report.xml --reporter-html "$REPORTS_PATH"/report.html
