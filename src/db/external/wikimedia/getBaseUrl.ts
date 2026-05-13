fb09deb7cffede4cf4c705b10aa108b46ad02fb2
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-13 11:18:10 +0200

    renaming the environment variables for the external APIs

--- src/db/external/wikimedia/getBaseUrl.ts
+++ src/db/external/wikimedia/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export function getBaseUrl() {
-    const host = process.env.WIKIMEDIA_MOCK_SERVER_URL || "https://commons.wikimedia.org"
+    const host = process.env.WIKIMEDIA_SERVER_URL || "https://commons.wikimedia.org"
 
     return `${host}/w/api.php`
 }
--- src/db/external/youtube/getBaseUrl.ts
+++ src/db/external/youtube/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export function getBaseUrl() {
-    const host = process.env.YOUTUBE_MOCK_SERVER_URL || "https://www.googleapis.com"
+    const host = process.env.YOUTUBE_SERVER_URL || "https://www.googleapis.com"
 
     return `${host}/youtube/v3/videos`
 }
