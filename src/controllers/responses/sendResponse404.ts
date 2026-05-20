c647ce161942693e60144a3c09582c3a6e8a257e
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-20 12:09:16 +0200

    updating 404 responses to be JSON:API compatible

--- src/controllers/responses/sendResponse404.ts
+++ src/controllers/responses/sendResponse404.ts
@@ -2,7 +2,15 @@ import express from "express"
 
 export function sendResponse404(res: express.Response) {
     res.status(404)
-    res.set('Content-Type', 'text/plain')
+    res.set('Content-Type', 'application/vnd.api+json')
     res.set('Cache-Control', 'public, max-age=30, stale-while-revalidate=60, stale-if-error=600')
-    res.send('Resource not found.')
+    res.send({
+        errors: [
+            {
+                status: '404',
+                title: 'Node not found',
+                detail: `The request contains a reference to a node that does not exist, does not exist anymore, or is temporarily unavailable.`,
+            },
+        ],
+    })
 }
