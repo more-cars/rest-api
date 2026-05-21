8c890e724608757a048e9d11467fd5844fe18a10
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-21 14:40:32 +0200

    updating 409 responses to be JSON:API compatible

--- src/controllers/responses/sendResponse409.ts
+++ src/controllers/responses/sendResponse409.ts
@@ -2,7 +2,15 @@ import express from "express"
 
 export function sendResponse409(res: express.Response) {
     res.status(409)
-    res.set('Content-Type', 'text/plain')
+    res.set('Content-Type', 'application/vnd.api+json')
     res.set('Cache-Control', 'no-store')
-    res.send('Request failed. The provided data already exists in the database.')
+    res.send({
+        errors: [
+            {
+                status: '409',
+                title: 'Node already exists',
+                detail: 'There already exists a node with this data in the database.',
+            },
+        ],
+    })
 }
