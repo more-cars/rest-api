73cabb6f56ed2c16cb7bd4cf03bfda5332655b17
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-21 15:07:12 +0200

    updating 500 responses to be JSON:API compatible

--- src/controllers/responses/sendResponse500.ts
+++ src/controllers/responses/sendResponse500.ts
@@ -2,7 +2,15 @@ import express from "express"
 
 export function sendResponse500(res: express.Response) {
     res.status(500)
-    res.set('Content-Type', 'text/plain')
+    res.set('Content-Type', 'application/vnd.api+json')
     res.set('Cache-Control', 'public, max-age=0, stale-if-error=600')
-    res.send('Request failed.')
+    res.send({
+        errors: [
+            {
+                status: '500',
+                title: 'Technical Difficulties',
+                detail: 'The request failed because of unknown reason. Please try again later.',
+            },
+        ],
+    })
 }
