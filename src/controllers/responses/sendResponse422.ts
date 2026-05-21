6a68301117e2a0373e2f86166c4dac62d26bb339
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-21 15:02:33 +0200

    updating 422 responses to be JSON:API compatible

--- src/controllers/responses/sendResponse422.ts
+++ src/controllers/responses/sendResponse422.ts
@@ -2,7 +2,15 @@ import express from "express"
 
 export function sendResponse422(res: express.Response) {
     res.status(422)
-    res.set('Content-Type', 'text/plain')
+    res.set('Content-Type', 'application/vnd.api+json')
     res.set('Cache-Control', 'no-store')
-    res.send('Request failed. The provided data might be semantically incorrect.')
+    res.send({
+        errors: [
+            {
+                status: '422',
+                title: 'Semantical Error',
+                detail: 'Request could not be processed. There is probably a semantical error in the provided data.',
+            },
+        ],
+    })
 }
