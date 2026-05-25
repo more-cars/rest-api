dab4cf628fb001e417c304f24750cc691a175962
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-25 15:49:03 +0200

    implementing test step

---
+++ tests/behavior/steps/Then/Then the response should return a relationship with {node_type} {label}.ts
@@ -0,0 +1,25 @@
+import {Then} from "@cucumber/cucumber"
+import assert from "assert"
+import {NodeManager} from "../../lib/NodeManager"
+import {ResponseManager} from "../../lib/ResponseManager"
+import {validateJson} from "../../../_toolbox/validateJson"
+import type {RelationResponse} from "../../../../src/controllers/types/RelationResponse"
+import {RelationshipSchema} from "../../../_toolbox/schemas/response/RelationshipSchema"
+
+Then('the response should return a relationship with {string} {string}',
+    (nodeType: string, label: string) => {
+        const expectedNode = NodeManager.getNodeByLabel(label)
+        const response = ResponseManager.getPreviousResponse()
+        const relationResponse = response.body as RelationResponse
+
+        assert.ok(validateJson(relationResponse, RelationshipSchema))
+
+        for (const expectedProperty in expectedNode.fields) {
+            if (expectedProperty === 'id') {
+                assert.equal(relationResponse.data?.id, expectedNode.fields.id)
+            } else {
+                // @ts-expect-error TS7053 TS7053
+                assert.equal(relationResponse.data?.attributes[expectedProperty], expectedNode.fields[expectedProperty])
+            }
+        }
+    })
