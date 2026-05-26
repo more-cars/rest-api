d688d4eb38fb4cfb194c728fe3c3748d6127a910
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-26 16:39:22 +0200

    deleting obsolete code

--- tests/performance/_testdata/createImageBelongsToNodeRelationship.ts
+++
@@ -1,6 +0,0 @@
-import http from "k6/http"
-
-export function createImageBelongsToNodeRelationship(imageId: number, nodeId: number) {
-    const url = `${__ENV.API_URL}/images/${imageId}/belongs-to-node/${nodeId}`
-    http.post(url)
-}
