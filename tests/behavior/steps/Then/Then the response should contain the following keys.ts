1973c444cc0698d94910f0ad55682728a60a28ef
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-27 16:24:05 +0200

    removing obsolete code

--- tests/behavior/steps/Then/Then the response should contain the following keys.ts
+++ tests/behavior/steps/Then/Then the response should contain the following keys.ts
@@ -7,16 +7,6 @@ Then('the response should contain the following keys',
         const keys = dataTable.hashes().map(row => row.key)
         const response = ResponseManager.getPreviousResponse()
 
-        // TODO temporary workaround -> can be removed when all relationship-related scenarios have been migrated
-        if (keys.includes('relationship_id')) {
-            assert('relationship_id' in response.body.data, "relationship_id not found in the response")
-            assert('relationship_name' in response.body.data, "relationship_name not found in the response")
-            assert('start_node' in response.body.data, "start_node not found in the response")
-            assert('partner_node' in response.body.data, "partner_node not found in the response")
-
-            return
-        }
-
         const data = response.body.attributes
         keys.forEach((key) => {
             assert(key in data, `"${key}" not found in the response`)
