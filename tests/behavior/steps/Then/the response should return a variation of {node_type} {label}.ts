703946c8e05d4f8702b176a79b59840d62eee7c3
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-29 08:27:58 +0200

    implementing test step

---
+++ tests/behavior/steps/Then/the response should return a variation of {node_type} {label}.ts
@@ -0,0 +1,23 @@
+import {Then} from "@cucumber/cucumber"
+import assert from "assert"
+import {NodeManager} from "../../lib/NodeManager"
+import {ResponseManager} from "../../lib/ResponseManager"
+import type {NodeResponse} from "../../../../src/controllers/types/NodeResponse"
+import {getResponseNodeSchema} from "../../../_toolbox/schemas/response/getResponseNodeSchema"
+import {getBasePathFragmentForNodeType} from "../../lib/getBasePathFragmentForNodeType"
+import {ControllerNodeType} from "../../../../src/controllers/types/ControllerNodeType"
+import {validateJson} from "../../../_toolbox/validateJson"
+
+Then('the response should return a variation of {string} {string}',
+    (nodeType: string, label: string) => {
+        const originalNode = NodeManager.getNodeByLabel(label)
+        const response = ResponseManager.getPreviousResponse()
+        const nodeVariant = response.body as NodeResponse
+        const originalData = (({id, ...fields}) => fields)(originalNode.fields)
+        const variantData = nodeVariant.attributes
+
+        const schema = getResponseNodeSchema(getBasePathFragmentForNodeType(nodeType) as ControllerNodeType)
+        assert.ok(validateJson(nodeVariant, schema))
+        assert.equal(originalNode.fields.id, nodeVariant.id)
+        assert.notEqual(JSON.stringify(originalData), JSON.stringify(variantData))
+    })
