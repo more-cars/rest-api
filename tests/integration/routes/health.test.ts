7dfdcf1cd3254e7c1206af486662bcaa4e0a9359
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-06-17 17:00:44 +0200

    updating integration tests regarding health check endpoints

--- tests/integration/routes/health.test.ts
+++ tests/integration/routes/health.test.ts
@@ -3,14 +3,22 @@ import request from "supertest"
 import {app} from "../../../src/app"
 import {HealthController} from "../../../src/controllers/HealthController"
 
-describe('Health', () => {
-    vi.mock("../../../src/controllers/HealthController.ts", {spy: true})
+vi.mock("../../../src/controllers/HealthController.ts", {spy: true})
+
+describe('Health Check', () => {
+    test('Get health', async () => {
+        await request(app)
+            .get('/healthz')
+
+        expect(HealthController.healthz)
+            .toHaveBeenCalledTimes(1)
+    })
 
     test('Get health', async () => {
         await request(app)
-            .get('/health')
+            .get('/ready')
 
-        expect(HealthController.health)
+        expect(HealthController.ready)
             .toHaveBeenCalledTimes(1)
     })
 })
