69225f1cdd838ea9a430e20db30eb6eeabc2175e
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-06-28 12:21:50 +0200

    switching from "kubernetes dashboard" to "headlamp"

--- deployment/minikube-cluster-start.sh
+++ deployment/minikube-cluster-start.sh
@@ -2,7 +2,8 @@
 
 minikube start --memory 8000 --cpus 2 -p morecars
 minikube addons enable metrics-server -p morecars
-minikube dashboard -p morecars &
+minikube addons enable headlamp -p morecars
+minikube service headlamp -n headlamp -p morecars
 
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
